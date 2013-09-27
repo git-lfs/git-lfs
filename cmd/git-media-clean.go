0694316ae0ed96ab08fa62eaf9defbae1a0a233e
risk danger olson <technoweenie@gmail.com>
2013-09-27 09:19:54 -0600

    add a git-media clean script

---
+++ cmd/git-media-clean.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	".."
+	"crypto/md5"
+	"crypto/sha1"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	tmp, err := gitmedia.TempFile()
+	if err != nil {
+		fmt.Println("Error trying to create temp file")
+		panic(err)
+	}
+
+	sha1Hash := sha1.New()
+	md5Hash := md5.New()
+	writer := io.MultiWriter(sha1Hash, md5Hash, tmp)
+
+	written, _ := io.Copy(writer, os.Stdin)
+	sha := hex.EncodeToString(sha1Hash.Sum(nil))
+
+	meta := &gitmedia.LargeAsset{
+		MediaType: "application/vnd.github.large-asset",
+		Size:      written,
+		MD5:       hex.EncodeToString(md5Hash.Sum(nil)),
+		SHA1:      sha,
+	}
+
+	mediafile := gitmedia.LocalMediaPath(sha)
+	metafile := mediafile + ".json"
+
+	if err := os.Rename(tmp.Name(), mediafile); err != nil {
+		fmt.Printf("Unable to move %s to %s\n", tmp.Name(), mediafile)
+		panic(err)
+	}
+	fmt.Println(tmp.Name())
+
+	file, err := os.Create(metafile)
+	if err != nil {
+		fmt.Printf("Unable to create meta data file: %s\n", metafile)
+		panic(err)
+	}
+
+	defer file.Close()
+	defer func() { os.Remove(tmp.Name()) }()
+
+	enc := json.NewEncoder(io.MultiWriter(os.Stdout, file))
+	enc.Encode(meta)
+}
--- gitmedia.go
+++ gitmedia.go
@@ -2,13 +2,31 @@ package gitmedia
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 )
 
 const Version = "0.0.1"
 
-var LargeSizeThreshold = 5 * 1024 * 1024
+var (
+	LargeSizeThreshold = 5 * 1024 * 1024
+	TempDir            = filepath.Join(os.TempDir(), "git-media")
+	LocalMediaDir      string
+)
+
+type LargeAsset struct {
+	MediaType string `json:"media_type"`
+	Size      int64  `json:"size"`
+	MD5       string `json:"md5"`
+	SHA1      string `json:"sha1"`
+}
+
+func TempFile() (*os.File, error) {
+	return ioutil.TempFile(TempDir, "")
+}
 
 func SimpleExec(name string, arg ...string) string {
 	output, err := exec.Command(name, arg...).Output()
@@ -20,4 +38,28 @@ func SimpleExec(name string, arg ...string) string {
 	}
 
 	return strings.Trim(string(output), " \n")
+}
+
+func LocalMediaPath(sha string) string {
+	path := filepath.Join(LocalMediaDir, sha[0:2], sha[2:4])
+	if err := os.MkdirAll(path, 0744); err != nil {
+		fmt.Printf("Error trying to create local media directory: %s\n", path)
+		panic(err)
+	}
+
+	return filepath.Join(path, sha)
+}
+
+func init() {
+	wd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	LocalMediaDir = filepath.Join(wd, ".git", "media")
+
+	if err = os.MkdirAll(TempDir, 0744); err != nil {
+		fmt.Printf("Error trying to create temp directory: %s\n", TempDir)
+		panic(err)
+	}
 }
