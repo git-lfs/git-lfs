aad597d449c216bd6c3f3b213efd7f2e552430f1
rick <technoweenie@gmail.com>
2013-10-31 16:12:30 -0600

    add support for pulling credentials out of git-credentials
    /cc @arrbee

--- client/client.go
+++ client/client.go
@@ -1,12 +1,17 @@
 package gitmediaclient
 
 import (
+	"bytes"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"strings"
 )
 
 func Put(filename string) error {
@@ -69,11 +74,57 @@ func Get(filename string) (io.ReadCloser, error) {
 }
 
 func clientRequest(method, oid string) (*http.Request, error) {
-	return http.NewRequest(method, objectUrl(oid), nil)
+	u := objectUrl(oid)
+	req, err := http.NewRequest(method, u.String(), nil)
+	if err == nil {
+		creds, err := credentials(u)
+		if err != nil {
+			return req, err
+		}
+
+		token := fmt.Sprintf("%s:%s", creds["username"], creds["password"])
+		auth := "Basic " + base64.URLEncoding.EncodeToString([]byte(token))
+		req.Header.Set("Authorization", auth)
+	}
+
+	return req, err
 }
 
-func objectUrl(oid string) string {
-	return "http://localhost:8080/objects/" + oid
+func objectUrl(oid string) *url.URL {
+	u, _ := url.Parse("http://localhost:8080")
+	u.Path = "/objects/" + oid
+	return u
+}
+
+func credentials(u *url.URL) (map[string]string, error) {
+	creds := make(map[string]string)
+
+	credInput := fmt.Sprintf("protocol=%s\nhost=%s\n", u.Scheme, u.Host)
+	buf := new(bytes.Buffer)
+
+	cmd := exec.Command("git", "credential", "fill")
+	cmd.Stdin = bytes.NewBufferString(credInput)
+	cmd.Stdout = buf
+
+	err := cmd.Start()
+	if err != nil {
+		return creds, err
+	}
+
+	err = cmd.Wait()
+	if err != nil {
+		return creds, err
+	}
+
+	for _, line := range strings.Split(buf.String(), "\n") {
+		pieces := strings.SplitN(line, "=", 2)
+		if len(pieces) < 2 {
+			continue
+		}
+		creds[pieces[0]] = pieces[1]
+	}
+
+	return creds, nil
 }
 
 type Error struct {
