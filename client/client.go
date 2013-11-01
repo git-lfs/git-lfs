4dd79c5225ae2b8d6ddcf6bbde7cae67219c4a5f
risk danger olson <technoweenie@gmail.com>
2013-11-01 17:16:10 -0600

    extract the credential logic to a function and an embedded struct

--- client/client.go
+++ client/client.go
@@ -97,26 +97,53 @@ func objectUrl(oid string) *url.URL {
 }
 
 func credentials(u *url.URL) (map[string]string, error) {
-	creds := make(map[string]string)
-
 	credInput := fmt.Sprintf("protocol=%s\nhost=%s\n", u.Scheme, u.Host)
-	buf := new(bytes.Buffer)
+	cmd, err := execCreds(credInput, "fill")
+	if err != nil {
+		return nil, err
+	}
+	return cmd.Credentials(), nil
+}
 
-	cmd := exec.Command("git", "credential", "fill")
-	cmd.Stdin = bytes.NewBufferString(credInput)
-	cmd.Stdout = buf
-
+func execCreds(input, subCommand string) (*CredentialCmd, error) {
+	cmd := NewCommand(input, subCommand)
 	err := cmd.Start()
 	if err != nil {
-		return creds, err
+		return cmd, err
 	}
 
 	err = cmd.Wait()
-	if err != nil {
-		return creds, err
-	}
+	return cmd, err
+}
 
-	for _, line := range strings.Split(buf.String(), "\n") {
+type CredentialCmd struct {
+	bufOut *bytes.Buffer
+	bufErr *bytes.Buffer
+	*exec.Cmd
+}
+
+func NewCommand(input, subCommand string) *CredentialCmd {
+	buf1 := new(bytes.Buffer)
+	buf2 := new(bytes.Buffer)
+	cmd := exec.Command("git", "credential", subCommand)
+	cmd.Stdin = bytes.NewBufferString(input)
+	cmd.Stdout = buf1
+	cmd.Stderr = buf2
+	return &CredentialCmd{buf1, buf2, cmd}
+}
+
+func (c *CredentialCmd) StderrString() string {
+	return c.bufErr.String()
+}
+
+func (c *CredentialCmd) StdoutString() string {
+	return c.bufOut.String()
+}
+
+func (c *CredentialCmd) Credentials() map[string]string {
+	creds := make(map[string]string)
+
+	for _, line := range strings.Split(c.StdoutString(), "\n") {
 		pieces := strings.SplitN(line, "=", 2)
 		if len(pieces) < 2 {
 			continue
@@ -124,7 +151,7 @@ func credentials(u *url.URL) (map[string]string, error) {
 		creds[pieces[0]] = pieces[1]
 	}
 
-	return creds, nil
+	return creds
 }
 
 type Error struct {
