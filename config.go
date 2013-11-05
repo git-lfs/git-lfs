b0d77f122aef6908e514355472ee4bffc1a226aa
rick <technoweenie@gmail.com>
2013-11-05 10:45:01 -0700

    read toml file if it exists

--- config.go
+++ config.go
@@ -1,5 +1,11 @@
 package gitmedia
 
+import (
+	"github.com/pelletier/go-toml"
+	"os"
+	"path/filepath"
+)
+
 type Configuration struct {
 	Endpoint string
 }
@@ -12,8 +18,38 @@ var config *Configuration
 // https://github.com/mojombo/toml
 func Config() *Configuration {
 	if config == nil {
-		config = &Configuration{"http://localhost:8080"}
+		config = &Configuration{}
+		readToml(config)
 	}
 
+	panic(config.Endpoint)
 	return config
+}
+
+func readToml(config *Configuration) {
+	tomlPath := tomlFile()
+	stat, _ := os.Stat(tomlPath)
+	if stat != nil {
+		readTomlFile(tomlPath, config)
+	}
+}
+
+func tomlFile() string {
+	wd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	return filepath.Join(wd, ".gitmedia")
+}
+
+func readTomlFile(path string, config *Configuration) {
+	tomlConfig, err := toml.LoadFile(path)
+	if err != nil {
+		panic(err)
+	}
+
+	if endpoint, ok := tomlConfig.Get("endpoint").(string); ok {
+		config.Endpoint = endpoint
+	}
 }
