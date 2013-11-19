5f8c073e1f45423d4e25d60cd6a215fa0115c80a
rick <technoweenie@gmail.com>
2013-11-18 17:32:15 -0800

    add stderr messages /cc @jbarnette

--- cmd/git-media-smudge.go
+++ cmd/git-media-smudge.go
@@ -10,14 +10,16 @@ import (
 func main() {
 	sha, err := gitmedia.Decode(os.Stdin)
 	if err != nil {
-		fmt.Println("Error reading git-media meta data from stdin:")
+		fmt.Fprintln(os.Stderr, "Error reading git-media meta data from stdin:")
 		panic(err)
 	}
+
+	fmt.Fprintf(os.Stderr, "Downloading media: %s\n", sha)
 
 	err = gitmediafilters.Smudge(os.Stdout, sha)
 	if err != nil {
 		smudgerr := err.(*gitmediafilters.SmudgeError)
-		fmt.Printf("Error reading file from local media dir: %s\n", smudgerr.Filename)
+		fmt.Fprintf(os.Stderr, "Error reading file from local media dir: %s\n", smudgerr.Filename)
 		panic(err)
 	}
 }
