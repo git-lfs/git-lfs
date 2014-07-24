6e20e2883f610653d08538e2e72d26456911c5c9
Rick Olson <technoweenie@gmail.com>
2014-07-24 15:23:02 -0600

    add ini decoding

--- metafile/metafile.go
+++ metafile/metafile.go
@@ -1,10 +1,14 @@
 package metafile
 
 import (
+	"bufio"
 	"bytes"
 	"errors"
+	ini "github.com/glacjay/goini"
 	"io"
 	"regexp"
+	"strconv"
+	"strings"
 )
 
 var (
@@ -13,7 +17,6 @@ var (
 	latest        = "http://git-media.io/v/2"
 	oidType       = "sha256"
 	alphaHeaderRE = regexp.MustCompile(`\A# (.*git-media|external)`)
-	linebreak     = []byte("\n")
 )
 
 type Pointer struct {
@@ -24,7 +27,7 @@ type Pointer struct {
 }
 
 func NewPointer(oid string, size int64) *Pointer {
-	return &Pointer{alpha, oid, size, oidType}
+	return &Pointer{latest, oid, size, oidType}
 }
 
 func Encode(writer io.Writer, pointer *Pointer) (int, error) {
@@ -49,12 +52,55 @@ func Decode(reader io.Reader) (*Pointer, error) {
 	if alphaHeaderRE.Match(data) {
 		return decodeAlpha(data)
 	} else {
-		return nil, errors.New("No INI decoder yet")
+		return decodeIni(data)
 	}
 }
 
+func decodeIni(data []byte) (*Pointer, error) {
+	dict, err := ini.LoadReader(bufio.NewReader(bytes.NewReader(data)))
+	if err != nil {
+		return nil, err
+	}
+
+	v, ok := dict.GetString("git-media", "version")
+	if !ok || v != latest {
+		if len(v) == 0 {
+			v = "--"
+		}
+
+		return nil, errors.New("Invalid version: " + v)
+	}
+
+	oidValue, ok := dict.GetString("git-media", "oid")
+	if !ok {
+		return nil, errors.New("Invalid Oid")
+	}
+
+	oidParts := strings.SplitN(oidValue, ":", 2)
+	if len(oidParts) != 2 {
+		return nil, errors.New("Invalid Oid type in" + oidValue)
+	}
+	if oidParts[0] != oidType {
+		return nil, errors.New("Invalid Oid type: " + oidParts[0])
+	}
+	oid := oidParts[1]
+
+	var size int64
+	sizeStr, ok := dict.GetString("git-media", "size")
+	if !ok {
+		return nil, errors.New("Invalid Oid")
+	} else {
+		size, err = strconv.ParseInt(sizeStr, 10, 0)
+		if err != nil {
+			return nil, errors.New("Invalid size: " + sizeStr)
+		}
+	}
+
+	return NewPointer(oid, size), nil
+}
+
 func decodeAlpha(data []byte) (*Pointer, error) {
-	lines := bytes.Split(data, linebreak)
+	lines := bytes.Split(data, []byte("\n"))
 	last := len(lines) - 1
 	if last == 0 {
 		return nil, errors.New("No sha in pointer file")
