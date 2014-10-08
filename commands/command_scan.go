533eb1f74e4e172c53c120dfd6e071e7bfde0a55
rubyist <scott.barron@github.com>
2014-10-08 09:05:23 -0400

    --create-links for scan

--- commands/command_scan.go
+++ commands/command_scan.go
@@ -12,6 +12,7 @@ var (
 		Short: "Scan for git media files",
 		Run:   scanCommand,
 	}
+	createLinks = false
 )
 
 func scanCommand(cmd *cobra.Command, args []string) {
@@ -27,10 +28,14 @@ func scanCommand(cmd *cobra.Command, args []string) {
 	// ?
 	// Profit
 	for _, p := range pointers {
-		fmt.Println("Git Media OID:", p.Oid)
+		fmt.Println("Git Media OID:", p.Name)
+		if createLinks {
+			p.CreateLink(p.Name)
+		}
 	}
 }
 
 func init() {
+	scanCmd.Flags().BoolVarP(&createLinks, "create-links", "l", false, "Create git media link files")
 	RootCmd.AddCommand(scanCmd)
 }
