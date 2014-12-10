f83b028792a68f8ac07b86f2e8999ed8aee9d6b2
Rick Olson <technoweenie@gmail.com>
2014-12-10 14:40:04 -0700

    skip https verification if GIT_SSL_NO_VERIFY is set

--- gitmediaclient/client.go
+++ gitmediaclient/client.go
@@ -1,6 +1,7 @@
 package gitmediaclient
 
 import (
+	"crypto/tls"
 	"encoding/base64"
 	"encoding/json"
 	"errors"
@@ -150,8 +151,25 @@ func validateMediaHeader(contentType string, reader io.Reader) (bool, *gitmedia.
 	return true, nil
 }
 
+var httpClient *http.Client
+
+func getHttpClient() *http.Client {
+	if httpClient == nil {
+		if len(os.Getenv("GIT_SSL_NO_VERIFY")) > 0 {
+			tr := &http.Transport{
+				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+			}
+			httpClient = &http.Client{Transport: tr}
+		} else {
+			httpClient = http.DefaultClient
+		}
+	}
+
+	return httpClient
+}
+
 func doRequest(req *http.Request, creds Creds) (*http.Response, *gitmedia.WrappedError) {
-	res, err := http.DefaultClient.Do(req)
+	res, err := getHttpClient().Do(req)
 
 	var wErr *gitmedia.WrappedError
 
