c729aeac25c3283b77e15a756ea39fb66a9d4098
Rick Olson <technoweenie@gmail.com>
2015-02-11 13:45:38 -0700

    make it easy to replace the credential fetcher in tests

--- hawserclient/client_test.go
+++ hawserclient/client_test.go
@@ -3,6 +3,8 @@ package hawserclient
 import (
 	"github.com/bmizerany/assert"
 	"github.com/hawser/git-hawser/hawser"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -20,4 +22,28 @@ func TestObjectUrl(t *testing.T) {
 		config.SetConfig("hawser.url", endpoint)
 		assert.Equal(t, expected, ObjectUrl(oid).String())
 	}
+}
+
+var TmpPath string
+
+func init() {
+	wd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	TmpPath = filepath.Join(wd, "..", "tmp", "hawserclient")
+	os.MkdirAll(TmpPath, 0755)
+
+	execCreds = func(input Creds, subCommand string) (credentialFetcher, error) {
+		return &testCredentialFetcher{input}, nil
+	}
+}
+
+type testCredentialFetcher struct {
+	Creds Creds
+}
+
+func (c *testCredentialFetcher) Credentials() Creds {
+	return c.Creds
 }
