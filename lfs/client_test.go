91f3491076e413d6624de2fb01a51cfb2f3923e7
Rick Olson <technoweenie@gmail.com>
2015-03-19 20:47:35 -0600

    check authorization header in download tests

--- lfs/client_test.go
+++ lfs/client_test.go
@@ -1,7 +1,11 @@
 package lfs
 
 import (
+	"encoding/base64"
+	"fmt"
 	"io/ioutil"
+	"net/http/httptest"
+	"net/url"
 	"testing"
 )
 
@@ -11,4 +15,14 @@ func tempdir(t *testing.T) string {
 		t.Fatalf("Error getting temp dir: %s", err)
 	}
 	return dir
+}
+
+func expectedAuth(t *testing.T, server *httptest.Server) string {
+	u, err := url.Parse(server.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	token := fmt.Sprintf("%s:%s", u.Host, "monkey")
+	return "Basic " + base64.URLEncoding.EncodeToString([]byte(token))
 }
--- lfs/download_test.go
+++ lfs/download_test.go
@@ -18,15 +18,20 @@ func TestSuccessfulDownload(t *testing.T) {
 	defer os.RemoveAll(tmp)
 
 	mux.HandleFunc("/media/objects/oid", func(w http.ResponseWriter, r *http.Request) {
-		t.Logf("Method: %s", r.Method)
+		t.Logf("Server: %s %s", r.Method, r.URL)
+		t.Logf("request header: %v", r.Header)
 
 		if r.Method != "GET" {
 			w.WriteHeader(405)
 			return
 		}
 
-		if accept := r.Header.Get("Accept"); accept != mediaType {
-			t.Errorf("Invalid Accept: %s", accept)
+		if r.Header.Get("Accept") != mediaType {
+			t.Error("Invalid Accept")
+		}
+
+		if r.Header.Get("Authorization") != expectedAuth(t, server) {
+			t.Error("Invalid Authorization")
 		}
 
 		obj := &objectResource{
@@ -53,19 +58,226 @@ func TestSuccessfulDownload(t *testing.T) {
 	})
 
 	mux.HandleFunc("/download", func(w http.ResponseWriter, r *http.Request) {
-		t.Logf("Method: %s", r.Method)
+		t.Logf("Server: %s %s", r.Method, r.URL)
+		t.Logf("request header: %v", r.Header)
 
 		if r.Method != "GET" {
 			w.WriteHeader(405)
 			return
 		}
 
-		if accept := r.Header.Get("Accept"); accept != "" {
-			t.Errorf("Accept: %s", accept)
+		if r.Header.Get("Accept") != "" {
+			t.Error("Invalid Accept")
 		}
 
-		if a := r.Header.Get("A"); a != "1" {
-			t.Logf("A: %s", a)
+		if r.Header.Get("Authorization") != expectedAuth(t, server) {
+			t.Error("Invalid Authorization")
+		}
+
+		if r.Header.Get("A") != "1" {
+			t.Error("invalid A")
+		}
+
+		head := w.Header()
+		head.Set("Content-Type", "application/octet-stream")
+		head.Set("Content-Length", "4")
+		w.WriteHeader(200)
+		w.Write([]byte("test"))
+	})
+
+	Config.SetConfig("lfs.url", server.URL+"/media")
+	reader, size, wErr := Download("oid")
+	if wErr != nil {
+		t.Fatalf("unexpected error: %s", wErr)
+	}
+	defer reader.Close()
+
+	if size != 4 {
+		t.Errorf("unexpected size: %d", size)
+	}
+
+	by, err := ioutil.ReadAll(reader)
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+
+	if body := string(by); body != "test" {
+		t.Errorf("unexpected body: %s", body)
+	}
+}
+
+// nearly identical to TestSuccessfulDownload
+// the api request returns a custom Authorization header
+func TestSuccessfulDownloadWithAuthorization(t *testing.T) {
+	mux := http.NewServeMux()
+	server := httptest.NewServer(mux)
+	tmp := tempdir(t)
+	defer server.Close()
+	defer os.RemoveAll(tmp)
+
+	mux.HandleFunc("/media/objects/oid", func(w http.ResponseWriter, r *http.Request) {
+		t.Logf("Server: %s %s", r.Method, r.URL)
+		t.Logf("request header: %v", r.Header)
+
+		if r.Method != "GET" {
+			w.WriteHeader(405)
+			return
+		}
+
+		if r.Header.Get("Accept") != mediaType {
+			t.Error("Invalid Accept")
+		}
+
+		if r.Header.Get("Authorization") != expectedAuth(t, server) {
+			t.Error("Invalid Authorization")
+		}
+
+		obj := &objectResource{
+			Oid:  "oid",
+			Size: 4,
+			Links: map[string]*linkRelation{
+				"download": &linkRelation{
+					Href: server.URL + "/download",
+					Header: map[string]string{
+						"A":             "1",
+						"Authorization": "custom",
+					},
+				},
+			},
+		}
+
+		by, err := json.Marshal(obj)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		head := w.Header()
+		head.Set("Content-Type", mediaType)
+		head.Set("Content-Length", strconv.Itoa(len(by)))
+		w.WriteHeader(200)
+		w.Write(by)
+	})
+
+	mux.HandleFunc("/download", func(w http.ResponseWriter, r *http.Request) {
+		t.Logf("Server: %s %s", r.Method, r.URL)
+		t.Logf("request header: %v", r.Header)
+
+		if r.Method != "GET" {
+			w.WriteHeader(405)
+			return
+		}
+
+		if r.Header.Get("Accept") != "" {
+			t.Error("Invalid Accept")
+		}
+
+		if r.Header.Get("Authorization") != "custom" {
+			t.Error("Invalid Authorization")
+		}
+
+		if r.Header.Get("A") != "1" {
+			t.Error("invalid A")
+		}
+
+		head := w.Header()
+		head.Set("Content-Type", "application/octet-stream")
+		head.Set("Content-Length", "4")
+		w.WriteHeader(200)
+		w.Write([]byte("test"))
+	})
+
+	Config.SetConfig("lfs.url", server.URL+"/media")
+	reader, size, wErr := Download("oid")
+	if wErr != nil {
+		t.Fatalf("unexpected error: %s", wErr)
+	}
+	defer reader.Close()
+
+	if size != 4 {
+		t.Errorf("unexpected size: %d", size)
+	}
+
+	by, err := ioutil.ReadAll(reader)
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+
+	if body := string(by); body != "test" {
+		t.Errorf("unexpected body: %s", body)
+	}
+}
+
+// nearly identical to TestSuccessfulDownload
+// download is served from a second server
+func TestSuccessfulDownloadFromSeparateHost(t *testing.T) {
+	mux := http.NewServeMux()
+	server := httptest.NewServer(mux)
+	mux2 := http.NewServeMux()
+	server2 := httptest.NewServer(mux2)
+	tmp := tempdir(t)
+	defer server.Close()
+	defer server2.Close()
+	defer os.RemoveAll(tmp)
+
+	mux.HandleFunc("/media/objects/oid", func(w http.ResponseWriter, r *http.Request) {
+		t.Logf("Server: %s %s", r.Method, r.URL)
+		t.Logf("request header: %v", r.Header)
+
+		if r.Method != "GET" {
+			w.WriteHeader(405)
+			return
+		}
+
+		if r.Header.Get("Accept") != mediaType {
+			t.Error("Invalid Accept")
+		}
+
+		if r.Header.Get("Authorization") != expectedAuth(t, server) {
+			t.Error("Invalid Authorization")
+		}
+
+		obj := &objectResource{
+			Oid:  "oid",
+			Size: 4,
+			Links: map[string]*linkRelation{
+				"download": &linkRelation{
+					Href:   server2.URL + "/download",
+					Header: map[string]string{"A": "1"},
+				},
+			},
+		}
+
+		by, err := json.Marshal(obj)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		head := w.Header()
+		head.Set("Content-Type", mediaType)
+		head.Set("Content-Length", strconv.Itoa(len(by)))
+		w.WriteHeader(200)
+		w.Write(by)
+	})
+
+	mux2.HandleFunc("/download", func(w http.ResponseWriter, r *http.Request) {
+		t.Logf("Server: %s %s", r.Method, r.URL)
+		t.Logf("request header: %v", r.Header)
+
+		if r.Method != "GET" {
+			w.WriteHeader(405)
+			return
+		}
+
+		if r.Header.Get("Accept") != "" {
+			t.Error("Invalid Accept")
+		}
+
+		if r.Header.Get("Authorization") != "" {
+			t.Error("Invalid Authorization")
+		}
+
+		if r.Header.Get("A") != "1" {
+			t.Error("invalid A")
 		}
 
 		head := w.Header()
