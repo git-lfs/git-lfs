79d578b3664f95f36c681e068e9601e0d99d8a68
Claes Elgemark <claes.elgemark@sonymobile.com>
2015-04-22 16:12:45 +0200

    Add missing check for errors in smudge and clean commands

--- commands/command_smudge.go
+++ commands/command_smudge.go
@@ -63,7 +63,7 @@ func smudgeCommand(cmd *cobra.Command, args []string) {
 
 	if err != nil {
 		ptr.Encode(os.Stdout)
-		LoggedError(err, "Error accessing media: %s (%s)", filename, ptr.Oid)
+		Panic(err, "Error accessing media: %s (%s)", filename, ptr.Oid)
 	}
 }
 
