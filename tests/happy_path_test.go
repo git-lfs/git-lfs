af26065c45941d2e24b59a4b21a611713c8efe36
Rick Olson <technoweenie@gmail.com>
2015-05-08 16:46:32 -0600

    ensure correct git-lfs command is used

--- tests/happy_path_test.go
+++ tests/happy_path_test.go
@@ -8,10 +8,25 @@ func TestHappyPath(t *testing.T) {
 	run := Setup(t)
 	defer run.Teardown()
 
-	run.Git("lfs", "track", "*.dat")
+	AssertCommand(t,
+		run.Git("lfs", "track", "*.dat"),
+		"Tracking *.dat",
+	)
+
 	run.WriteFile("a.dat", []byte("a"))
 	run.Git("add", "a.dat")
-	run.Git("commit", "-m", "add a.dat")
+
+	AssertCommandContains(t,
+		run.Git("commit", "-m", "add a.dat"),
+		"master (root-commit)",
+		"1 file changed",
+		"create mode 100755 a.dat",
+	)
 
 	AssertString(t, "a", string(run.ReadFile("a.dat")))
+
+	AssertPointerBlob(run,
+		"ca978112ca1bbdcafac231b39a23dc4da786eff8147c4e72b9807785afee48bb", 1,
+		"master", "a.dat",
+	)
 }
--- tests/tests.go
+++ tests/tests.go
@@ -11,7 +11,11 @@ import (
 	"testing"
 )
 
-var testDir string
+var (
+	rootDir string
+	testDir string
+	bin     string
+)
 
 type runner struct {
 	tempDir string
@@ -20,8 +24,30 @@ type runner struct {
 }
 
 // Git executes a Git command.
-func (r *runner) Git(args ...string) {
-	r.exec("git", args...)
+func (r *runner) Git(args ...string) string {
+	return r.exec("git", args...)
+}
+
+func (r *runner) GitBlob(commitish, path string) string {
+	out := r.Git("ls-tree", commitish)
+	lines := strings.Split(out, "\n")
+	for _, line := range lines {
+		tabs := strings.Split(line, "\t")
+		if len(tabs) < 2 {
+			continue
+		}
+
+		attrs := strings.Split(tabs[0], " ")
+		if len(attrs) < 3 {
+			continue
+		}
+
+		if tabs[1] == path {
+			return attrs[2]
+		}
+	}
+
+	return ""
 }
 
 // WriteFile writes the byte contents to the given path, which should be
@@ -43,17 +69,13 @@ func (r *runner) ReadFile(path string) []byte {
 	return by
 }
 
-func (r *runner) exec(name string, args ...string) {
-	loggedArgs := make([]string, len(args)+1)
-	loggedArgs[0] = name
-	for idx, arg := range args {
-		if strings.Contains(arg, " ") {
-			arg = fmt.Sprintf(`"%s"`, arg)
-		}
-		loggedArgs[idx+1] = arg
+func (r *runner) exec(name string, args ...string) string {
+	if name == "git" && len(args) > 0 && args[0] == "lfs" {
+		name = bin
+		args = args[1:len(args)]
 	}
 
-	r.Logf("$ %s", strings.Join(loggedArgs, " "))
+	r.logCmd(name, args...)
 
 	cmd := exec.Command(name, args...)
 	out := &bytes.Buffer{}
@@ -63,6 +85,32 @@ func (r *runner) exec(name string, args ...string) {
 	if err := cmd.Run(); err != nil {
 		r.Fatalf("%s\n\n%s", err, out.String())
 	}
+
+	return out.String()
+}
+
+func (r *runner) logCmd(name string, args ...string) {
+	loggedArgs := make([]string, len(args)+1)
+	if strings.HasPrefix(name, "/") {
+		rel, err := filepath.Rel(rootDir, name)
+		if err == nil {
+			loggedArgs[0] = rel
+		} else {
+			r.Errorf("Cannot make %q relative to %q", name, rootDir)
+			loggedArgs[0] = name
+		}
+	} else {
+		loggedArgs[0] = name
+	}
+
+	for idx, arg := range args {
+		if strings.Contains(arg, " ") {
+			arg = fmt.Sprintf(`"%s"`, arg)
+		}
+		loggedArgs[idx+1] = arg
+	}
+
+	r.Logf("$ %s", strings.Join(loggedArgs, " "))
 }
 
 func (r *runner) repoPath(path string) string {
@@ -111,11 +159,21 @@ func (r *runner) Teardown() {
 func init() {
 	wd, err := os.Getwd()
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
-	testDir = filepath.Join(wd, "..", "tmp")
+	rootDir = filepath.Join(wd, "..")
+
+	testDir = filepath.Join(rootDir, "tmp")
 	if err := os.MkdirAll(testDir, 0755); err != nil {
-		panic(err)
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	bin = filepath.Join(rootDir, "bin", "git-lfs")
+	if _, err := os.Stat(bin); err != nil {
+		fmt.Println("git-lfs is not compiled to " + bin)
+		os.Exit(1)
 	}
 }
