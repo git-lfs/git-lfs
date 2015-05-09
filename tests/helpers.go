8cded4d4fa09de6cad82752f185a23c82f5c8ae0
Rick Olson <technoweenie@gmail.com>
2015-05-09 08:13:53 -0600

    forgot to add this

---
+++ tests/helpers.go
@@ -0,0 +1,54 @@
+package tests
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
+
+// Pointer builds a Git LFS pointer.
+func Pointer(oid string, size int64) string {
+	return fmt.Sprintf("version https://git-lfs.github.com/spec/v1\noid sha256:%s\nsize %d", oid, size)
+}
+
+// AssertPointerBlob ensures that the pointer of the given oid and size is
+// committed to Git at the given commitish and path.
+func AssertPointerBlob(run *runner, oid string, size int64, commitish, path string) {
+	blob := run.GitBlob(commitish, path)
+	AssertCommand(run.T,
+		run.Git("cat-file", "-p", blob),
+		Pointer(oid, size),
+	)
+}
+
+// AssertString ensures that the given strings match.
+func AssertString(t *testing.T, expected, actual string) {
+	if expected != actual {
+		t.Fatalf("Expected %s, got %s", expected, actual)
+	}
+}
+
+// AssertCommand checks the output of a command.  The expected value comes
+// after the command, and does not need a trailing linebreak.
+func AssertCommand(t *testing.T, output, expected string) {
+	if expected != strings.TrimSpace(output) {
+		t.Fatalf("Expected:\n%s\n\nGot:\n%s", expected, output)
+	}
+}
+
+// AssertCommandContains checks that each given part is in the command's output.
+func AssertCommandContains(t *testing.T, output string, parts ...string) {
+	trimmed := strings.TrimSpace(output)
+	failed := false
+
+	for _, part := range parts {
+		if !strings.Contains(trimmed, part) {
+			failed = true
+			t.Errorf("Expected %q", part)
+		}
+	}
+
+	if failed {
+		t.Fatalf("Got:\n%s", output)
+	}
+}
