801dfc3283d3e01c41a06fd98cd40d6976582820
Rick Olson <technoweenie@gmail.com>
2015-05-21 15:42:07 -0600

    fail early if the track command runs outside a git repo

--- test/test-track.sh
+++ test/test-track.sh
@@ -48,3 +48,27 @@ begin_test "track without trailing linebreak"
   fi
 )
 end_test
+
+begin_test "track outside git repo"
+(
+  set -e
+
+  git lfs track "*.foo" || {
+    # this fails if it's run outside of a git repo using GIT_LFS_TEST_DIR
+
+    # git itself returns an exit status of 128
+    # $ git show
+    # fatal: Not a git repository (or any of the parent directories): .git
+    # $ echo "$?"
+    # 128
+
+    [ "$?" == "128" ]
+    exit 0
+  }
+
+  if [ -n "$GIT_LFS_TEST_DIR" ]; then
+    echo "GIT_LFS_TEST_DIR should be set outside of any Git repository"
+    exit 1
+  fi
+)
+end_test
