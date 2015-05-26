793d894fe87217bd6067751abd1f8e8d7ffb7d33
Rick Olson <technoweenie@gmail.com>
2015-05-26 11:48:24 -0600

    port env tests

--- test/test-submodule.sh
+++ test/test-submodule.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 . "test/testlib.sh"
+reponame="submodule-test-repo"
+submodname="submodule-test-submodule"
 
 begin_test "submodule local git dir"
 (
   set -e
 
-  reponame="$(basename "$0" ".sh")"
-  submodname="$reponame-submodule"
 
   setup_remote_repo "$reponame"
   setup_remote_repo "$submodname"
@@ -21,8 +21,38 @@ begin_test "submodule local git dir"
 
   clone_repo "$reponame" repo
   git submodule add "$GITSERVER/$submodname" sub
-  cd sub/dir
-  cat README | grep "sub module"
-  git lfs help
+  git submodule update
+  git add .gitmodules sub
+  git commit -m "add submodule"
+  git push origin master
+
+  cat sub/dir/README | grep "sub module"
+)
+end_test
+
+begin_test "submodule env"
+(
+  set -e
+
+  # using the local clone from the above test
+  cd repo
+  expected=$(printf "Endpoint=$GITSERVER/$reponame.git/info/lfs
+LocalWorkingDir=$TRASHDIR/repo
+LocalGitDir=$TRASHDIR/repo/.git
+LocalMediaDir=$TRASHDIR/repo/.git/lfs/objects
+TempDir=$TRASHDIR/repo/.git/lfs/tmp
+GIT_LFS_TEST_DIR=$TMPDIR
+GIT_LFS_TEST_MAXPROCS=4
+")
+  actual=$(git lfs env)
+  [ "$expected" == "$actual" ]
+
+  cd .git
+
+  [ "$expected" == "$actual" ]
+
+  cd ../sub
+
+  [ "$expected" == "$actual" ]
 )
 end_test
