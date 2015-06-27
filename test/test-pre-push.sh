6c0a9af737120f9fab804e0c2c397c4ab5a2670f
Rick Olson <technoweenie@gmail.com>
2015-06-27 10:42:50 -0500

    add a push test containing git and lfs objects

--- test/test-pre-push.sh
+++ test/test-pre-push.sh
@@ -24,7 +24,6 @@ begin_test "pre-push"
   git add hi.dat
   git commit -m "add hi.dat"
   git show
-  git lfs env
 
   # file isn't on the git lfs server yet
   curl -v "$GITSERVER/$reponame.git/info/lfs/objects/98ea6e4f216f2fb4b69fff9b3a44842c38686ca685f3f55dc48c5d3fb1107be4" \
@@ -78,7 +77,6 @@ begin_test "pre-push dry-run"
   git add hi.dat
   git commit -m "add hi.dat"
   git show
-  git lfs env
 
   # file doesn't exist yet
   curl -v "$GITSERVER/$reponame.git/info/lfs/objects/2840e0eafda1d0760771fe28b91247cf81c76aa888af28a850b5648a338dc15b" \
@@ -98,5 +96,44 @@ begin_test "pre-push dry-run"
     -H "Accept: application/vnd.git-lfs+json" 2>&1 |
     tee http.log
   grep "404 Not Found" http.log
+)
+end_test
+
+begin_test "pre-push with existing file"
+(
+  set -e
+
+  reponame="$(basename "$0" ".sh")-existing-file"
+  setup_remote_repo "$reponame"
+
+  clone_repo "$reponame" dry-run2
+  echo "existing" > existing.dat
+  git add existing.dat
+  git commit -m "add existing dat"
+
+  git lfs track "*.dat"
+  echo "new" > new.dat
+  git add new.dat
+  git add .gitattributes
+  git commit -m "add new file through git lfs"
+
+  # push file to the git lfs server
+  echo "refs/heads/master master refs/heads/master 0000000000000000000000000000000000000000" |
+    git lfs pre-push origin "$GITSERVER/$reponame" 2>&1 |
+    tee push.log
+  grep "(1 of 1 files)" push.log
+
+  # now the file exists
+  curl -v "$GITSERVER/$reponame.git/info/lfs/objects/7aa7a5359173d05b63cfd682e3c38487f3cb4f7f1d60659fe59fab1505977d4c" \
+    -u "user:pass" \
+    -o lfs.json \
+    -H "Accept: application/vnd.git-lfs+json" 2>&1 |
+    tee http.log
+  grep "200 OK" http.log
+
+  grep "download" lfs.json || {
+    cat lfs.json
+    exit 1
+  }
 )
 end_test
