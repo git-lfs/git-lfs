7df8db2946065b8587e482b15485365508839b7a
Steve Streeting <steve@stevestreeting.com>
2015-07-28 10:18:42 +0100

    Externalise goroutine and wait from checkoutWithChan

--- commands/command_checkout.go
+++ commands/command_checkout.go
@@ -43,7 +43,10 @@ func checkoutWithIncludeExclude(include []string, exclude []string) {
 
 	c := make(chan *lfs.WrappedPointer)
 
-	checkoutWithChan(c, &wait)
+	go func() {
+		checkoutWithChan(c)
+		wait.Done()
+	}()
 	for _, pointer := range pointers {
 		if lfs.FilenamePassesIncludeExcludeFilter(pointer.Name, include, exclude) {
 			c <- pointer
@@ -62,57 +65,52 @@ func checkoutAll() {
 // Populate the working copy with the real content of objects where the file is
 // either missing, or contains a matching pointer placeholder, from a list of pointers.
 // If the file exists but has other content it is left alone
-// returns immediately but a goroutine listens on the in channel for objects
-// calls wait.Done() when the final item after the channel is closed is done
-func checkoutWithChan(in <-chan *lfs.WrappedPointer, wait *sync.WaitGroup) {
-	go func() {
-		defer wait.Done()
-		// Fire up the update-index command
-		cmd := exec.Command("git", "update-index", "-q", "--refresh", "--stdin")
-		updateIdxStdin, err := cmd.StdinPipe()
-		if err != nil {
-			Panic(err, "Could not update the index")
-		}
+func checkoutWithChan(in <-chan *lfs.WrappedPointer) {
+	// Fire up the update-index command
+	cmd := exec.Command("git", "update-index", "-q", "--refresh", "--stdin")
+	updateIdxStdin, err := cmd.StdinPipe()
+	if err != nil {
+		Panic(err, "Could not update the index")
+	}
 
-		if err := cmd.Start(); err != nil {
-			Panic(err, "Could not update the index")
-		}
+	if err := cmd.Start(); err != nil {
+		Panic(err, "Could not update the index")
+	}
 
-		// As files come in, write them to the wd and update the index
-		for pointer := range in {
+	// As files come in, write them to the wd and update the index
+	for pointer := range in {
 
-			// Check the content - either missing or still this pointer (not exist is ok)
-			filepointer, err := lfs.DecodePointerFromFile(pointer.Name)
-			if err != nil && !os.IsNotExist(err) {
-				if err == lfs.NotAPointerError {
-					// File has non-pointer content, leave it alone
-					continue
-				}
-				Panic(err, "Problem accessing %v", pointer.Name)
-			}
-			if filepointer != nil && filepointer.Oid != pointer.Oid {
-				// User has probably manually reset a file to another commit
-				// while leaving it a pointer; don't mess with this
+		// Check the content - either missing or still this pointer (not exist is ok)
+		filepointer, err := lfs.DecodePointerFromFile(pointer.Name)
+		if err != nil && !os.IsNotExist(err) {
+			if err == lfs.NotAPointerError {
+				// File has non-pointer content, leave it alone
 				continue
 			}
-			// OK now we can (over)write the file content
-			file, err := os.Create(pointer.Name)
-			if err != nil {
-				Panic(err, "Could not create working directory file")
-			}
-
-			if err := lfs.PointerSmudge(file, pointer.Pointer, pointer.Name, nil); err != nil {
-				Panic(err, "Could not write working directory file")
-			}
-			file.Close()
-
-			updateIdxStdin.Write([]byte(pointer.Name + "\n"))
+			Panic(err, "Problem accessing %v", pointer.Name)
+		}
+		if filepointer != nil && filepointer.Oid != pointer.Oid {
+			// User has probably manually reset a file to another commit
+			// while leaving it a pointer; don't mess with this
+			continue
+		}
+		// OK now we can (over)write the file content
+		file, err := os.Create(pointer.Name)
+		if err != nil {
+			Panic(err, "Could not create working directory file")
 		}
 
-		updateIdxStdin.Close()
-		if err := cmd.Wait(); err != nil {
-			Panic(err, "Error updating the git index")
+		if err := lfs.PointerSmudge(file, pointer.Pointer, pointer.Name, nil); err != nil {
+			Panic(err, "Could not write working directory file")
 		}
-	}()
+		file.Close()
+
+		updateIdxStdin.Write([]byte(pointer.Name + "\n"))
+	}
+
+	updateIdxStdin.Close()
+	if err := cmd.Wait(); err != nil {
+		Panic(err, "Error updating the git index")
+	}
 
 }
