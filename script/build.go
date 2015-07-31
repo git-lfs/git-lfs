46c39cea9a41488947474666546e3f00401eb1a1
Scott Barron <scott@barron.io>
2015-07-31 11:02:09 -0400

    Backport allow-no-commit from #549 to release-0.5

--- script/build.go
+++ script/build.go
@@ -35,10 +35,7 @@ func mainBuild() {
 		return
 	}
 
-	cmd, err := exec.Command("git", "rev-parse", "--short", "HEAD").Output()
-	if err != nil {
-		panic(err)
-	}
+	cmd, _ := exec.Command("git", "rev-parse", "--short", "HEAD").Output()
 
 	if len(cmd) > 0 {
 		LdFlag = strings.TrimSpace("-X github.com/github/git-lfs/lfs.GitCommit " + string(cmd))
