4f93045c3767dd032cb6bc3f60d91c9394fa84dc
Steve Streeting <steve@stevestreeting.com>
2015-07-31 16:50:50 +0100

    Simplify skipping of worktree tests when git < 2.5.0

--- test/test-worktree.sh
+++ test/test-worktree.sh
@@ -4,21 +4,24 @@
 
 # git 2.5 or higher only
 compare_version $(git version | cut -d" " -f3) "2.5.0"
-if [[ $? != $VERSION_LOWER ]]; then
-  begin_test "git worktree"
-  (
-      set -e
-      reponame="worktree-main"
-      mkdir $reponame
-      cd $reponame
-      git init
+if [[ $? == $VERSION_LOWER ]]; then
+  echo "Skipping git worktree tests since git version < 2.5.0"
+fi
 
-      # can't create a worktree until there's 1 commit at least
-      echo "a" > tmp.txt
-      git add tmp.txt
-      git commit -m "Initial commit"
+begin_test "git worktree"
+(
+    set -e
+    reponame="worktree-main"
+    mkdir $reponame
+    cd $reponame
+    git init
 
-      expected=$(printf "%s\n%s\n
+    # can't create a worktree until there's 1 commit at least
+    echo "a" > tmp.txt
+    git add tmp.txt
+    git commit -m "Initial commit"
+
+    expected=$(printf "%s\n%s\n
 LocalWorkingDir=$TRASHDIR/$reponame
 LocalGitDir=$TRASHDIR/$reponame/.git
 LocalGitStorageDir=$TRASHDIR/$reponame/.git
@@ -28,17 +31,17 @@ ConcurrentTransfers=3
 BatchTransfer=false
 $(env | grep "^GIT")
 " "$(git lfs version)" "$(git version)")
-      actual=$(git lfs env)
-      [ "$expected" = "$actual" ]
+    actual=$(git lfs env)
+    [ "$expected" = "$actual" ]
 
-      worktreename="worktree-2"
-      git worktree add "$TRASHDIR/$worktreename"
-      cd "$TRASHDIR/$worktreename"
+    worktreename="worktree-2"
+    git worktree add "$TRASHDIR/$worktreename"
+    cd "$TRASHDIR/$worktreename"
 
-      # git dir in worktree is like submodules (except path is worktrees) but this
-      # is only for index, temp etc
-      # storage of git objects and lfs objects is in the original .git
-      expected=$(printf "%s\n%s\n
+    # git dir in worktree is like submodules (except path is worktrees) but this
+    # is only for index, temp etc
+    # storage of git objects and lfs objects is in the original .git
+    expected=$(printf "%s\n%s\n
 LocalWorkingDir=$TRASHDIR/$worktreename
 LocalGitDir=$TRASHDIR/$reponame/.git/worktrees/$worktreename
 LocalGitStorageDir=$TRASHDIR/$reponame/.git
@@ -48,8 +51,7 @@ ConcurrentTransfers=3
 BatchTransfer=false
 $(env | grep "^GIT")
 " "$(git lfs version)" "$(git version)")
-      actual=$(git lfs env)
-      [ "$expected" = "$actual" ]
-  )
-  end_test
-fi
+    actual=$(git lfs env)
+    [ "$expected" = "$actual" ]
+)
+end_test
