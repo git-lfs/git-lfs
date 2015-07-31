7161afeb6ccf6246b9203187e0cec2ef5c3a6fff
Steve Streeting <steve@stevestreeting.com>
2015-07-31 14:45:42 +0100

    Add git worktree tests when git version >= 2.5.0

--- test/testhelpers.sh
+++ test/testhelpers.sh
@@ -208,3 +208,38 @@ shutdown() {
     [ -z "$KEEPTRASH" ] && rm -rf "$REMOTEDIR"
   fi
 }
+
+VERSION_EQUAL=0
+VERSION_HIGHER=1
+VERSION_LOWER=2
+# Compare $1 and $2 and return VERSION_EQUAL / VERSION_LOWER / VERSION_HIGHER
+compare_version() {
+    if [[ $1 == $2 ]]
+    then
+        return $VERSION_EQUAL
+    fi
+    local IFS=.
+    local i ver1=($1) ver2=($2)
+    # fill empty fields in ver1 with zeros
+    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
+    do
+        ver1[i]=0
+    done
+    for ((i=0; i<${#ver1[@]}; i++))
+    do
+        if [[ -z ${ver2[i]} ]]
+        then
+            # fill empty fields in ver2 with zeros
+            ver2[i]=0
+        fi
+        if ((10#${ver1[i]} > 10#${ver2[i]}))
+        then
+            return $VERSION_HIGHER
+        fi
+        if ((10#${ver1[i]} < 10#${ver2[i]}))
+        then
+            return $VERSION_LOWER
+        fi
+    done
+    return $VERSION_EQUAL
+}
