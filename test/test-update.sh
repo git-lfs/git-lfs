87b557d75b0344b5819a306e709de4690f1f1b65
Rick Olson <technoweenie@gmail.com>
2015-08-13 17:12:17 -0600

    teach the upgrade command how to update pre-push hooks

--- test/test-update.sh
+++ test/test-update.sh
@@ -7,36 +7,50 @@ begin_test "update"
   set -e
 
   pre_push_hook="#!/bin/sh
-command -v git-lfs >/dev/null 2>&1 || { echo >&2 \"\\nThis repository has been set up with Git LFS but Git LFS is not installed.\\n\"; exit 2; }
+command -v git-lfs >/dev/null 2>&1 || { echo >&2 \"\\nThis repository has been set up with Git LFS but Git LFS is not installed.\\nDelete \$0 if this repository no longer uses Git LFS.\\n\"; exit 2; }
 git lfs pre-push \"\$@\""
 
   mkdir without-pre-push
   cd without-pre-push
   git init
 
-  [ "Updated pre-push hook" = "$(git lfs update)" ]
+  [ "Updated pre-push hook." = "$(git lfs update)" ]
   [ "$pre_push_hook" = "$(cat .git/hooks/pre-push)" ]
 
   # run it again
-  [ "Updated pre-push hook" = "$(git lfs update)" ]
+  [ "Updated pre-push hook." = "$(git lfs update)" ]
   [ "$pre_push_hook" = "$(cat .git/hooks/pre-push)" ]
 
   # replace old hook 1
   echo "#!/bin/sh
 git lfs push --stdin \$*" > .git/hooks/pre-push
-  [ "Updated pre-push hook" = "$(git lfs update)" ]
+  [ "Updated pre-push hook." = "$(git lfs update)" ]
   [ "$pre_push_hook" = "$(cat .git/hooks/pre-push)" ]
 
   # replace old hook 2
   echo "#!/bin/sh
 git lfs push --stdin \"\$@\"" > .git/hooks/pre-push
-  [ "Updated pre-push hook" = "$(git lfs update)" ]
+  [ "Updated pre-push hook." = "$(git lfs update)" ]
   [ "$pre_push_hook" = "$(cat .git/hooks/pre-push)" ]
 
   # replace old hook 3
   echo "#!/bin/sh
 git lfs pre-push \"\$@\"" > .git/hooks/pre-push
-  [ "Updated pre-push hook" = "$(git lfs update)" ]
+  [ "Updated pre-push hook." = "$(git lfs update)" ]
+  [ "$pre_push_hook" = "$(cat .git/hooks/pre-push)" ]
+
+  # replace old hook 4
+  echo "#!/bin/sh
+command -v git-lfs >/dev/null 2>&1 || { echo >&2 \"\\nThis repository has been set up with Git LFS but Git LFS is not installed.\\n\"; exit 0; }
+git lfs pre-push \"$@\""
+  [ "Updated pre-push hook." = "$(git lfs update)" ]
+  [ "$pre_push_hook" = "$(cat .git/hooks/pre-push)" ]
+
+  # replace old hook 5
+  echo "#!/bin/sh
+command -v git-lfs >/dev/null 2>&1 || { echo >&2 \"\\nThis repository has been set up with Git LFS but Git LFS is not installed.\\n\"; exit 2; }
+git lfs pre-push \"$@\""
+  [ "Updated pre-push hook." = "$(git lfs update)" ]
   [ "$pre_push_hook" = "$(cat .git/hooks/pre-push)" ]
 
   # don't replace unexpected hook
@@ -51,7 +65,7 @@ Run \`git lfs update --force\` to overwrite this hook."
   [ "test" = "$(cat .git/hooks/pre-push)" ]
 
   # force replace unexpected hook
-  [ "Updated pre-push hook" = "$(git lfs update --force)" ]
+  [ "Updated pre-push hook." = "$(git lfs update --force)" ]
   [ "$pre_push_hook" = "$(cat .git/hooks/pre-push)" ]
 )
 end_test
