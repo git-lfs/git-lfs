5b7990ce92ae32c815bd5b6927e90ef0f897a52e
Rick Olson <technoweenie@gmail.com>
2015-08-13 17:13:00 -0600

    teach the init command how to update pre-push hooks with force
    This reuses the update command completely.

--- test/test-init.sh
+++ test/test-init.sh
@@ -41,3 +41,47 @@ begin_test "init with old settings"
   [ "git-lfs clean %f" = "$(git config filter.lfs.clean)" ]
 )
 end_test
+
+begin_test "init updates repo hooks"
+(
+  set -e
+
+  mkdir init-repo-hooks
+  cd init-repo-hooks
+  git init
+
+  pre_push_hook="#!/bin/sh
+command -v git-lfs >/dev/null 2>&1 || { echo >&2 \"\\nThis repository has been set up with Git LFS but Git LFS is not installed.\\nDelete \$0 if this repository no longer uses Git LFS.\\n\"; exit 2; }
+git lfs pre-push \"\$@\""
+
+  [ "Updated pre-push hook.
+Git LFS initialized." = "$(git lfs init)" ]
+  [ "$pre_push_hook" = "$(cat .git/hooks/pre-push)" ]
+
+  # replace old hook
+  # more-comprehensive hook update tests are in test-update.sh
+  echo "#!/bin/sh
+git lfs push --stdin \$*" > .git/hooks/pre-push
+  [ "Updated pre-push hook.
+Git LFS initialized." = "$(git lfs init)" ]
+  [ "$pre_push_hook" = "$(cat .git/hooks/pre-push)" ]
+
+  # don't replace unexpected hook
+  expected="Hook already exists: pre-push
+
+test
+
+Run \`git lfs update --force\` to overwrite this hook.
+Git LFS initialized."
+
+  echo "test" > .git/hooks/pre-push
+  [ "test" = "$(cat .git/hooks/pre-push)" ]
+  [ "$expected" = "$(git lfs init 2>&1)" ]
+  [ "test" = "$(cat .git/hooks/pre-push)" ]
+
+  # force replace unexpected hook
+  [ "Updated pre-push hook.
+Git LFS initialized." = "$(git lfs init --force)" ]
+  [ "$pre_push_hook" = "$(cat .git/hooks/pre-push)" ]
+)
+end_test
