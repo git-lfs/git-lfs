f6d96803a322f2a0563726a7885c5ff8c357baa7
Rick Olson <technoweenie@gmail.com>
2015-08-14 09:59:16 -0600

    update pre-push to be more explicit about the path

--- test/test-init.sh
+++ test/test-init.sh
@@ -51,7 +51,7 @@ begin_test "init updates repo hooks"
   git init
 
   pre_push_hook="#!/bin/sh
-command -v git-lfs >/dev/null 2>&1 || { echo >&2 \"\\nThis repository has been set up with Git LFS but Git LFS is not installed.\\nDelete \$0 if this repository no longer uses Git LFS.\\n\"; exit 2; }
+command -v git-lfs >/dev/null 2>&1 || { echo >&2 \"\\nThis repository is configured for Git LFS but 'git-lfs' was not found on your path. If you no longer wish to use Git LFS, remove this hook by deleting .git/hooks/pre-push.\\n\"; exit 2; }
 git lfs pre-push \"\$@\""
 
   [ "Updated pre-push hook.
