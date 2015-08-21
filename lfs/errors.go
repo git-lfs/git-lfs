0c02eda8c94a81413e41ac5274f672c46adc7e41
rubyist <scott.barron@github.com>
2015-08-21 12:15:29 -0400

    add some docs

--- lfs/errors.go
+++ lfs/errors.go
@@ -1,49 +1,163 @@
 package lfs
 
+// The LFS error system provides a simple wrapper around Go errors and the
+// ability to inspect errors. It is strongly influenced by Dave Cheney's post
+// at http://dave.cheney.net/2014/12/24/inspecting-errors.
+//
+// When passing errors out of lfs package functions, the return type should
+// always be `error`. The wrappedError details are not exported. If an error is
+// the kind of error a caller should need to investigate, an IsXError()
+// function is provided that tells the caller if the error is of that type.
+// There should only be a handfull of cases where a simple `error` is
+// insufficient.
+//
+// The error behaviors can be nested when created. For example, the not
+// implemented error can also be marked as a fatal error:
+//
+//	func LfsFunction() error {
+//		err := functionCall()
+//		if err != nil {
+//			return newFatalError(newNotImplementedError(err))
+//		}
+//		return nil
+//	}
+//
+// Then in the caller:
+//
+//	err := lfs.LfsFunction()
+//	if lfs.IsNotImplementedError(err) {
+//		log.Print("feature not implemented")
+//	}
+//	if lfs.IsFatalError(err) {
+//		os.Exit(1)
+//	}
+
 import (
 	"errors"
 	"runtime"
 )
 
-type errorWrapper interface {
-	InnerError() error
-	Error() string
-}
-
-type wrappedError struct {
-	Message string
-	stack   []byte
-	context map[string]string
-	error
-}
-
-func newWrappedError(err error) errorWrapper {
+// IsFatalError indicates that the error is fatal and the process should exit
+// immediately after handling the error.
+func IsFatalError(err error) bool {
+	if e, ok := err.(interface {
+		Fatal() bool
+	}); ok {
+		return e.Fatal()
+	}
 	if e, ok := err.(errorWrapper); ok {
-		return e
+		return IsFatalError(e.InnerError())
 	}
+	return false
+}
 
+// IsNotImplementedError indicates the client attempted to use a feature the
+// server has not implemented (e.g. the batch endpoint).
+func IsNotImplementedError(err error) bool {
+	if e, ok := err.(interface {
+		NotImplemented() bool
+	}); ok {
+		return e.NotImplemented()
+	}
+	if e, ok := err.(errorWrapper); ok {
+		return IsFatalError(e.InnerError())
+	}
+	return false
+}
+
+// IsInvalidPointerError indicates an attempt to parse data that was not a
+// valid pointer.
+func IsInvalidPointerError(err error) bool {
+	if e, ok := err.(interface {
+		InvalidPointer() bool
+	}); ok {
+		return e.InvalidPointer()
+	}
+	if e, ok := err.(errorWrapper); ok {
+		return IsInvalidPointerError(e.InnerError())
+	}
+	return false
+}
+
+// IsInvalidRepoError indicates an operation was attempted from outside a git
+// repository.
+func IsInvalidRepoError(err error) bool {
+	if e, ok := err.(interface {
+		InvalidRepo() bool
+	}); ok {
+		return e.InvalidRepo()
+	}
+	if e, ok := err.(errorWrapper); ok {
+		return IsInvalidRepoError(e.InnerError())
+	}
+	return false
+}
+
+// IsSmudgeError indicates an error while smudging a files.
+func IsSmudgeError(err error) bool {
+	if e, ok := err.(interface {
+		SmudgeError() bool
+	}); ok {
+		return e.SmudgeError()
+	}
+	if e, ok := err.(errorWrapper); ok {
+		return IsSmudgeError(e.InnerError())
+	}
+	return false
+}
+
+// IsCleanPointerError indicates an error while cleaning a file. Because of the
+// structure of the code, this check also returns a *cleanPointerError because
+// that's how a Pointer and []byte were passed up to the caller. This is not
+// very clean and should be refactored. The returned *cleanPointerError MUST
+// NOT be accessed if the bool value is false.
+func IsCleanPointerError(err error) (*cleanPointerError, bool) {
+	if e, ok := err.(interface {
+		CleanPointerError() bool
+	}); ok {
+		cpe := err.(cleanPointerError)
+		return &cpe, e.CleanPointerError()
+	}
+	if e, ok := err.(errorWrapper); ok {
+		return IsCleanPointerError(e.InnerError())
+	}
+	return nil, false
+}
+
+// Error wraps an error with an empty message.
+func Error(err error) error {
+	return Errorf(err, "")
+}
+
+// Errorf wraps an error with an additional formatted message.
+func Errorf(err error, format string, args ...interface{}) error {
 	if err == nil {
-		err = errors.New("LFS Error")
+		return nil
 	}
 
-	return wrappedError{
-		Message: err.Error(),
-		stack:   Stack(),
-		context: make(map[string]string),
-		error:   err,
-	}
+	e := newWrappedError(err)
+
+	// ERRTODO this isn't right
+	/*
+		if len(format) > 0 {
+			we := e.(wrappedError)
+			we.Message = fmt.Sprintf(format, args...)
+		}
+	*/
+
+	return e
 }
 
-func (e wrappedError) InnerError() error {
-	return e.error
-}
-
+// ErrorSetContext sets a value in the error's context. If the error has not
+// been wrapped, it does nothing.
 func ErrorSetContext(err error, key, value string) {
 	if e, ok := err.(wrappedError); ok {
 		e.context[key] = value
 	}
 }
 
+// ErrorGetContext gets a value from the error's context. If the error has not
+// been wrapped, it returns an empty string.
 func ErrorGetContext(err error, key string) string {
 	if e, ok := err.(wrappedError); ok {
 		return e.context[key]
@@ -51,6 +165,8 @@ func ErrorGetContext(err error, key string) string {
 	return ""
 }
 
+// ErrorDelContext removes a value from the error's context. If the error has
+// not been wrapped, it does nothing.
 func ErrorDelContext(err error, key string) {
 	if e, ok := err.(wrappedError); ok {
 		delete(e.context, key)
@@ -75,7 +191,46 @@ func ErrorContext(err error) map[string]string {
 	return nil
 }
 
-// fatalError indicates that the process should halt
+type errorWrapper interface {
+	InnerError() error
+	Error() string
+}
+
+// wrappedError is the base error wrapper. It provides a Message string, a
+// stack, and a context map around a regular Go error.
+type wrappedError struct {
+	Message string
+	stack   []byte
+	context map[string]string
+	error
+}
+
+// newWrappedError creates a wrappedError. If the error has already been
+// wrapped it is simply returned as is.
+func newWrappedError(err error) errorWrapper {
+	if e, ok := err.(errorWrapper); ok {
+		return e
+	}
+
+	if err == nil {
+		err = errors.New("LFS Error")
+	}
+
+	return wrappedError{
+		Message: err.Error(),
+		stack:   Stack(),
+		context: make(map[string]string),
+		error:   err,
+	}
+}
+
+// InnerError returns the underlying error. This could be a Go error or another wrappedError.
+func (e wrappedError) InnerError() error {
+	return e.error
+}
+
+// Definitions for IsFatalError()
+
 type fatalError struct {
 	errorWrapper
 }
@@ -92,21 +247,8 @@ func newFatalError(err error) error {
 	return fatalError{newWrappedError(err)}
 }
 
-func IsFatalError(err error) bool {
-	type fatalerror interface {
-		Fatal() bool
-	}
-	if e, ok := err.(fatalerror); ok {
-		return e.Fatal()
-	}
-	if e, ok := err.(errorWrapper); ok {
-		return IsFatalError(e.InnerError())
-	}
-	return false
-}
+// Definitions for IsNotImplementedError()
 
-// notImplementedError indicates that a feature (e.g. batch) is not implemented
-// on the server.
 type notImplementedError struct {
 	errorWrapper
 }
@@ -123,20 +265,8 @@ func newNotImplementedError(err error) error {
 	return notImplementedError{newWrappedError(err)}
 }
 
-func IsNotImplementedError(err error) bool {
-	type notimplerror interface {
-		NotImplemented() bool
-	}
-	if e, ok := err.(notimplerror); ok {
-		return e.NotImplemented()
-	}
-	if e, ok := err.(errorWrapper); ok {
-		return IsFatalError(e.InnerError())
-	}
-	return false
-}
+// Definitions for IsInvalidPointerError()
 
-// invalidPointerError indicates the the pointer was invalid.
 type invalidPointerError struct {
 	errorWrapper
 }
@@ -153,20 +283,8 @@ func newInvalidPointerError(err error) error {
 	return invalidPointerError{newWrappedError(err)}
 }
 
-func IsInvalidPointerError(err error) bool {
-	type invalidptrerror interface {
-		InvalidPointer() bool
-	}
-	if e, ok := err.(invalidptrerror); ok {
-		return e.InvalidPointer()
-	}
-	if e, ok := err.(errorWrapper); ok {
-		return IsInvalidPointerError(e.InnerError())
-	}
-	return false
-}
+// Definitions for IsInvalidRepoError()
 
-// invalidRepo error indicates that we are not in a git repository.
 type invalidRepoError struct {
 	errorWrapper
 }
@@ -183,18 +301,7 @@ func newInvalidrepoError(err error) error {
 	return invalidRepoError{newWrappedError(err)}
 }
 
-func IsInvalidRepoError(err error) bool {
-	type invalidrepoerror interface {
-		InvalidRepo() bool
-	}
-	if e, ok := err.(invalidrepoerror); ok {
-		return e.InvalidRepo()
-	}
-	if e, ok := err.(errorWrapper); ok {
-		return IsInvalidRepoError(e.InnerError())
-	}
-	return false
-}
+// Definitions for IsSmudgeError()
 
 type smudgeError struct {
 	errorWrapper
@@ -215,18 +322,7 @@ func newSmudgeError(err error, oid, filename string) error {
 	return e
 }
 
-func IsSmudgeError(err error) bool {
-	type smudgeerror interface {
-		SmudgeError() bool
-	}
-	if e, ok := err.(smudgeerror); ok {
-		return e.SmudgeError()
-	}
-	if e, ok := err.(errorWrapper); ok {
-		return IsSmudgeError(e.InnerError())
-	}
-	return false
-}
+// Definitions for IsCleanPointerError()
 
 type cleanPointerError struct {
 	pointer *Pointer
@@ -258,42 +354,7 @@ func newCleanPointerError(err error, pointer *Pointer, bytes []byte) error {
 	}
 }
 
-func IsCleanPointerError(err error) (*cleanPointerError, bool) {
-	type cleanptrerror interface {
-		CleanPointerError() bool
-	}
-	if e, ok := err.(cleanptrerror); ok {
-		cpe := err.(cleanPointerError)
-		return &cpe, e.CleanPointerError()
-	}
-	if e, ok := err.(errorWrapper); ok {
-		return IsCleanPointerError(e.InnerError())
-	}
-	return nil, false
-}
-
-func Error(err error) error {
-	return Errorf(err, "")
-}
-
-func Errorf(err error, format string, args ...interface{}) error {
-	if err == nil {
-		return nil
-	}
-
-	e := newWrappedError(err)
-
-	// ERRTODO this isn't right
-	/*
-		if len(format) > 0 {
-			we := e.(wrappedError)
-			we.Message = fmt.Sprintf(format, args...)
-		}
-	*/
-
-	return e
-}
-
+// Stack returns a byte slice containing the runtime.Stack()
 func Stack() []byte {
 	stackBuf := make([]byte, 1024*1024)
 	written := runtime.Stack(stackBuf, false)
