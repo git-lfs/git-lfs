6a2fdacca806311deb643de4247412325f4ceff7
Steve Streeting <steve@stevestreeting.com>
2015-09-09 15:16:30 +0100

    Make sure data in AddCommits really is uniquely seeded for all files
    Previously because of the multiply either being 0 would result in the
    same seed

--- test/testutils.go
+++ test/testutils.go
@@ -286,7 +286,7 @@ func (repo *Repo) AddCommits(inputs []*CommitInput) []*CommitOutput {
 			}
 			if inputData == nil {
 				// Different data for each file but deterministic
-				inputData = NewPlaceholderDataReader(int64(i*fi), infile.Size)
+				inputData = NewPlaceholderDataReader(int64(i+1*fi+1), infile.Size)
 			}
 			cleaned, err := lfs.PointerClean(inputData, infile.Filename, infile.Size, nil)
 			if err != nil {
