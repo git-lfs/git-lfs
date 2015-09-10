3288c9d160159a7cc5240be1e20859044032fe88
Steve Streeting <steve@stevestreeting.com>
2015-09-10 16:59:19 +0100

    Skeleton of algorithm & main goroutines

--- commands/command_prune.go
+++ commands/command_prune.go
@@ -1,6 +1,8 @@
 package commands
 
 import (
+	"sync"
+
 	"github.com/github/git-lfs/lfs"
 	"github.com/github/git-lfs/vendor/_nuts/github.com/spf13/cobra"
 )
@@ -32,7 +34,12 @@ func pruneCommand(cmd *cobra.Command, args []string) {
 }
 
 func prune(verifyRemote, dryRun, verbose bool) {
-	// TODO
+	localObjects := make([]string, 0, 100)
+	retainedObjects := lfs.NewStringSetWithCapacity(100)
+	var reachableObjects lfs.StringSet
+	var taskwait sync.WaitGroup
+
+	// Data collection algorithm summary
 	// Separate goroutines must find:
 	//  1. Local objects
 	//  2. Current checkout, 2 sub-goroutines
@@ -48,11 +55,46 @@ func prune(verifyRemote, dryRun, verbose bool) {
 	// This main routine will collate the outputs of chans, report progress
 	// with spinner of # objects stored, # objects referenced
 
-	// storedObjects:array = 1
+	// localObjects:array = 1
 	// retainedObjects:set = 2..5
 	// [if --verify-remote] reachableObjects:set = 6
 
-	// prunableObjects:set = (storedObjects - retainedObjects)
+	// Add all the base funcs to the waitgroup before starting them, in case
+	// one completes really fast & hits 0 unexpectedly
+	// each main process can Add() to the wg itself if it subdivides the task
+	taskwait.Add(5) // 1..5: localObjects, current checkout, recent refs, unpushed, worktree
+	if verifyRemote {
+		taskwait.Add(1) // 6
+	}
+
+	// Populate the single list of local objects
+	go pruneTaskGetLocalObjects(&localObjects, &taskwait)
+
+	// Now find files to be retained from many sources
+	retainChan := make(chan string, 100)
+
+	go pruneTaskGetRetainedCurrentCheckout(retainChan, &taskwait)
+	go pruneTaskGetRetainedRecentRefs(retainChan, &taskwait)
+	go pruneTaskGetRetainedUnpushed(retainChan, &taskwait)
+	go pruneTaskGetRetainedWorktree(retainChan, &taskwait)
+	if verifyRemote {
+		reachableObjects = lfs.NewStringSetWithCapacity(100)
+		go pruneTaskGetReachableObjects(&reachableObjects, &taskwait)
+	}
+
+	// Now collect all the retained objects
+	var retainwait sync.WaitGroup
+	retainwait.Add(1)
+	go func() {
+		for oid := range retainChan {
+			retainedObjects.Add(oid)
+		}
+	}()
+	taskwait.Wait()   // wait for subtasks
+	close(retainChan) // triggers retain collector to end now all tasks have
+	retainwait.Wait() // make sure all retained objects added
+
+	// prunableObjects:set = (localObjects - retainedObjects)
 	// [if --verify-remote] verifyObjects:set = (prunableObjects n reachableObjects)
 	//    (this is so we don't try to verify unreachable objects on remote)
 	// [if --verify-remote] call remote, remove unverified objects from prunableObjects & report
@@ -61,6 +103,51 @@ func prune(verifyRemote, dryRun, verbose bool) {
 	// [if !dry-run] delete prunableObjects
 	// [if verbose] report oids & individual sizes
 
+}
+
+// Background task, must call waitg.Done() once at end
+func pruneTaskGetLocalObjects(outLocalObjects *[]string, waitg *sync.WaitGroup) {
+	defer waitg.Done()
+
+	localObjectsChan := lfs.AllLocalObjectsChan()
+	for oid := range localObjectsChan {
+		*outLocalObjects = append(*outLocalObjects, oid)
+	}
+}
+
+// Background task, must call waitg.Done() once at end
+func pruneTaskGetRetainedCurrentCheckout(retainChan chan string, waitg *sync.WaitGroup) {
+	defer waitg.Done()
+
+	// TODO
+}
+
+// Background task, must call waitg.Done() once at end
+func pruneTaskGetRetainedRecentRefs(retainChan chan string, waitg *sync.WaitGroup) {
+	defer waitg.Done()
+
+	// TODO
+}
+
+// Background task, must call waitg.Done() once at end
+func pruneTaskGetRetainedUnpushed(retainChan chan string, waitg *sync.WaitGroup) {
+	defer waitg.Done()
+
+	// TODO
+}
+
+// Background task, must call waitg.Done() once at end
+func pruneTaskGetRetainedWorktree(retainChan chan string, waitg *sync.WaitGroup) {
+	defer waitg.Done()
+
+	// TODO
+}
+
+// Background task, must call waitg.Done() once at end
+func pruneTaskGetReachableObjects(outObjectSet *lfs.StringSet, waitg *sync.WaitGroup) {
+	defer waitg.Done()
+
+	// TODO
 }
 
 func init() {
