b2311604967bc712768590bfe677e8920c803629
Rick Olson <technoweenie@gmail.com>
2015-09-30 15:50:59 -0700

    push GIT_TERMINAL_PROMPT tests to separate file so they run only on git 2.3+

--- test/test-push-legacy-failures.sh
+++ test/test-push-legacy-failures.sh
@@ -2,36 +2,6 @@
 
 . "test/testlib.sh"
 
-begin_test "push (legacy): upload to bad dns"
-(
-  set -e
-
-  reponame="$(basename "$0" ".sh")-bad-dns"
-  setup_remote_repo "$reponame"
-  clone_repo "$reponame" "$reponame"
-
-  git lfs track "*.dat"
-  printf "hi" > good.dat
-  git add .gitattributes good.dat
-  git commit -m "welp"
-
-  port="$(echo "http://127.0.0.1:63378" | cut -f 3 -d ":")"
-  git config lfs.batch false
-  git config lfs.url "http://git-lfs-bad-dns:$port"
-
-  set +e
-  GIT_TERMINAL_PROMPT=0 git push origin master
-  res="$?"
-  set -e
-
-  refute_server_object "$reponame" "$(calc_oid "hi")"
-  if [ "$res" = "0" ]; then
-    echo "push successful?"
-    exit 1
-  fi
-)
-end_test
-
 push_legacy_fail_test() {
   local contents="$1"
 
