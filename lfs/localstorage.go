029418546d7b6210a84e4510cec18fac8b5d7613
risk danger olson <technoweenie@gmail.com>
2015-10-21 15:49:46 -0600

    remove temp files if they dont match the '{oid}-{rand}' pattern

--- lfs/localstorage.go
+++ lfs/localstorage.go
@@ -26,12 +26,13 @@ func shouldDeleteTempObject(path string, info os.FileInfo) bool {
 
 	base := filepath.Base(path)
 	parts := strings.SplitN(base, "-", 2)
-	if len(parts) < 2 {
+	oid := parts[0]
+	if len(parts) < 2 || len(oid) != 64 {
 		tracerx.Printf("Removing invalid tmp object file: %s", path)
 		return true
 	}
 
-	if FileExists(localMediaPathNoCreate(parts[0])) {
+	if FileExists(localMediaPathNoCreate(oid)) {
 		tracerx.Printf("Removing existing tmp object file: %s", path)
 		return true
 	}
