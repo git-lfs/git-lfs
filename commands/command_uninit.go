8089b9e549c14d5f64931ea776610fc1e445751a
risk danger olson <technoweenie@gmail.com>
2015-11-16 13:39:58 -0700

    add 2.0 deprecation todos

--- commands/command_uninit.go
+++ commands/command_uninit.go
@@ -7,6 +7,8 @@ import (
 	"github.com/github/git-lfs/vendor/_nuts/github.com/spf13/cobra"
 )
 
+// TODO: Remove for Git LFS v2.0
+
 var (
 	// uninitCmd removes any configuration and hooks set by Git LFS.
 	uninitCmd = &cobra.Command{
