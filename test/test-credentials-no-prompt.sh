e232b3df7711d1850278fbc573fc1cd7ce1640f6
risk danger olson <technoweenie@gmail.com>
2016-02-23 13:46:51 -0700

    update test so it passes regardless of git version or batch api
    changed in a4771400b29ef8356bc60c47f8f2dab26b217903

--- test/test-credentials-no-prompt.sh
+++ test/test-credentials-no-prompt.sh
@@ -5,6 +5,14 @@
 # these tests rely on GIT_TERMINAL_PROMPT to test properly
 ensure_git_version_isnt $VERSION_LOWER "2.3.0"
 
+# if there is a system cred helper we can't run this test
+# can't disable without changing state outside test & probably don't have permission
+# this is common on OS X with certain versions of Git installed, default cred helper
+if [[ "$(git config --system credential.helper)" -ne "" ]]; then
+  echo "skip: $0 (system cred helper we can't disable)"
+  exit
+fi
+
 begin_test "attempt private access without credential helper"
 (
   set -e
@@ -23,7 +31,7 @@ begin_test "attempt private access without credential helper"
   git config --global --unset credential.helper
 
   GIT_TERMINAL_PROMPT=0 git push origin master 2>&1 | tee push.log
-
-  grep "Authorization error: $GITSERVER/$reponame" push.log
+  grep "Authorization error: $GITSERVER/$reponame" push.log ||
+    grep "Git credentials for $GITSERVER/$reponame not found" push.log
 )
 end_test
