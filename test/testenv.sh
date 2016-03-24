cc41ca541361372323ccafd9d4f14797151c421c
Steve Streeting <steve@stevestreeting.com>
2016-03-24 17:41:42 +0000

    Fix auto tempdir for GNU mktemp, needs X's (BSD doesn't)

--- test/testenv.sh
+++ test/testenv.sh
@@ -54,7 +54,7 @@ BINPATH="$ROOTDIR/bin"
 PATH="$BINPATH:$PATH"
 
 # Always provide a test dir outside our git repo if not specified
-TEMPDIR_PREFIX="git-lfs_TEMP"
+TEMPDIR_PREFIX="git-lfs_TEMP.XXXXXX"
 if [ -z "$GIT_LFS_TEST_DIR" ]; then
     GIT_LFS_TEST_DIR=$(mktemp -d -t "$TEMPDIR_PREFIX")
     GIT_LFS_TEST_DIR=$(resolve_symlink $GIT_LFS_TEST_DIR)
