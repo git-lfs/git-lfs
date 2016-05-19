f7fc82387f04e4aa101ad4d4e6457b06067d4df5
Taylor Blau <me@ttaylorr.com>
2016-05-19 09:52:29 -0600

    api/schema: remove MethodTestCase in favor of AssertSchema()
    MethodTestCase had lots of problems, most caused by a good degree of ambiguity
    in what we were actually testing.
    
    AssertSchema(), on the other hand, simplifies this problem greatly by correctly
    identifying the responsibilities of an *api.RequestSchema. An
    *api.RequestSchema is responsible for one thing, and one thing only: to provide
    a template for requests to be made against the API, completely regardless of
    protocol or implementation.
    
    Most notably, these responsibilities do not include:
     - Preforming an HTTP request, or
     - reading the response of an http request
    
    So MethodTestCase gets simplified to a simple assert.Equal(). We expect to get
    some *api.RequestSchema out, and we expect it to have certain parameters. Now,
    all of the API's Service tests are rather simple: create a schema, compare,
    rinse and repeat.
    
    The only thing left to do is test the schema of the request and response
    payload. This is what I had tried to do with the original MethodTestCase
    implementation, but the way the system is desinged, trying to accomplish this
    involves integrating with too many unrelated parts. To address this, I plan to
    introduce a JSON Schema implementation that compares generated request/reponse
    payloads with their ideal types.

--- api/lock_api_test.go
+++ api/lock_api_test.go
@@ -3,52 +3,19 @@ package api_test
 import (
 	"net/http"
 	"testing"
-	"time"
 
 	"github.com/github/git-lfs/api"
-	"github.com/stretchr/testify/assert"
 )
 
 var LockService api.LockService
 
 func TestSuccessfullyObtainingALock(t *testing.T) {
-	now := time.Now()
+	got, body := LockService.Lock(new(api.LockRequest))
 
-	schema, resp := LockService.Lock(&api.LockRequest{
-		Path:               "/path/to/file",
-		LatestRemoteCommit: "deadbeef",
-		Committer: api.Committer{
-			Name:  "Jane Doe",
-			Email: "jane@example.com",
-		},
-	})
-
-	tc := &MethodTestCase{
-		Schema:         schema,
-		ExpectedPath:   "/locks",
-		ExpectedMethod: http.MethodPost,
-		Resp:           resp,
-		Output: &api.LockResponse{
-			Lock: api.Lock{
-				Id:   "some-lock-id",
-				Path: "/path/to/file",
-				Committer: api.Committer{
-					Name:  "Jane Doe",
-					Email: "jane@example.com",
-				},
-				CommitSHA: "deadbeef",
-				LockedAt:  now,
-			},
-		},
-	}
-
-	tc.Assert(t)
-
-	assert.Nil(t, resp.Err)
-	assert.Equal(t, "", resp.CommitNeeded)
-	assert.Equal(t, "some-lock-id", resp.Lock.Id)
-	assert.Equal(t, "/path/to/file", resp.Lock.Path)
-	assert.Equal(t, api.Committer{"Jane Doe", "jane@example.com"}, resp.Lock.Committer)
-	assert.Equal(t, now, resp.Lock.LockedAt)
-	assert.True(t, resp.Lock.Active())
+	AssertSchema(t, &api.RequestSchema{
+		Method: http.MethodPost,
+		Path:   "/locks",
+		Body:   new(api.LockRequest),
+		Into:   body,
+	}, got)
 }
