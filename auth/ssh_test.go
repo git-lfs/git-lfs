7e46933b121e883efa735d03b6ba1274b1639371
Peter Fern <github@obfusc8.org>
2016-05-29 13:22:40 +1000

    Implement support for GIT_SSH_COMMAND
    Per the spec, `GIT_SSH_COMMAND` takes precedence over `GIT_SSH`.
    
    Fixes #1142

--- auth/ssh_test.go
+++ auth/ssh_test.go
@@ -11,6 +11,8 @@ import (
 func TestSSHGetExeAndArgsSsh(t *testing.T) {
 	endpoint := config.Config.Endpoint("download")
 	endpoint.SshUserAndHost = "user@foo.com"
+	oldGITSSHCommand := config.Config.Getenv("GIT_SSH_COMMAND")
+	config.Config.Setenv("GIT_SSH_COMMAND", "")
 	oldGITSSH := config.Config.Getenv("GIT_SSH")
 	config.Config.Setenv("GIT_SSH", "")
 	exe, args := sshGetExeAndArgs(endpoint)
@@ -18,12 +20,15 @@ func TestSSHGetExeAndArgsSsh(t *testing.T) {
 	assert.Equal(t, []string{"user@foo.com"}, args)
 
 	config.Config.Setenv("GIT_SSH", oldGITSSH)
+	config.Config.Setenv("GIT_SSH_COMMAND", oldGITSSHCommand)
 }
 
 func TestSSHGetExeAndArgsSshCustomPort(t *testing.T) {
 	endpoint := config.Config.Endpoint("download")
 	endpoint.SshUserAndHost = "user@foo.com"
 	endpoint.SshPort = "8888"
+	oldGITSSHCommand := config.Config.Getenv("GIT_SSH_COMMAND")
+	config.Config.Setenv("GIT_SSH_COMMAND", "")
 	oldGITSSH := config.Config.Getenv("GIT_SSH")
 	config.Config.Setenv("GIT_SSH", "")
 	exe, args := sshGetExeAndArgs(endpoint)
@@ -31,11 +36,14 @@ func TestSSHGetExeAndArgsSshCustomPort(t *testing.T) {
 	assert.Equal(t, []string{"-p", "8888", "user@foo.com"}, args)
 
 	config.Config.Setenv("GIT_SSH", oldGITSSH)
+	config.Config.Setenv("GIT_SSH_COMMAND", oldGITSSHCommand)
 }
 
 func TestSSHGetExeAndArgsPlink(t *testing.T) {
 	endpoint := config.Config.Endpoint("download")
 	endpoint.SshUserAndHost = "user@foo.com"
+	oldGITSSHCommand := config.Config.Getenv("GIT_SSH_COMMAND")
+	config.Config.Setenv("GIT_SSH_COMMAND", "")
 	oldGITSSH := config.Config.Getenv("GIT_SSH")
 	// this will run on non-Windows platforms too but no biggie
 	plink := filepath.Join("Users", "joebloggs", "bin", "plink.exe")
@@ -45,12 +53,15 @@ func TestSSHGetExeAndArgsPlink(t *testing.T) {
 	assert.Equal(t, []string{"user@foo.com"}, args)
 
 	config.Config.Setenv("GIT_SSH", oldGITSSH)
+	config.Config.Setenv("GIT_SSH_COMMAND", oldGITSSHCommand)
 }
 
 func TestSSHGetExeAndArgsPlinkCustomPort(t *testing.T) {
 	endpoint := config.Config.Endpoint("download")
 	endpoint.SshUserAndHost = "user@foo.com"
 	endpoint.SshPort = "8888"
+	oldGITSSHCommand := config.Config.Getenv("GIT_SSH_COMMAND")
+	config.Config.Setenv("GIT_SSH_COMMAND", "")
 	oldGITSSH := config.Config.Getenv("GIT_SSH")
 	// this will run on non-Windows platforms too but no biggie
 	plink := filepath.Join("Users", "joebloggs", "bin", "plink")
@@ -60,11 +71,14 @@ func TestSSHGetExeAndArgsPlinkCustomPort(t *testing.T) {
 	assert.Equal(t, []string{"-P", "8888", "user@foo.com"}, args)
 
 	config.Config.Setenv("GIT_SSH", oldGITSSH)
+	config.Config.Setenv("GIT_SSH_COMMAND", oldGITSSHCommand)
 }
 
 func TestSSHGetExeAndArgsTortoisePlink(t *testing.T) {
 	endpoint := config.Config.Endpoint("download")
 	endpoint.SshUserAndHost = "user@foo.com"
+	oldGITSSHCommand := config.Config.Getenv("GIT_SSH_COMMAND")
+	config.Config.Setenv("GIT_SSH_COMMAND", "")
 	oldGITSSH := config.Config.Getenv("GIT_SSH")
 	// this will run on non-Windows platforms too but no biggie
 	plink := filepath.Join("Users", "joebloggs", "bin", "tortoiseplink.exe")
@@ -74,12 +88,15 @@ func TestSSHGetExeAndArgsTortoisePlink(t *testing.T) {
 	assert.Equal(t, []string{"-batch", "user@foo.com"}, args)
 
 	config.Config.Setenv("GIT_SSH", oldGITSSH)
+	config.Config.Setenv("GIT_SSH_COMMAND", oldGITSSHCommand)
 }
 
 func TestSSHGetExeAndArgsTortoisePlinkCustomPort(t *testing.T) {
 	endpoint := config.Config.Endpoint("download")
 	endpoint.SshUserAndHost = "user@foo.com"
 	endpoint.SshPort = "8888"
+	oldGITSSHCommand := config.Config.Getenv("GIT_SSH_COMMAND")
+	config.Config.Setenv("GIT_SSH_COMMAND", "")
 	oldGITSSH := config.Config.Getenv("GIT_SSH")
 	// this will run on non-Windows platforms too but no biggie
 	plink := filepath.Join("Users", "joebloggs", "bin", "tortoiseplink")
@@ -89,4 +106,103 @@ func TestSSHGetExeAndArgsTortoisePlinkCustomPort(t *testing.T) {
 	assert.Equal(t, []string{"-batch", "-P", "8888", "user@foo.com"}, args)
 
 	config.Config.Setenv("GIT_SSH", oldGITSSH)
+	config.Config.Setenv("GIT_SSH_COMMAND", oldGITSSHCommand)
+}
+
+func TestSSHGetExeAndArgsSshCommandPrecedence(t *testing.T) {
+	endpoint := config.Config.Endpoint("download")
+	endpoint.SshUserAndHost = "user@foo.com"
+	oldGITSSHCommand := config.Config.Getenv("GIT_SSH_COMMAND")
+	config.Config.Setenv("GIT_SSH_COMMAND", "sshcmd")
+	oldGITSSH := config.Config.Getenv("GIT_SSH")
+	config.Config.Setenv("GIT_SSH", "bad")
+	exe, args := sshGetExeAndArgs(endpoint)
+	assert.Equal(t, "sshcmd", exe)
+	assert.Equal(t, []string{"user@foo.com"}, args)
+
+	config.Config.Setenv("GIT_SSH", oldGITSSH)
+	config.Config.Setenv("GIT_SSH_COMMAND", oldGITSSHCommand)
+}
+
+func TestSSHGetExeAndArgsSshCommandArgs(t *testing.T) {
+	endpoint := config.Config.Endpoint("download")
+	endpoint.SshUserAndHost = "user@foo.com"
+	oldGITSSHCommand := config.Config.Getenv("GIT_SSH_COMMAND")
+	config.Config.Setenv("GIT_SSH_COMMAND", "sshcmd --args 1")
+	exe, args := sshGetExeAndArgs(endpoint)
+	assert.Equal(t, "sshcmd", exe)
+	assert.Equal(t, []string{"--args", "1", "user@foo.com"}, args)
+
+	config.Config.Setenv("GIT_SSH_COMMAND", oldGITSSHCommand)
+}
+
+func TestSSHGetExeAndArgsSshCommandCustomPort(t *testing.T) {
+	endpoint := config.Config.Endpoint("download")
+	endpoint.SshUserAndHost = "user@foo.com"
+	endpoint.SshPort = "8888"
+	oldGITSSHCommand := config.Config.Getenv("GIT_SSH_COMMAND")
+	config.Config.Setenv("GIT_SSH_COMMAND", "sshcmd")
+	exe, args := sshGetExeAndArgs(endpoint)
+	assert.Equal(t, "sshcmd", exe)
+	assert.Equal(t, []string{"-p", "8888", "user@foo.com"}, args)
+
+	config.Config.Setenv("GIT_SSH_COMMAND", oldGITSSHCommand)
+}
+
+func TestSSHGetExeAndArgsPlinkCommand(t *testing.T) {
+	endpoint := config.Config.Endpoint("download")
+	endpoint.SshUserAndHost = "user@foo.com"
+	oldGITSSHCommand := config.Config.Getenv("GIT_SSH_COMMAND")
+	// this will run on non-Windows platforms too but no biggie
+	plink := filepath.Join("Users", "joebloggs", "bin", "plink.exe")
+	config.Config.Setenv("GIT_SSH_COMMAND", plink)
+	exe, args := sshGetExeAndArgs(endpoint)
+	assert.Equal(t, plink, exe)
+	assert.Equal(t, []string{"user@foo.com"}, args)
+
+	config.Config.Setenv("GIT_SSH_COMMAND", oldGITSSHCommand)
+}
+
+func TestSSHGetExeAndArgsPlinkCommandCustomPort(t *testing.T) {
+	endpoint := config.Config.Endpoint("download")
+	endpoint.SshUserAndHost = "user@foo.com"
+	endpoint.SshPort = "8888"
+	oldGITSSHCommand := config.Config.Getenv("GIT_SSH_COMMAND")
+	// this will run on non-Windows platforms too but no biggie
+	plink := filepath.Join("Users", "joebloggs", "bin", "plink")
+	config.Config.Setenv("GIT_SSH_COMMAND", plink)
+	exe, args := sshGetExeAndArgs(endpoint)
+	assert.Equal(t, plink, exe)
+	assert.Equal(t, []string{"-P", "8888", "user@foo.com"}, args)
+
+	config.Config.Setenv("GIT_SSH_COMMAND", oldGITSSHCommand)
+}
+
+func TestSSHGetExeAndArgsTortoisePlinkCommand(t *testing.T) {
+	endpoint := config.Config.Endpoint("download")
+	endpoint.SshUserAndHost = "user@foo.com"
+	oldGITSSHCommand := config.Config.Getenv("GIT_SSH_COMMAND")
+	// this will run on non-Windows platforms too but no biggie
+	plink := filepath.Join("Users", "joebloggs", "bin", "tortoiseplink.exe")
+	config.Config.Setenv("GIT_SSH_COMMAND", plink)
+	exe, args := sshGetExeAndArgs(endpoint)
+	assert.Equal(t, plink, exe)
+	assert.Equal(t, []string{"-batch", "user@foo.com"}, args)
+
+	config.Config.Setenv("GIT_SSH_COMMAND", oldGITSSHCommand)
+}
+
+func TestSSHGetExeAndArgsTortoisePlinkCommandCustomPort(t *testing.T) {
+	endpoint := config.Config.Endpoint("download")
+	endpoint.SshUserAndHost = "user@foo.com"
+	endpoint.SshPort = "8888"
+	oldGITSSHCommand := config.Config.Getenv("GIT_SSH_COMMAND")
+	// this will run on non-Windows platforms too but no biggie
+	plink := filepath.Join("Users", "joebloggs", "bin", "tortoiseplink")
+	config.Config.Setenv("GIT_SSH_COMMAND", plink)
+	exe, args := sshGetExeAndArgs(endpoint)
+	assert.Equal(t, plink, exe)
+	assert.Equal(t, []string{"-batch", "-P", "8888", "user@foo.com"}, args)
+
+	config.Config.Setenv("GIT_SSH_COMMAND", oldGITSSHCommand)
 }
