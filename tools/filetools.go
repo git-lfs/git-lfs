d8fee48006d10168724705aa7ef0eadc3377c4a4
Taylor Blau <me@ttaylorr.com>
2016-06-02 09:20:51 -0600

    backport: tools: remove unused funcs

--- tools/filetools.go
+++ tools/filetools.go
@@ -3,56 +3,9 @@
 package tools
 
 import (
-	"os"
 	"path/filepath"
 	"strings"
 )
-
-// FileOrDirExists determines if a file/dir exists, returns IsDir() results too.
-func FileOrDirExists(path string) (exists bool, isDir bool) {
-	fi, err := os.Stat(path)
-	if err != nil {
-		return false, false
-	} else {
-		return true, fi.IsDir()
-	}
-}
-
-// FileExists determines if a file (NOT dir) exists.
-func FileExists(path string) bool {
-	ret, isDir := FileOrDirExists(path)
-	return ret && !isDir
-}
-
-// DirExists determines if a dir (NOT file) exists.
-func DirExists(path string) bool {
-	ret, isDir := FileOrDirExists(path)
-	return ret && isDir
-}
-
-// FileExistsOfSize determines if a file exists and is of a specific size.
-func FileExistsOfSize(path string, sz int64) bool {
-	fi, err := os.Stat(path)
-
-	if err != nil {
-		return false
-	}
-
-	return !fi.IsDir() && fi.Size() == sz
-}
-
-// ResolveSymlinks ensures that if the path supplied is a symlink, it is
-// resolved to the actual concrete path
-func ResolveSymlinks(path string) string {
-	if len(path) == 0 {
-		return path
-	}
-
-	if resolved, err := filepath.EvalSymlinks(path); err == nil {
-		return resolved
-	}
-	return path
-}
 
 // CleanPaths splits the given `paths` argument by the delimiter argument, and
 // then "cleans" that path according to the filepath.Clean function (see
