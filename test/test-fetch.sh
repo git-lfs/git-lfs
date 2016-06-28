git-lfs/git-lfs

commit 39676192da53261efd441ccf0c67c85a341ae1a1

Make ref resolution return a complete Ref rather than just SHA, & test

git/git.go
@@ -57,11 +57,19 @@ func LsRemote(remote, remoteRef string) (string, error) {
 	return simpleExec("git", "ls-remote", remote, remoteRef)
 }
 
-func ResolveRef(ref string) (string, error) {
-	return simpleExec("git", "rev-parse", ref)
+func ResolveRef(ref string) (*Ref, error) {
+
+	outp, err := simpleExec("git", "rev-parse", ref, "--symbolic-full-name", ref)
+	if err != nil {
+		return nil, err
+	}
+	lines := strings.Split(outp, "\n")
+	fullref := &Ref{Sha: lines[0]}
+	fullref.Type, fullref.Name = ParseRefToTypeAndName(lines[1])
+	return fullref, nil
 }
 
-func CurrentRef() (string, error) {
+func CurrentRef() (*Ref, error) {
 	return ResolveRef("HEAD")
 }
 
@@ -69,10 +77,10 @@ func CurrentBranch() (string, error) {
 	return simpleExec("git", "rev-parse", "--abbrev-ref", "HEAD")
 }
 
-func CurrentRemoteRef() (string, error) {
+func CurrentRemoteRef() (*Ref, error) {
 	remote, err := CurrentRemote()
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	return ResolveRef(remote)

git/git_test.go
@@ -9,6 +9,59 @@ import (
 	"github.com/github/git-lfs/vendor/_nuts/github.com/technoweenie/assert"
 )
 
+func TestCurrentRefAndCurrentRemoteRef(t *testing.T) {
+	repo := test.NewRepo(t)
+	repo.Pushd()
+	defer func() {
+		repo.Popd()
+		repo.Cleanup()
+	}()
+
+	// test commits; we'll just modify the same file each time since we're
+	// only interested in branches
+	inputs := []*test.CommitInput{
+		{ // 0
+			Files: []*test.FileInput{
+				{"file1.txt", 20, nil},
+			},
+		},
+		{ // 1
+			NewBranch: "branch2",
+			Files: []*test.FileInput{
+				{"file1.txt", 25, nil},
+			},
+		},
+		{ // 2
+			ParentBranches: []string{"master"}, // back on master
+			Files: []*test.FileInput{
+				{"file1.txt", 30, nil},
+			},
+		},
+		{ // 3
+			NewBranch: "branch3",
+			Files: []*test.FileInput{
+				{"file1.txt", 32, nil},
+			},
+		},
+	}
+	outputs := repo.AddCommits(inputs)
+	// last commit was on branch3
+	ref, err := CurrentRef()
+	assert.Equal(t, nil, err)
+	assert.Equal(t, &Ref{"branch3", RefTypeLocalBranch, outputs[3].Sha}, ref)
+	test.RunGitCommand(t, true, "checkout", "master")
+	ref, err = CurrentRef()
+	assert.Equal(t, nil, err)
+	assert.Equal(t, &Ref{"master", RefTypeLocalBranch, outputs[2].Sha}, ref)
+	// Check remote
+	repo.AddRemote("origin")
+	test.RunGitCommand(t, true, "push", "-u", "origin", "master")
+	ref, err = CurrentRemoteRef()
+	assert.Equal(t, nil, err)
+	assert.Equal(t, &Ref{"origin/master", RefTypeRemoteBranch, outputs[2].Sha}, ref)
+
+}
+
 func TestRecentBranches(t *testing.T) {
 	repo := test.NewRepo(t)
 	repo.Pushd()

commands/command_checkout.go
@@ -54,7 +54,7 @@ func checkoutFromFetchChan(include []string, exclude []string, in chan *lfs.Wrap
 		Panic(err, "Could not checkout")
 	}
 	// Need to ScanTree to identify multiple files with the same content (fetch will only report oids once)
-	pointers, err := lfs.ScanTree(ref)
+	pointers, err := lfs.ScanTree(ref.Sha)
 	if err != nil {
 		Panic(err, "Could not scan for Git LFS files")
 	}
@@ -95,7 +95,7 @@ func checkoutWithIncludeExclude(include []string, exclude []string) {
 		Panic(err, "Could not checkout")
 	}
 
-	pointers, err := lfs.ScanTree(ref)
+	pointers, err := lfs.ScanTree(ref.Sha)
 	if err != nil {
 		Panic(err, "Could not scan for Git LFS files")
 	}

commands/command_fetch.go
@@ -17,11 +17,11 @@ var (
 	}
 	fetchIncludeArg string
 	fetchExcludeArg string
-	fetchRecent     bool
+	fetchRecentArg  bool
 )
 
 func fetchCommand(cmd *cobra.Command, args []string) {
-	var refs []string
+	var refs []*Ref
 
 	if len(args) > 0 {
 		// Remote is first arg
@@ -39,14 +39,20 @@ func fetchCommand(cmd *cobra.Command, args []string) {
 		if err != nil {
 			Panic(err, "Could not fetch")
 		}
-		refs = []string{ref}
+		refs = []*Ref{ref}
 	}
 
 	includePaths, excludePaths := determineIncludeExcludePaths(fetchIncludeArg, fetchExcludeArg)
 
 	// Fetch refs sequentially per arg order; duplicates in later refs will be ignored
 	for _, ref := range refs {
-		fetchRef(ref, includePaths, excludePaths)
+		Print("Fetching %v", ref.Name)
+		fetchRef(ref.Sha, includePaths, excludePaths)
+	}
+
+	if fetchRecentArg || lfs.Config.FetchPruneConfig().FetchRecentAlways {
+		Print("Fetching recent objects")
+		fetchRecent(refs, includePaths, excludePaths)
 	}
 
 }
@@ -54,7 +60,7 @@ func fetchCommand(cmd *cobra.Command, args []string) {
 func init() {
 	fetchCmd.Flags().StringVarP(&fetchIncludeArg, "include", "I", "", "Include a list of paths")
 	fetchCmd.Flags().StringVarP(&fetchExcludeArg, "exclude", "X", "", "Exclude a list of paths")
-	fetchCmd.Flags().BoolVarP(&fetchRecent, "recent", "r", false, "Fetch recent refs & commits")
+	fetchCmd.Flags().BoolVarP(&fetchRecentArg, "recent", "r", false, "Fetch recent refs & commits")
 	RootCmd.AddCommand(fetchCmd)
 }
 
@@ -77,6 +83,36 @@ func fetchRef(ref string, include, exclude []string) {
 		Panic(err, "Could not scan for Git LFS files")
 	}
 	fetchPointers(pointers, include, exclude)
+}
+
+// Fetch recent objects based on config
+func fetchRecent(alreadyFetchedRefs []*Ref, include, exclude []string) {
+	fetchconf := lfs.Config.FetchPruneConfig()
+
+	// First find any other recent refs
+	if fetchconf.FetchRecentRefsDays > 0 {
+		refsSince := time.Now().AddDate(0, 0, -fetchconf.FetchRecentRefsDays)
+		refs, err := git.RecentRefs(refsSince, fetchconf.FetchRecentRefsIncludeRemotes, "")
+		if err != nil {
+			Panic(err, "Could not scan for recent refs")
+		}
+		uniqueRefShas := make(map[string]string, len(refsSince))
+		for _, ref := range alreadyFetchedRefs {
+			uniqueRefShas[ref.Sha] = ref.Name
+		}
+		for _, ref := range refsSince {
+			// Don't fetch for the same SHA twice
+			if prevRefName, ok := uniqueRefShas[ref.Sha]; ok {
+				Print("Skipping fetch for %v, already fetched commit via %v", ref.Name, prevRefName)
+			} else {
+				uniqueRefShas[ref.Sha] = ref.Name
+				Print("Fetching recent ref %v", ref.Name)
+				fetchRef(ref.Sha, includePaths, excludePaths)
+			}
+		}
+		// Add to list to scan for recent commits
+
+	}
 }
 
 func fetchPointers(pointers []*lfs.WrappedPointer, include, exclude []string) {

commands/command_fsck.go
@@ -32,7 +32,7 @@ func doFsck() (bool, error) {
 	// All we care about is the pointer OID and file name
 	pointerIndex := make(map[string]string)
 
-	pointers, err := lfs.ScanRefs(ref, "", nil)
+	pointers, err := lfs.ScanRefs(ref.Sha, "", nil)
 	if err != nil {
 		return false, err
 	}

commands/command_ls_files.go
@@ -21,10 +21,11 @@ func lsFilesCommand(cmd *cobra.Command, args []string) {
 	if len(args) == 1 {
 		ref = args[0]
 	} else {
-		ref, err = git.CurrentRef()
+		fullref, err = git.CurrentRef()
 		if err != nil {
 			Panic(err, "Could not ls-files")
 		}
+		ref = fullref.Sha
 	}
 
 	scanOpt := &lfs.ScanRefsOptions{SkipDeletedBlobs: true}

commands/command_pull.go
@@ -35,7 +35,7 @@ func pullCommand(cmd *cobra.Command, args []string) {
 
 	includePaths, excludePaths := determineIncludeExcludePaths(pullIncludeArg, pullExcludeArg)
 
-	c := fetchRefToChan(ref, includePaths, excludePaths)
+	c := fetchRefToChan(ref.Sha, includePaths, excludePaths)
 	checkoutFromFetchChan(includePaths, excludePaths, c)
 }
 

commands/command_push.go
@@ -157,7 +157,7 @@ func pushCommand(cmd *cobra.Command, args []string) {
 		if err != nil {
 			Panic(err, "Error getting local ref")
 		}
-		left = localRef
+		left = localRef.Sha
 
 		remoteRef, err := git.LsRemote(remoteArg, refArg)
 		if err != nil {

commit f86997f8636c471a4f6656693f688ea1fa2cc092

Fix 'git lfs fetch' with a sha1 ref
The commit 39676192da53261efd441ccf0c67c85a341ae1a1
introduced a regression in the capability to fetch a
sha1 ref.
The command 'git lfs fetch origin <sha1>' was returning
following error: 'Invalid ref argument: [<sha1>]'.

This is due to the fact that 'git rev-parse --symbolic-full-name <ref>'
returns an empty content if the ref is a sha1.

Signed-off-by: Olivier Monnier <olivier.monnier@intel.com>

git/git.go
@@ -67,12 +67,21 @@ func ResolveRef(ref string) (*Ref, error) {
 	if err != nil {
 		return nil, err
 	}
-	lines := strings.Split(outp, "\n")
-	if len(lines) <= 1 {
+	if outp == "" {
 		return nil, fmt.Errorf("Git can't resolve ref: %q", ref)
 	}
 
+	lines := strings.Split(outp, "\n")
 	fullref := &Ref{Sha: lines[0]}
+
+	if len(lines) == 1 {
+		// ref is a sha1 and has no symbolic-full-name
+		fullref.Name = lines[0] // fullref.Sha
+		fullref.Type = RefTypeOther
+		return fullref, nil
+	}
+
+	// parse the symbolic-full-name
 	fullref.Type, fullref.Name = ParseRefToTypeAndName(lines[1])
 	return fullref, nil
 }

git/git_test.go
@@ -71,6 +71,10 @@ func TestCurrentRefAndCurrentRemoteRef(t *testing.T) {
 	remote, err := RemoteForCurrentBranch()
 	assert.Nil(t, err)
 	assert.Equal(t, "origin", remote)
+
+	ref, err = ResolveRef(outputs[2].Sha)
+	assert.Nil(t, err)
+	assert.Equal(t, &Ref{outputs[2].Sha, RefTypeOther, outputs[2].Sha}, ref)
 }
 
 func TestRecentBranches(t *testing.T) {

test/test-fetch.sh
@@ -80,6 +80,13 @@ begin_test "fetch"
   assert_local_object "$contents_oid" 1
   assert_local_object "$b_oid" 1
 
+  # test with commit sha1 specified
+  rm -rf .git/lfs/objects
+  newbranch_sha1=$(git rev-parse newbranch)
+  git lfs fetch origin "$newbranch_sha1"
+  assert_local_object "$contents_oid" 1
+  assert_local_object "$b_oid" 1
+
   # Test include / exclude filters supplied in gitconfig
   rm -rf .git/lfs/objects
   git config "lfs.fetchinclude" "a*"