e6b29c79acf5b8d5b2084ff3e66da1a2927da7dc
Taylor Blau <me@ttaylorr.com>
2016-07-21 11:42:41 -0600

    command/test: guard `unlock` command behind feature flag, too

--- test/test-unlock.sh
+++ test/test-unlock.sh
@@ -8,12 +8,12 @@ begin_test "unlocking a lock by path"
 
   setup_remote_repo_with_file "unlock_by_path" "c.dat"
 
-  git lfs lock "c.dat" | tee lock.log
+  GITLFSLOCKSENABLED=1 git lfs lock "c.dat" | tee lock.log
 
   id=$(grep -oh "\((.*)\)" lock.log | tr -d "()")
   assert_server_lock $id
 
-  git lfs unlock "c.dat" 2>&1 | tee unlock.log
+  GITLFSLOCKSENABLED=1 git lfs unlock "c.dat" 2>&1 | tee unlock.log
   refute_server_lock $id
 )
 end_test
@@ -24,12 +24,12 @@ begin_test "unlocking a lock by id"
 
   setup_remote_repo_with_file "unlock_by_id" "d.dat"
 
-  git lfs lock "d.dat" | tee lock.log
+  GITLFSLOCKSENABLED=1 git lfs lock "d.dat" | tee lock.log
 
   id=$(grep -oh "\((.*)\)" lock.log | tr -d "()")
   assert_server_lock $id
 
-  git lfs unlock --id="$id" 2>&1 | tee unlock.log
+  GITLFSLOCKSENABLED=1 git lfs unlock --id="$id" 2>&1 | tee unlock.log
   refute_server_lock $id
 )
 end_test
@@ -40,12 +40,12 @@ begin_test "unlocking a lock without sufficient info"
 
   setup_remote_repo_with_file "unlock_ambiguous" "e.dat"
 
-  git lfs lock "e.dat" | tee lock.log
+  GITLFSLOCKSENABLED=1 git lfs lock "e.dat" | tee lock.log
 
   id=$(grep -oh "\((.*)\)" lock.log | tr -d "()")
   assert_server_lock $id
 
-  git lfs unlock 2>&1 | tee unlock.log
+  GITLFSLOCKSENABLED=1 git lfs unlock 2>&1 | tee unlock.log
   grep "Usage: git lfs unlock" unlock.log
   assert_server_lock $id
 )
