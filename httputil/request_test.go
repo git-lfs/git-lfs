062dc3a2848402e6b4e2c395bc06e2d7a2241576
risk danger olson <technoweenie@gmail.com>
2016-07-21 11:07:46 -0600

    more fixes

--- httputil/request_test.go
+++ httputil/request_test.go
@@ -13,7 +13,7 @@ type AuthenticateHeaderTestCase struct {
 }
 
 func (c *AuthenticateHeaderTestCase) Assert(t *testing.T) {
-	t.Log("lfs/httputil: asserting auth type: %q for: %v", c.ExpectedAuthType, c.Headers)
+	t.Logf("lfs/httputil: asserting auth type: %q for: %v", c.ExpectedAuthType, c.Headers)
 
 	assert.Equal(t, c.ExpectedAuthType, GetAuthType(c.HttpResponse()))
 }
@@ -32,12 +32,12 @@ func (c *AuthenticateHeaderTestCase) HttpResponse() *http.Response {
 
 func TestGetAuthType(t *testing.T) {
 	for _, c := range []AuthenticateHeaderTestCase{
-		{BasicAuthType, map[string][]string{}},
-		{NtlmAuthType, map[string][]string{"WWW-Authenticate": {"Basic", "NTLM", "Bearer"}}},
-		{NtlmAuthType, map[string][]string{"LFS-Authenticate": {"Basic", "NTLM", "Bearer"}}},
-		{NtlmAuthType, map[string][]string{"LFS-Authenticate": {"Basic", "Ntlm"}}},
-		{NtlmAuthType, map[string][]string{"Www-Authenticate": {"Basic", "Ntlm"}}},
-		{NtlmAuthType, map[string][]string{"WWW-Authenticate": {"Basic"},
+		{basicAuthType, map[string][]string{}},
+		{ntlmAuthType, map[string][]string{"WWW-Authenticate": {"Basic", "NTLM", "Bearer"}}},
+		{ntlmAuthType, map[string][]string{"LFS-Authenticate": {"Basic", "NTLM", "Bearer"}}},
+		{ntlmAuthType, map[string][]string{"LFS-Authenticate": {"Basic", "Ntlm"}}},
+		{ntlmAuthType, map[string][]string{"Www-Authenticate": {"Basic", "Ntlm"}}},
+		{ntlmAuthType, map[string][]string{"WWW-Authenticate": {"Basic"},
 			"LFS-Authenticate": {"Ntlm"}}},
 	} {
 		c.Assert(t)
