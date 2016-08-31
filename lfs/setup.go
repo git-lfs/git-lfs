c0843c7c7baf48252be3c83446258228e725b36c
risk danger olson <technoweenie@gmail.com>
2016-08-31 14:48:38 -0600

    install pre-commit hook

--- lfs/setup.go
+++ lfs/setup.go
@@ -19,8 +19,14 @@ var (
 		},
 	}
 
+	preCommitHook = &Hook{
+		Type:     "pre-commit",
+		Contents: "#!/bin/sh\ncommand -v git-lfs >/dev/null 2>&1 || { echo >&2 \"\\nThis repository is configured for Git LFS but 'git-lfs' was not found on your path. If you no longer wish to use Git LFS, remove this hook by deleting .git/hooks/pre-commit.\\n\"; exit 2; }\ngit lfs pre-commit \"$@\"",
+	}
+
 	hooks = []*Hook{
 		prePushHook,
+		preCommitHook,
 	}
 
 	filters = &Attribute{
