4f4021506c33404cd7c46516a210254b17936cda
rubyist <scott.barron@github.com>
2016-08-31 15:38:17 -0600

    basic pre-commit command

---
+++ commands/command_pre_commit.go
@@ -0,0 +1,50 @@
+package commands
+
+import (
+	"os"
+
+	"github.com/git-lfs/git-lfs/git"
+	"github.com/git-lfs/git-lfs/locking"
+	"github.com/spf13/cobra"
+)
+
+func preCommitCommand(cmd *cobra.Command, args []string) {
+	requireGitVersion()
+
+	name, email := cfg.CurrentCommitter()
+	lc, err := locking.NewClient(cfg)
+	if err != nil {
+		Exit("Unable to create lock system: %v", err.Error())
+	}
+	defer lc.Close()
+
+	lockSet, err := findLocks(lc, nil, 0, false)
+	if err != nil {
+		Exit("error finding locks: %s", err)
+	}
+
+	files, err := git.StagedFiles()
+	if err != nil {
+		Exit("error finding staged files: %s", err)
+	}
+
+	lockConflicts := make([]string, 0, len(lockSet))
+
+	for _, f := range files {
+		if l, ok := lockSet[f]; ok && !(l.Name == name && l.Email == email) {
+			lockConflicts = append(lockConflicts, f)
+		}
+	}
+
+	if len(lockConflicts) > 0 {
+		Error("Some files are locked")
+		for _, file := range lockConflicts {
+			Error("* %s", file)
+		}
+		os.Exit(1)
+	}
+}
+
+func init() {
+	RegisterCommand("pre-commit", preCommitCommand, nil)
+}
