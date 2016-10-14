eb6c4a3eb6d2174d9c0f4534a37ffb517e9ae69e
Taylor Blau <me@ttaylorr.com>
2016-10-14 14:56:56 -0600

    lfs/tq,test: use Transferable's OID when performing legacy check
    Closes: github/git-lfs#1580

---
+++ test/test-legacy-retries.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+. "test/testlib.sh"
+
+begin_test "legacy upload check causes retries"
+(
+  set -e
+
+  reponame="legacy-upload-check-retry"
+  setup_remote_repo "$reponame"
+  clone_repo "$reponame" repo-upload
+  git config --local lfs.batch false
+
+  contents="legacy-upload-check-retry"
+  oid="$(calc_oid "$contents")"
+  printf "$contents" > a.dat
+
+  git lfs track "*.dat"
+  git add .gitattributes a.dat
+  git commit -m "initial commit"
+
+  git config --local lfs.transfer.maxretries 3
+  git push origin master
+
+  assert_server_object "$reponame" "$oid"
+)
+end_test
+
+begin_test "legacy download check causes retries"
+(
+  set -e
+
+  reponame="legacy-download-check-retry"
+  setup_remote_repo "$reponame"
+  clone_repo "$reponame" repo-download
+
+  contents="legacy-download-check-retry"
+  oid="$(calc_oid "$contents")"
+  printf "$contents" > a.dat
+
+  git lfs track "*.dat"
+  git add .gitattributes a.dat
+  git commit -m "initial commit"
+
+  git push origin master
+  assert_server_object "$reponame" "$oid"
+
+  pushd ..
+    git \
+      -c "filter.lfs.smudge=" \
+      -c "filter.lfs.required=false" \
+      clone "$GITSERVER/$reponame" "$reponame-assert"
+
+    cd "$reponame-assert"
+
+    git config credential.helper lfstest
+    git config --local lfs.batch false
+    git config --local lfs.transfer.maxretries 2
+
+    git lfs pull origin
+  popd
+)
+end_test
