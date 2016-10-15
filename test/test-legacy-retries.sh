6ece33a3becc1eda25c9a52dfcab8d10296b3ddf
Taylor Blau <me@ttaylorr.com>
2016-10-15 16:04:06 -0600

    test: legacy storage download with retries

--- test/test-legacy-retries.sh
+++ test/test-legacy-retries.sh
@@ -86,3 +86,41 @@ begin_test "legacy storage upload causes retries"
   assert_server_object "$reponame" "$oid"
 )
 end_test
+
+begin_test "legacy storage download causes retries"
+(
+  set -e
+
+  reponame="legacy-storage-download-retry"
+  setup_remote_repo "$reponame"
+  clone_repo "$reponame" legacy-storage-repo-download
+
+  contents="storage-download-retry"
+  oid="$(calc_oid "$contents")"
+  printf "$contents" > a.dat
+
+  git lfs track "*.dat"
+  git add .gitattributes a.dat
+  git commit -m "initial commit"
+
+  git push origin master
+  assert_server_object "$reponame" "$oid"
+
+  pushd ..
+    git \
+      -c "filter.lfs.smudge=cat" \
+      -c "filter.lfs.required=false" \
+      clone "$GITSERVER/$reponame" "$reponame-assert"
+
+    cd "$reponame-assert"
+
+    git config credential.helper lfstest
+    git config --local lfs.batch false
+    git config --local lfs.transfer.maxretries 3
+
+    git lfs pull origin
+
+    assert_local_object "$oid" "${#contents}"
+  popd
+)
+end_test
