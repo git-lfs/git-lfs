bbd25f28abfaf38f887874ac445d8010129db41f
Taylor Blau <me@ttaylorr.com>
2016-10-18 10:13:05 -0600

    scanner/mode: apply some :nail_care:

--- scanner/mode.go
+++ scanner/mode.go
@@ -14,26 +14,29 @@ type ScanRefsOptions struct {
 	ScanMode         ScanningMode
 	RemoteName       string
 	SkipDeletedBlobs bool
-	nameMap          map[string]string
-	mutex            *sync.Mutex
+
+	nameMap map[string]string
+	mutex   *sync.Mutex
 }
 
 func (o *ScanRefsOptions) GetName(sha string) (string, bool) {
 	o.mutex.Lock()
+	defer o.mutex.Unlock()
+
 	name, ok := o.nameMap[sha]
-	o.mutex.Unlock()
 	return name, ok
 }
 
 func (o *ScanRefsOptions) SetName(sha, name string) {
 	o.mutex.Lock()
+	defer o.mutex.Unlock()
+
 	o.nameMap[sha] = name
-	o.mutex.Unlock()
 }
 
 func NewScanRefsOptions() *ScanRefsOptions {
 	return &ScanRefsOptions{
-		nameMap: make(map[string]string, 0),
+		nameMap: make(map[string]string),
 		mutex:   &sync.Mutex{},
 	}
 }
