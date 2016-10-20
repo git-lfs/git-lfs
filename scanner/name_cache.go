e16e2de4a11885b5c996f722f24830e0210bb515
Taylor Blau <me@ttaylorr.com>
2016-10-20 14:12:19 -0600

    scanner/name_cache: documentation

--- scanner/name_cache.go
+++ scanner/name_cache.go
@@ -2,11 +2,19 @@ package scanner
 
 import "sync"
 
+// NameCache is a goroutine-safe cache of the SHA1 hash of a file, to its name
+// on disk.
 type NameCache struct {
-	m  map[string]string
+	// m maps the SHA1 hash of a file to its name on disk.
+	m map[string]string
+	// mu guards m.
 	mu *sync.Mutex
 }
 
+// NewNameCache instantiates a new `*NameCache` using the given map, `m`.
+//
+// NOTE: passing `m` in from the call-site is only a temporary measure, and
+// exists so old references into ScanOpts can still remain valid.
 func NewNameCache(m map[string]string) *NameCache {
 	return &NameCache{
 		m:  m,
@@ -14,6 +22,9 @@ func NewNameCache(m map[string]string) *NameCache {
 	}
 }
 
+// GetName returns the name of the file who's contents has the SHA1 hash of
+// "sha". If no name was found matching the given "sha", then an empty string
+// and the value false are returned, true otherwise.
 func (c *NameCache) GetName(sha string) (string, bool) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
@@ -22,6 +33,8 @@ func (c *NameCache) GetName(sha string) (string, bool) {
 	return rev, ok
 }
 
+// Cache caches the given SHA1, "sha", against the given name, "name",
+// overwriting pre-existing cache entries, if any.
 func (c *NameCache) Cache(sha, name string) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
