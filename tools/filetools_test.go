1dfdea1f588824db4dd219232af1cbda439ac6f3
Steve Streeting <steve@stevestreeting.com>
2016-11-01 14:36:03 +0000

    Test for walking a git repo, skipping .git and reading .gitignore

--- tools/filetools_test.go
+++ tools/filetools_test.go
@@ -11,6 +11,8 @@ import (
 	"sync"
 	"testing"
 
+	"github.com/github/git-lfs/subprocess"
+
 	"github.com/github/git-lfs/tools"
 	"github.com/stretchr/testify/assert"
 )
@@ -105,9 +107,97 @@ func TestFastWalkBasic(t *testing.T) {
 	if err != nil {
 		assert.FailNow(t, "Unable to get temp dir: %v", err)
 	}
-
 	defer os.RemoveAll(rootDir)
 	os.Chdir(rootDir)
+
+	expectedEntries := createFastWalkInputData(10, 160)
+
+	fchan, errchan := tools.FastWalk(expectedEntries[0], nil, nil)
+	gotEntries, gotErrors := collectFastWalkResults(fchan, errchan)
+
+	assert.Empty(t, gotErrors)
+
+	sort.Strings(expectedEntries)
+	sort.Strings(gotEntries)
+	assert.Equal(t, expectedEntries, gotEntries)
+
+}
+
+func TestFastWalkGitRepo(t *testing.T) {
+	rootDir, err := ioutil.TempDir(os.TempDir(), "GitLfsTestFastWalkGitRepo")
+	if err != nil {
+		assert.FailNow(t, "Unable to get temp dir: %v", err)
+	}
+	defer os.RemoveAll(rootDir)
+	os.Chdir(rootDir)
+
+	expectedEntries := createFastWalkInputData(3, 3)
+
+	mainDir := expectedEntries[0]
+
+	// Set up a git repo and add some ignored files / dirs
+	subprocess.SimpleExec("git", "init", mainDir)
+	ignored := []string{
+		"filethatweignore.ign",
+		"foldercontainingignored",
+		"foldercontainingignored/notthisone.ign",
+		"ignoredfolder",
+		"ignoredfolder/file1.txt",
+		"ignoredfolder/file2.txt",
+		"ignoredfrominside",
+		"ignoredfrominside/thisisok.txt",
+		"ignoredfrominside/thisisnot.txt",
+		"ignoredfrominside/thisone",
+		"ignoredfrominside/thisone/file1.txt",
+	}
+	for _, f := range ignored {
+		fullPath := filepath.Join(mainDir, f)
+		if len(filepath.Ext(f)) > 0 {
+			ioutil.WriteFile(fullPath, []byte("TEST"), 0644)
+		} else {
+			os.MkdirAll(fullPath, 0755)
+		}
+	}
+	// write root .gitignore
+	rootGitIgnore := `
+# ignore *.ign everywhere
+*.ign
+# ignore folder
+ignoredfolder
+`
+	ioutil.WriteFile(filepath.Join(mainDir, ".gitignore"), []byte(rootGitIgnore), 0644)
+	// Subfolder ignore; folder will show up but but subfolder 'thisone' won't
+	subFolderIgnore := `
+thisone
+thisisnot.txt
+`
+	ioutil.WriteFile(filepath.Join(mainDir, "ignoredfrominside", ".gitignore"), []byte(subFolderIgnore), 0644)
+
+	// This dir will be walked but content won't be
+	expectedEntries = append(expectedEntries, filepath.Join(mainDir, "foldercontainingignored"))
+	// This dir will be walked and some of its content but has its own gitignore
+	expectedEntries = append(expectedEntries, filepath.Join(mainDir, "ignoredfrominside"))
+	expectedEntries = append(expectedEntries, filepath.Join(mainDir, "ignoredfrominside", "thisisok.txt"))
+	// Also gitignores
+	expectedEntries = append(expectedEntries, filepath.Join(mainDir, ".gitignore"))
+	expectedEntries = append(expectedEntries, filepath.Join(mainDir, "ignoredfrominside", ".gitignore"))
+	// nothing else should be there
+
+	fchan, errchan := tools.FastWalkGitRepo(mainDir)
+	gotEntries, gotErrors := collectFastWalkResults(fchan, errchan)
+
+	assert.Empty(t, gotErrors)
+
+	sort.Strings(expectedEntries)
+	sort.Strings(gotEntries)
+	assert.Equal(t, expectedEntries, gotEntries)
+
+}
+
+// Make test data - ensure you've Chdir'ed into a temp dir first
+// Returns list of files/dirs that are created
+// First entry is the parent dir of all others
+func createFastWalkInputData(smallFolder, largeFolder int) []string {
 	dirs := []string{
 		"testroot",
 		"testroot/folder1",
@@ -122,11 +212,11 @@ func TestFastWalkBasic(t *testing.T) {
 
 	for i, dir := range dirs {
 		os.MkdirAll(dir, 0755)
-		numFiles := 10
+		numFiles := smallFolder
 		expectedEntries = append(expectedEntries, dir)
 		if i >= 3 && i <= 5 {
 			// Bulk test to ensure works with > 1 batch
-			numFiles = 160
+			numFiles = largeFolder
 		}
 		for f := 0; f < numFiles; f++ {
 			filename := filepath.Join(dir, fmt.Sprintf("file%d.txt", f))
@@ -135,15 +225,7 @@ func TestFastWalkBasic(t *testing.T) {
 		}
 	}
 
-	fchan, errchan := tools.FastWalk(dirs[0], nil, nil)
-	gotEntries, gotErrors := collectFastWalkResults(fchan, errchan)
-
-	assert.Equal(t, 0, len(gotErrors))
-
-	sort.Strings(expectedEntries)
-	sort.Strings(gotEntries)
-	assert.Equal(t, expectedEntries, gotEntries)
-
+	return expectedEntries
 }
 
 func collectFastWalkResults(fchan <-chan tools.FastWalkInfo, errchan <-chan error) ([]string, []error) {
