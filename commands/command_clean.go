d634ae0935f0e8f361aa76ad767a290036bad777
Taylor Blau <me@ttaylorr.com>
2016-11-09 10:55:54 +0100

    commands: make clean/smudge functions reusable for new filter protocol

--- commands/command_clean.go
+++ commands/command_clean.go
@@ -1,6 +1,7 @@
 package commands
 
 import (
+	"io"
 	"os"
 
 	"github.com/github/git-lfs/errors"
@@ -9,17 +10,20 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func cleanCommand(cmd *cobra.Command, args []string) {
-	requireStdin("This command should be run by the Git 'clean' filter")
-	lfs.InstallHooks(false)
-
-	var fileName string
+// clean cleans an object read from the given `io.Reader`, "from", and writes
+// out a corresponding pointer to the `io.Writer`, "to". If there were any
+// errors encountered along the way, they will be returned immediately if the
+// error is non-fatal, otherwise they will halt using the built in
+// `commands.Panic`.
+//
+// If the object read from "from" is _already_ a clean pointer, then it will be
+// written out verbatim to "to", without trying to make it a pointer again.
+func clean(from io.Reader, to io.Writer, fileName string) error {
 	var cb progress.CopyCallback
 	var file *os.File
 	var fileSize int64
-	if len(args) > 0 {
-		fileName = args[0]
 
+	if len(fileName) > 0 {
 		stat, err := os.Stat(fileName)
 		if err == nil && stat != nil {
 			fileSize = stat.Size()
@@ -34,7 +38,7 @@ func cleanCommand(cmd *cobra.Command, args []string) {
 		}
 	}
 
-	cleaned, err := lfs.PointerClean(os.Stdin, fileName, fileSize, cb)
+	cleaned, err := lfs.PointerClean(from, fileName, fileSize, cb)
 	if file != nil {
 		file.Close()
 	}
@@ -44,8 +48,12 @@ func cleanCommand(cmd *cobra.Command, args []string) {
 	}
 
 	if errors.IsCleanPointerError(err) {
-		os.Stdout.Write(errors.GetContext(err, "bytes").([]byte))
-		return
+		// If the contents read from the working directory was _already_
+		// a pointer, we'll get a `CleanPointerError`, with the context
+		// containing the bytes that we should write back out to Git.
+
+		_, err = to.Write(errors.GetContext(err, "bytes").([]byte))
+		return err
 	}
 
 	if err != nil {
@@ -71,7 +79,22 @@ func cleanCommand(cmd *cobra.Command, args []string) {
 		Debug("Writing %s", mediafile)
 	}
 
-	lfs.EncodePointer(os.Stdout, cleaned.Pointer)
+	_, err = lfs.EncodePointer(to, cleaned.Pointer)
+	return err
+}
+
+func cleanCommand(cmd *cobra.Command, args []string) {
+	requireStdin("This command should be run by the Git 'clean' filter")
+	lfs.InstallHooks(false)
+
+	var fileName string
+	if len(args) > 0 {
+		fileName = args[0]
+	}
+
+	if err := clean(os.Stdin, os.Stdout, fileName); err != nil {
+		Error(err.Error())
+	}
 }
 
 func init() {
--- commands/command_smudge.go
+++ commands/command_smudge.go
@@ -14,9 +14,57 @@ import (
 )
 
 var (
+	// smudgeInfo is a command-line flag belonging to the "git-lfs smudge"
+	// command specifying whether to skip the smudge process and simply
+	// print out the info of the files being smudged.
+	//
+	// As of v1.5.0, it is deprecated.
 	smudgeInfo = false
+	// smudgeInfo is a command-line flag belonging to the "git-lfs smudge"
+	// command specifying whether to skip the smudge process.
 	smudgeSkip = false
 )
+
+// smudge smudges the given `*lfs.Pointer`, "ptr", and writes its objects
+// contents to the `io.Writer`, "to".
+//
+// If the smudged object did not "pass" the include and exclude filterset, it
+// will not be downloaded, and the object will remain a pointer on disk, as if
+// the smudge filter had not been applied at all.
+//
+// Any errors encountered along the way will be returned immediately if they
+// were non-fatal, otherwise execution will halt and the process will be
+// terminated by using the `commands.Panic()` func.
+func smudge(to io.Writer, ptr *lfs.Pointer, filename string, skip bool) error {
+	cb, file, err := lfs.CopyCallbackFile("smudge", filename, 1, 1)
+	if err != nil {
+		return err
+	}
+
+	download := tools.FilenamePassesIncludeExcludeFilter(filename, cfg.FetchIncludePaths(), cfg.FetchExcludePaths())
+
+	if skip || cfg.Os.Bool("GIT_LFS_SKIP_SMUDGE", false) {
+		download = false
+	}
+
+	err = ptr.Smudge(to, filename, download, TransferManifest(), cb)
+	if file != nil {
+		file.Close()
+	}
+
+	if err != nil {
+		ptr.Encode(to)
+		// Download declined error is ok to skip if we weren't requesting download
+		if !(errors.IsDownloadDeclinedError(err) && !download) {
+			LoggedError(err, "Error downloading object: %s (%s)", filename, ptr.Oid)
+			if !cfg.SkipDownloadErrors() {
+				os.Exit(2)
+			}
+		}
+	}
+
+	return nil
+}
 
 func smudgeCommand(cmd *cobra.Command, args []string) {
 	requireStdin("This command should be run by the Git 'smudge' filter")
@@ -49,41 +97,17 @@ func smudgeCommand(cmd *cobra.Command, args []string) {
 			Exit(err.Error())
 		}
 
-		stat, err := os.Stat(localPath)
-		if err != nil {
+		if stat, err := os.Stat(localPath); err != nil {
 			Print("%d --", ptr.Size)
 		} else {
 			Print("%d %s", stat.Size(), localPath)
 		}
+
 		return
 	}
 
-	filename := smudgeFilename(args, perr)
-	cb, file, err := lfs.CopyCallbackFile("smudge", filename, 1, 1)
-	if err != nil {
+	if err := smudge(os.Stdout, ptr, smudgeFilename(args, perr), smudgeSkip); err != nil {
 		Error(err.Error())
-	}
-
-	download := tools.FilenamePassesIncludeExcludeFilter(filename, cfg.FetchIncludePaths(), cfg.FetchExcludePaths())
-
-	if smudgeSkip || cfg.Os.Bool("GIT_LFS_SKIP_SMUDGE", false) {
-		download = false
-	}
-
-	err = ptr.Smudge(os.Stdout, filename, download, TransferManifest(), cb)
-	if file != nil {
-		file.Close()
-	}
-
-	if err != nil {
-		ptr.Encode(os.Stdout)
-		// Download declined error is ok to skip if we weren't requesting download
-		if !(errors.IsDownloadDeclinedError(err) && !download) {
-			LoggedError(err, "Error downloading object: %s (%s)", filename, ptr.Oid)
-			if !cfg.SkipDownloadErrors() {
-				os.Exit(2)
-			}
-		}
 	}
 }
 
