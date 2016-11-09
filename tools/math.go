e6eb985b0aa2d0438fe87e9b05eac9534df4d64b
Taylor Blau <me@ttaylorr.com>
2016-11-08 18:50:52 -0700

    tools: :nail_care:

--- tools/math.go
+++ tools/math.go
@@ -1,5 +1,6 @@
 package tools
 
+// MinInt returns the smaller of two `int`s, "a", or "b".
 func MinInt(a, b int) int {
 	if a < b {
 		return a
@@ -8,6 +9,7 @@ func MinInt(a, b int) int {
 	return b
 }
 
+// MaxInt returns the greater of two `int`s, "a", or "b".
 func MaxInt(a, b int) int {
 	if a > b {
 		return a
