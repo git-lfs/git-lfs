0677bdd06a97b60455dac2cfa19d54ff697a1e17
risk danger olson <technoweenie@gmail.com>
2016-12-19 11:02:13 -0700

    replace the last 2 external api.VerifyUpload uses

--- tq/custom.go
+++ tq/custom.go
@@ -12,13 +12,11 @@ import (
 	"strings"
 	"time"
 
+	"github.com/git-lfs/git-lfs/lfsapi"
 	"github.com/git-lfs/git-lfs/tools"
 
-	"github.com/git-lfs/git-lfs/api"
 	"github.com/git-lfs/git-lfs/subprocess"
 	"github.com/rubyist/tracerx"
-
-	"github.com/git-lfs/git-lfs/config"
 )
 
 // Adapter for custom transfer via external process
@@ -316,7 +314,8 @@ func (a *customAdapter) DoTransfer(ctx interface{}, t *Transfer, cb ProgressCall
 					return fmt.Errorf("Failed to copy downloaded file: %v", err)
 				}
 			} else if a.direction == Upload {
-				if err = api.VerifyUpload(config.Config, toApiObject(t)); err != nil {
+				cli := &lfsapi.Client{}
+				if err = verifyUpload(cli, t); err != nil {
 					return err
 				}
 			}
