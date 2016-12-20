0d6f87302ed7bd7fffae2a2a109daeca1b53906b
risk danger olson <technoweenie@gmail.com>
2016-12-20 10:02:25 -0700

    build http clients per host

--- lfsapi/lfsapi.go
+++ lfsapi/lfsapi.go
@@ -2,8 +2,11 @@ package lfsapi
 
 import (
 	"encoding/json"
+	"net"
 	"net/http"
 	"regexp"
+	"sync"
+	"time"
 
 	"github.com/git-lfs/git-lfs/errors"
 )
@@ -17,6 +20,14 @@ type Client struct {
 	Endpoints   EndpointFinder
 	Credentials CredentialHelper
 	Netrc       NetrcFinder
+
+	DialTimeout         int `git:"lfs.dialtimeout"`
+	KeepaliveTimeout    int `git:"lfs.keepalive"`
+	TLSTimeout          int `git:"lfs.tlstimeout"`
+	ConcurrentTransfers int `git:"lfs.concurrenttransfers"`
+
+	hostClients map[string]*http.Client
+	clientMu    sync.Mutex
 }
 
 func NewClient(osEnv env, gitEnv env) (*Client, error) {
@@ -35,12 +46,62 @@ func NewClient(osEnv env, gitEnv env) (*Client, error) {
 }
 
 func (c *Client) Do(req *http.Request) (*http.Response, error) {
-	res, err := http.DefaultClient.Do(req)
+	res, err := c.httpClient(req.Host).Do(req)
 	if err != nil {
 		return res, err
 	}
 
 	return res, c.handleResponse(res)
+}
+
+func (c *Client) httpClient(host string) *http.Client {
+	c.clientMu.Lock()
+	defer c.clientMu.Unlock()
+
+	if c.hostClients == nil {
+		c.hostClients = make(map[string]*http.Client)
+	}
+
+	if client, ok := c.hostClients[host]; ok {
+		return client
+	}
+
+	concurrentTransfers := c.ConcurrentTransfers
+	if concurrentTransfers < 1 {
+		concurrentTransfers = 3
+	}
+
+	dialtime := c.DialTimeout
+	if dialtime < 1 {
+		dialtime = 30
+	}
+
+	keepalivetime := c.KeepaliveTimeout
+	if keepalivetime < 1 {
+		keepalivetime = 1800
+	}
+
+	tlstime := c.TLSTimeout
+	if tlstime < 1 {
+		tlstime = 30
+	}
+
+	tr := &http.Transport{
+		Dial: (&net.Dialer{
+			Timeout:   time.Duration(dialtime) * time.Second,
+			KeepAlive: time.Duration(keepalivetime) * time.Second,
+		}).Dial,
+		TLSHandshakeTimeout: time.Duration(tlstime) * time.Second,
+		MaxIdleConnsPerHost: concurrentTransfers,
+	}
+
+	httpClient := &http.Client{
+		Transport: tr,
+	}
+
+	c.hostClients[host] = httpClient
+
+	return httpClient
 }
 
 func decodeResponse(res *http.Response, obj interface{}) error {
