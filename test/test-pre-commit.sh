967d4122fce89195b928ea5470cf051f33688d21
Taylor Blau <me@ttaylorr.com>
2016-12-28 12:15:47 -0700

    test/pre-commit: add test with unowned locks

--- test/test-pre-commit.sh
+++ test/test-pre-commit.sh
@@ -32,3 +32,53 @@ begin_test "pre-commit with own locked files"
   git commit -m "locked changes"
 )
 end_test
+
+begin_test "pre-commit with unowned locked files"
+(
+  set -e
+
+  reponame="pre-commit-unowned-locks"
+  setup_remote_repo "$reponame"
+  clone_repo "$reponame" "$reponame"
+
+  git config --local user.name "Example Locker"
+  git config --local user.email "locker@xample.com"
+
+  git lfs track "*.dat"
+  git add .gitattributes
+  git commit -m "initial commit"
+
+  contents="locked contents"
+  printf "$contents" > locked_pc_unauth.dat
+  git add locked_pc_unauth.dat
+  git commit -m "add locked_pc_unauth.dat"
+
+  git push origin master
+
+  GITLFSLOCKSENABLED=1 git lfs lock "locked_pc_unauth.dat" | tee lock.log
+  grep "'locked_pc_unauth.dat' was locked" lock.log
+
+  id=$(grep -oh "\((.*)\)" lock.log | tr -d "()")
+  assert_server_lock $id
+
+  pushd "$TRASHDIR" >/dev/null
+    clone_repo "$reponame" "$reponame-assert"
+
+    printf "authorized changes" >> locked_pc_unauth.dat
+    git add locked_pc_unauth.dat
+
+    set +e
+    git commit -m "locked_pc_unauth changes" 2>&1 | tee commit.log
+    ok="${PIPESTATUS[0]}"
+    set -e
+
+    if [ "0" -eq "$ok" ]; then
+      echo >&2 "ERR: expected \`git commit\` to fail, didn't..."
+      exit 1
+    fi
+
+    grep "Some files are locked" commit.log
+    grep "locked_pc_unauth.dat" commit.log
+  popd >/dev/null
+)
+end_test
