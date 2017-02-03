065e78a984ecf67a8cb69a08b6c7dfc9d18afb11
Steve Streeting <steve@stevestreeting.com>
2017-02-03 09:44:29 +0000

    Add test for unlocking untracked file

--- test/test-unlock.sh
+++ test/test-unlock.sh
@@ -121,3 +121,34 @@ begin_test "unlocking a lock while uncommitted with --force"
   refute_server_lock "$reponame" "$id"
 )
 end_test
+
+begin_test "unlocking a lock while untracked"
+(
+  set -e
+
+  reponame="unlock_untracked"
+  setup_remote_repo_with_file "$reponame" "notrelevant.dat"
+
+  git lfs track "*.dat"
+  # Create file but don't add it to git
+  # Shouldn't be able to unlock it
+  echo "something" > untracked.dat
+  GITLFSLOCKSENABLED=1 git lfs lock "untracked.dat" | tee lock.log
+
+  id=$(grep -oh "\((.*)\)" lock.log | tr -d "()")
+  assert_server_lock "$reponame" "$id"
+
+  GITLFSLOCKSENABLED=1 git lfs unlock "untracked.dat" 2>&1 | tee unlock.log
+  [ ${PIPESTATUS[0]} -ne "0" ]
+
+  grep "Cannot unlock file with uncommitted changes" unlock.log
+
+  assert_server_lock "$reponame" "$id"
+
+  # should allow after add/commit
+  git add untracked.dat
+  git commit -m "Added untracked"
+  GITLFSLOCKSENABLED=1 git lfs unlock "untracked.dat" 2>&1 | tee unlock.log
+  refute_server_lock "$reponame" "$id"
+)
+end_test
