df847b26ba602250a16f3e8c5c8e23fd3739c623
Taylor Blau <me@ttaylorr.com>
2017-02-28 13:42:31 -0800

    tools: cache cygwin support

--- tools/cygwin_windows.go
+++ tools/cygwin_windows.go
@@ -4,15 +4,50 @@ package tools
 
 import (
 	"bytes"
+	"fmt"
 
 	"github.com/git-lfs/git-lfs/subprocess"
 )
 
+type cygwinSupport byte
+
+const (
+	cygwinStateUnknown cygwinSupport = iota
+	cygwinStateEnabled
+	cygwinStateDisabled
+)
+
+func (c cygwinSupport) Enabled() bool {
+	switch c {
+	case cygwinStateEnabled:
+		return true
+	case cygwinStateDisabled:
+		return false
+	default:
+		panic(fmt.Sprintf("unknown enabled state for %v", c))
+	}
+}
+
+var (
+	cygwinState cygwinSupport
+)
+
 func isCygwin() bool {
+	if cygwinState != cygwinUnknown {
+		return cygwinState.Enabled()
+	}
+
 	cmd := subprocess.ExecCommand("uname")
 	out, err := cmd.Output()
 	if err != nil {
 		return false
 	}
-	return bytes.Contains(out, []byte("CYGWIN"))
+
+	if bytes.Contains(out, []byte("CYGWIN")) {
+		cygwinState = cygwinStateEnabled
+	} else {
+		cygwinState = cygwinStateDisabled
+	}
+
+	return cygwinState.Enabled()
 }
