b66a1069156b93c2b4638e549c2a1ae74fe39be4
Taylor Blau <me@ttaylorr.com>
2017-03-15 12:17:53 -0600

    lfs/diff_index_scanner: add documentation

--- lfs/diff_index_scanner.go
+++ lfs/diff_index_scanner.go
@@ -8,6 +8,11 @@ import (
 	"github.com/git-lfs/git-lfs/errors"
 )
 
+// Status represents the status of a file that appears in the output of `git
+// diff-index`.
+//
+// More information about each of its valid instances can be found:
+// https://git-scm.com/docs/git-diff-index
 type Status rune
 
 const (
@@ -21,6 +26,8 @@ const (
 	StatusUnknown      Status = 'X'
 )
 
+// String implements fmt.Stringer by returning a huamn-readable name for each
+// status.
 func (s Status) String() string {
 	switch s {
 	case StatusAddition:
@@ -43,24 +50,55 @@ func (s Status) String() string {
 	return "<unknown>"
 }
 
+// DiffIndexEntry holds information about a single item in the results of a `git
+// diff-index` command.
 type DiffIndexEntry struct {
-	SrcMode     string
-	DstMode     string
-	SrcSha      string
-	DstSha      string
-	Status      Status
+	// SrcMode is the file mode of the "src" file, stored as a string-based
+	// octal.
+	SrcMode string
+	// DstMode is the file mode of the "dst" file, stored as a string-based
+	// octal.
+	DstMode string
+	// SrcSha is the Git blob ID of the "src" file.
+	SrcSha string
+	// DstSha is the Git blob ID of the "dst" file.
+	DstSha string
+	// Status is the status of the file in the index.
+	Status Status
+	// StatusScore is the optional "score" assosicated with a particular
+	// status.
 	StatusScore int
-	SrcName     string
-	DstName     string
+	// SrcName is the name of the file in it's "src" state as it appears in
+	// the index.
+	SrcName string
+	// DstName is the name of the file in it's "dst" state as it appears in
+	// the index.
+	DstName string
 }
 
+// DiffIndexScanner scans the output of the `git diff-index` command.
 type DiffIndexScanner struct {
+	// next is the next entry scanned by the Scanner.
 	next *DiffIndexEntry
-	err  error
+	// err is any error that the Scanner encountered while scanning.
+	err error
 
+	// from is the underlying scanner, scanning the `git diff-index`
+	// command's stdout.
 	from *bufio.Scanner
 }
 
+// NewDiffIndexScanner initializes a new `DiffIndexScanner` scanning at the
+// given ref, "ref".
+//
+// If "cache" is given, the DiffIndexScanner will scan for differences between
+// the given ref and the index. If "cache" is _not_ given, DiffIndexScanner will
+// scan for differences between the given ref and the currently checked out
+// tree.
+//
+// If any error was encountered in starting the command or closing its `stdin`,
+// that error will be returned immediately. Otherwise, a `*DiffIndexScanner`
+// will be returned with a `nil` error.
 func NewDiffIndexScanner(ref string, cached bool) (*DiffIndexScanner, *wrappedCmd, error) {
 	cmd, err := startCommand("git", diffIndexCmdArgs(ref, cached)...)
 	if err != nil {
@@ -76,6 +114,8 @@ func NewDiffIndexScanner(ref string, cached bool) (*DiffIndexScanner, *wrappedCm
 	}, cmd, nil
 }
 
+// diffIndexCmdArgs returns a string slice containing the arguments necessary
+// to run the diff-index command.
 func diffIndexCmdArgs(ref string, cached bool) []string {
 	args := []string{"diff-index", "-M"}
 	if cached {
@@ -86,6 +126,9 @@ func diffIndexCmdArgs(ref string, cached bool) []string {
 	return args
 }
 
+// Scan advances the scan line and yields either a new value for Entry(), or an
+// Err(). It returns true or false, whether or not it can continue scanning for
+// more entries.
 func (s *DiffIndexScanner) Scan() bool {
 	if !s.prepareScan() {
 		return false
@@ -96,9 +139,14 @@ func (s *DiffIndexScanner) Scan() bool {
 	return s.err == nil
 }
 
+// Entry returns the last entry that was Scan()'d by the DiffIndexScanner.
 func (s *DiffIndexScanner) Entry() *DiffIndexEntry { return s.next }
-func (s *DiffIndexScanner) Err() error             { return s.err }
 
+// Entry returns the last error that was encountered by the DiffIndexScanner.
+func (s *DiffIndexScanner) Err() error { return s.err }
+
+// prepareScan clears out the results from the last Scan() loop, and advances
+// the internal scanner to fetch a new line of Text().
 func (s *DiffIndexScanner) prepareScan() bool {
 	s.next, s.err = nil, nil
 	if !s.from.Scan() {
@@ -109,6 +157,8 @@ func (s *DiffIndexScanner) prepareScan() bool {
 	return true
 }
 
+// scan parses the given line and returns a `*DiffIndexEntry` or an error,
+// depending on whether or not the parse was successful.
 func (s *DiffIndexScanner) scan(line string) (*DiffIndexEntry, error) {
 	parts := strings.Split(line, "\t")
 	if len(parts) < 2 {
