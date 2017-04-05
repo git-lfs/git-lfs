cfdf966d5ebb2d84028637673a7430d32cfe5286
Taylor Blau <me@ttaylorr.com>
2017-04-05 13:58:23 -0600

    tools/time: introduce IsExpiredAtOrIn to return whether an instant has expired

--- tools/time_tools.go
+++ tools/time_tools.go
@@ -2,6 +2,17 @@ package tools
 
 import "time"
 
+// IsExpiredAtOrIn returns whether or not the result of calling TimeAtOrIn is
+// "expired" within "until" units of time from now.
+func IsExpiredAtOrIn(until time.Duration, at time.Time, in time.Duration) (time.Time, bool) {
+	expiration := TimeAtOrIn(at, in)
+	if expiration.IsZero() {
+		return expiration, false
+	}
+
+	return expiration, expiration.Before(time.Now().Add(until))
+}
+
 // TimeAtOrIn returns either "at", or the "in" duration added to the current
 // time. TimeAtOrIn prefers to add a duration rather than return the "at"
 // parameter.
