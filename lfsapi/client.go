6df0d95ed2bb7bb152d788b1a1ea22677827799f
Taylor Blau <me@ttaylorr.com>
2017-05-18 15:04:59 -0600

    lfsapi/client: expand list of retry-able status codes

--- lfsapi/client.go
+++ lfsapi/client.go
@@ -130,7 +130,16 @@ func (c *Client) doWithRedirects(cli *http.Client, req *http.Request, via []*htt
 
 	c.traceResponse(req, tracedReq, res)
 
-	if res.StatusCode != 307 {
+	if res.StatusCode != 301 &&
+		res.StatusCode != 302 &&
+		res.StatusCode != 303 &&
+		res.StatusCode != 307 &&
+		res.StatusCode != 308 {
+
+		// Above are the list of 3xx status codes that we know
+		// how to handle below. If the status code contained in
+		// the HTTP response was none of them, return the (res,
+		// err) tuple as-is, otherwise handle the redirect.
 		return res, err
 	}
 
@@ -286,6 +295,8 @@ func newRequestForRetry(req *http.Request, location string) (*http.Request, erro
 	newURL := strings.SplitN(newReq.URL.String(), "?", 2)[0]
 	tracerx.Printf("api: redirect %s %s to %s", req.Method, oldestURL, newURL)
 
+	// This body will have already been rewound from a call to
+	// lfsapi.Client.traceRequest().
 	newReq.Body = req.Body
 	newReq.ContentLength = req.ContentLength
 	return newReq, nil
