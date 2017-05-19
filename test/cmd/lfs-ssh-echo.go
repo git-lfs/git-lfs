5d6ac6268dfceebe3f93d2dbbcdb2107b6504b3d
risk danger olson <technoweenie@gmail.com>
2017-05-19 09:25:22 -0600

    sanitize ssh options parsed from ssh:// url

--- test/cmd/lfs-ssh-echo.go
+++ test/cmd/lfs-ssh-echo.go
@@ -19,14 +19,29 @@ type sshResponse struct {
 
 func main() {
 	// expect args:
-	//   ssh-echo -p PORT git@127.0.0.1 git-lfs-authenticate REPO OPERATION
-	if len(os.Args) != 5 {
+	//   lfs-ssh-echo -p PORT -- git@127.0.0.1 git-lfs-authenticate REPO OPERATION
+	if len(os.Args) != 6 {
 		fmt.Fprintf(os.Stderr, "got %d args: %v", len(os.Args), os.Args)
 		os.Exit(1)
 	}
 
+	if os.Args[1] != "-p" {
+		fmt.Fprintf(os.Stderr, "$1 expected \"-p\", got %q", os.Args[1])
+		os.Exit(1)
+	}
+
+	if os.Args[3] != "--" {
+		fmt.Fprintf(os.Stderr, "$3 expected \"--\", got %q", os.Args[3])
+		os.Exit(1)
+	}
+
+	if os.Args[4] != "git@127.0.0.1" {
+		fmt.Fprintf(os.Stderr, "$4 expected \"git@127.0.0.1\", got %q", os.Args[4])
+		os.Exit(1)
+	}
+
 	// just "git-lfs-authenticate REPO OPERATION"
-	authLine := strings.Split(os.Args[4], " ")
+	authLine := strings.Split(os.Args[5], " ")
 	if len(authLine) < 13 {
 		fmt.Fprintf(os.Stderr, "bad git-lfs-authenticate line: %s\nargs: %v", authLine, os.Args)
 	}
