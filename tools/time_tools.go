cf8709c18615a8cc337a179e3002df727332de0e
Taylor Blau <me@ttaylorr.com>
2017-08-25 14:48:33 -0400

    tools: compare expiration to "time.Now" instead of "from"

--- tools/time_tools.go
+++ tools/time_tools.go
@@ -1,24 +1,26 @@
 package tools
 
-import "time"
+import (
+	"time"
+)
 
 // IsExpiredAtOrIn returns whether or not the result of calling TimeAtOrIn is
 // "expired" within "until" units of time from now.
-func IsExpiredAtOrIn(now time.Time, until time.Duration, at time.Time, in time.Duration) (time.Time, bool) {
-	expiration := TimeAtOrIn(now, at, in)
+func IsExpiredAtOrIn(from time.Time, until time.Duration, at time.Time, in time.Duration) (time.Time, bool) {
+	expiration := TimeAtOrIn(from, at, in)
 	if expiration.IsZero() {
 		return expiration, false
 	}
 
-	return expiration, expiration.Before(now.Add(until))
+	return expiration, expiration.Before(time.Now().Add(until))
 }
 
 // TimeAtOrIn returns either "at", or the "in" duration added to the current
 // time. TimeAtOrIn prefers to add a duration rather than return the "at"
 // parameter.
-func TimeAtOrIn(now, at time.Time, in time.Duration) time.Time {
+func TimeAtOrIn(from, at time.Time, in time.Duration) time.Time {
 	if in == 0 {
 		return at
 	}
-	return now.Add(in)
+	return from.Add(in)
 }
