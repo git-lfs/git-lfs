46e248b14560f944da241506e1f77276aee41fbb
Taylor Blau <me@ttaylorr.com>
2017-08-29 10:06:30 -0400

    test: clone from correct remote directory

--- test/test-clone-deprecated.sh
+++ test/test-clone-deprecated.sh
@@ -13,7 +13,7 @@ begin_test "clone (deprecated on new versions of Git)"
 
   mkdir -p "$reponame"
   pushd "$reponame" > /dev/null
-    git lfs clone "$reponame" 2>&1 | tee clone.log
+    git lfs clone "$GITSERVER/$reponame" 2>&1 | tee clone.log
     grep "WARNING: 'git lfs clone' is deprecated and will not be updated" clone.log
   popd
 )
