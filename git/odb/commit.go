246f232b0c4ab5504cd24842565111133d548d96
Taylor Blau <me@ttaylorr.com>
2017-08-30 12:02:55 -0400

    git/odb: teach '(*Commit).Equal()' to compare other commits

--- git/odb/commit.go
+++ git/odb/commit.go
@@ -2,6 +2,7 @@ package odb
 
 import (
 	"bufio"
+	"bytes"
 	"encoding/hex"
 	"fmt"
 	"io"
@@ -182,4 +183,45 @@ func (c *Commit) Encode(to io.Writer) (n int, err error) {
 	}
 
 	return n + n4, err
+}
+
+// Equal returns whether the receiving and given commits are equal, or in other
+// words, whether they are represented by the same SHA-1 when saved to the
+// object database.
+func (c *Commit) Equal(other *Commit) bool {
+	if (c == nil) != (other == nil) {
+		return false
+	}
+
+	if c != nil {
+		if len(c.ParentIDs) != len(other.ParentIDs) {
+			return false
+		}
+		for i := 0; i < len(c.ParentIDs); i++ {
+			p1 := c.ParentIDs[i]
+			p2 := other.ParentIDs[i]
+
+			if !bytes.Equal(p1, p2) {
+				return false
+			}
+		}
+
+		if len(c.ExtraHeaders) != len(other.ExtraHeaders) {
+			return false
+		}
+		for i := 0; i < len(c.ExtraHeaders); i++ {
+			e1 := c.ExtraHeaders[i]
+			e2 := other.ExtraHeaders[i]
+
+			if e1.K != e2.K || e1.V != e2.V {
+				return false
+			}
+		}
+
+		return c.Author == other.Author &&
+			c.Committer == other.Committer &&
+			c.Message == other.Message &&
+			bytes.Equal(c.TreeID, other.TreeID)
+	}
+	return true
 }
