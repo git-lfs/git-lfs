f5e2c936e96683b63dd045dbdd69bba4cb3947bd
Sebastian Schuberth <sschuberth@gmail.com>
2017-09-15 09:06:32 +0200

    compile-win-installer-unsigned: fix the name of the inno setup script

--- script/compile-win-installer-unsigned.bat
+++ script/compile-win-installer-unsigned.bat
@@ -1,1 +1,1 @@
-"%ProgramFiles(x86)%\Inno Setup 5\iscc.exe" /Qp "%~dp0\windows-installer\git-lfs-installer.iss"
+"%ProgramFiles(x86)%\Inno Setup 5\iscc.exe" /Qp "%~dp0\windows-installer\inno-setup-git-lfs-installer.iss"
