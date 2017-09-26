e53e34e3ce957a763a55b6295cfe87e76ab6f858
rick olson <technoweenie@gmail.com>
2017-09-26 11:26:57 -0600

    skip SSH_ASKPASS test in travis

--- test/test-askpass.sh
+++ test/test-askpass.sh
@@ -69,6 +69,14 @@ begin_test "askpass: push with SSH_ASKPASS"
 (
   set -e
 
+  if [ ! -z "$TRAVIS" ] ; then
+    # This test is known to be broken on Travis, so we skip it if the $TRAVIS
+    # environment variable is set.
+    #
+    # See: https://github.com/git-lfs/git-lfs/pull/2500 for more.
+    exit 0
+  fi
+
   reponame="askpass-with-ssh-environ"
   setup_remote_repo "$reponame"
   clone_repo "$reponame" "$reponame"
