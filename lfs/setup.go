8c0bd1e3c595ecf4f72fabd3b79ee6724ad81a5d
rick olson <technoweenie@gmail.com>
2017-10-18 15:28:42 -0600

    lfs: load Hook config on demand

--- lfs/setup.go
+++ lfs/setup.go
@@ -7,30 +7,24 @@ import (
 	"github.com/git-lfs/git-lfs/tools"
 )
 
-var (
-	// prePushHook invokes `git lfs pre-push` at the pre-push phase.
-	prePushHook = NewStandardHook("pre-push", []string{
-		"#!/bin/sh\ngit lfs push --stdin $*",
-		"#!/bin/sh\ngit lfs push --stdin \"$@\"",
-		"#!/bin/sh\ngit lfs pre-push \"$@\"",
-		"#!/bin/sh\ncommand -v git-lfs >/dev/null 2>&1 || { echo >&2 \"\\nThis repository has been set up with Git LFS but Git LFS is not installed.\\n\"; exit 0; }\ngit lfs pre-push \"$@\"",
-		"#!/bin/sh\ncommand -v git-lfs >/dev/null 2>&1 || { echo >&2 \"\\nThis repository has been set up with Git LFS but Git LFS is not installed.\\n\"; exit 2; }\ngit lfs pre-push \"$@\"",
-	})
-	// postCheckoutHook invokes `git lfs post-checkout`
-	postCheckoutHook = NewStandardHook("post-checkout", []string{})
-	postCommitHook   = NewStandardHook("post-commit", []string{})
-	postMergeHook    = NewStandardHook("post-merge", []string{})
-
-	hooks = []*Hook{
-		prePushHook,
-		postCheckoutHook,
-		postCommitHook,
-		postMergeHook,
+func loadHooks() []*Hook {
+	return []*Hook{
+		NewStandardHook("pre-push", []string{
+			"#!/bin/sh\ngit lfs push --stdin $*",
+			"#!/bin/sh\ngit lfs push --stdin \"$@\"",
+			"#!/bin/sh\ngit lfs pre-push \"$@\"",
+			"#!/bin/sh\ncommand -v git-lfs >/dev/null 2>&1 || { echo >&2 \"\\nThis repository has been set up with Git LFS but Git LFS is not installed.\\n\"; exit 0; }\ngit lfs pre-push \"$@\"",
+			"#!/bin/sh\ncommand -v git-lfs >/dev/null 2>&1 || { echo >&2 \"\\nThis repository has been set up with Git LFS but Git LFS is not installed.\\n\"; exit 2; }\ngit lfs pre-push \"$@\"",
+		}),
+		NewStandardHook("post-checkout", []string{}),
+		NewStandardHook("post-commit", []string{}),
+		NewStandardHook("post-merge", []string{}),
 	}
-)
+}
 
 // Get user-readable manual install steps for hooks
 func GetHookInstallSteps() string {
+	hooks := loadHooks()
 	steps := make([]string, 0, len(hooks))
 	for _, h := range hooks {
 		steps = append(steps, fmt.Sprintf(
@@ -41,8 +35,8 @@ func GetHookInstallSteps() string {
 	return strings.Join(steps, "\n\n")
 }
 
-// InstallHooks installs all hooks in the `hooks` var.
 func InstallHooks(force bool) error {
+	hooks := loadHooks()
 	for _, h := range hooks {
 		if err := h.Install(force); err != nil {
 			return err
@@ -54,6 +48,7 @@ func InstallHooks(force bool) error {
 
 // UninstallHooks removes all hooks in range of the `hooks` var.
 func UninstallHooks() error {
+	hooks := loadHooks()
 	for _, h := range hooks {
 		if err := h.Uninstall(); err != nil {
 			return err
