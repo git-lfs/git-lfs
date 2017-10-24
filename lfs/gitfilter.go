03260e0ef0bd66ad0f71964fafde4035a908b470
rick olson <technoweenie@gmail.com>
2017-10-24 13:54:09 -0600

    basic godoc

--- lfs/gitfilter.go
+++ lfs/gitfilter.go
@@ -4,10 +4,12 @@ import (
 	"github.com/git-lfs/git-lfs/config"
 )
 
+// GitFilter provides clean and smudge capabilities
 type GitFilter struct {
 	cfg *config.Configuration
 }
 
+// NewGitFilter initializes a new *GitFilter
 func NewGitFilter(cfg *config.Configuration) *GitFilter {
 	return &GitFilter{cfg: cfg}
 }
