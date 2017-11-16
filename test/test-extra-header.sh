e0064950e74397a2a8662133f165a972eb5fe2c1
Taylor Blau <me@ttaylorr.com>
2017-11-16 12:04:42 -0800

    lfsapi/auth: teach DoWithAuth to respect http.extraHeaders

--- test/test-extra-header.sh
+++ test/test-extra-header.sh
@@ -25,3 +25,37 @@ begin_test "http.<url>.extraHeader"
   grep "> X-Foo: baz" curl.log
 )
 end_test
+
+begin_test "http.<url>.extraHeader with authorization"
+(
+  set -e
+
+  reponame="requirecreds"
+  setup_remote_repo "$reponame"
+  clone_repo "$reponame" "$reponame"
+
+  # See: test/cmd/lfstest-gitserver.go:1176.
+  user="requirecreds"
+  pass="pass"
+  auth="Basic $(echo -n $user:$pass | base64)"
+
+  git config --add "http.extraHeader" "Authorization: $auth"
+
+  git lfs track "*.dat"
+  printf "contents" > a.dat
+  git add .gitattributes a.dat
+  git commit -m "initial commit"
+
+  git push origin master 2>&1 | tee curl.log
+  if [ "0" -ne "${PIPESTATUS[0]}" ]; then
+    echo >&2 "expected \`git push origin master\` to succeed, didn't"
+    exit 1
+  fi
+
+  [ "0" -eq "$(grep -c "creds: filling with GIT_ASKPASS" curl.log)" ]
+  [ "0" -eq "$(grep -c "creds: git credential approve" curl.log)" ]
+  [ "0" -eq "$(grep -c "creds: git credential cache" curl.log)" ]
+  [ "0" -eq "$(grep -c "creds: git credential fill" curl.log)" ]
+  [ "0" -eq "$(grep -c "creds: git credential reject" curl.log)" ]
+)
+end_test
