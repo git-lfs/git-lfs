50ac6d256bc2674135b2fbf1c585b8803e332d3e
Taylor Blau <me@ttaylorr.com>
2018-02-26 18:13:59 -0800

    git/githistory: copy entries from cache, elsewhere

--- git/githistory/rewriter.go
+++ git/githistory/rewriter.go
@@ -314,12 +314,12 @@ func (r *Rewriter) rewriteTree(commitOID []byte, treeOID []byte, path string, fn
 		path := filepath.Join(path, entry.Name)
 
 		if !r.allows(entry.Type(), path) {
-			entries = append(entries, entry)
+			entries = append(entries, copyEntry(entry))
 			continue
 		}
 
 		if cached := r.uncacheEntry(entry); cached != nil {
-			entries = append(entries, cached)
+			entries = append(entries, copyEntry(cached))
 			continue
 		}
 
@@ -354,6 +354,21 @@ func (r *Rewriter) rewriteTree(commitOID []byte, treeOID []byte, path string, fn
 		return treeOID, nil
 	}
 	return r.db.WriteTree(rewritten)
+}
+
+func copyEntry(e *odb.TreeEntry) *odb.TreeEntry {
+	if e == nil {
+		return nil
+	}
+
+	oid := make([]byte, len(e.Oid))
+	copy(oid, e.Oid)
+
+	return &odb.TreeEntry{
+		Filemode: e.Filemode,
+		Name:     e.Name,
+		Oid:      oid,
+	}
 }
 
 func (r *Rewriter) allows(typ odb.ObjectType, abs string) bool {
