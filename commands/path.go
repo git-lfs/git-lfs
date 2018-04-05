712769b37c610a88fae6a3ed104120c998b1ac77
Taylor Blau <me@ttaylorr.com>
2018-04-05 12:17:37 -0700

    commands/path.go: add documentation to trimCurrentPrefix

--- commands/path.go
+++ commands/path.go
@@ -17,6 +17,12 @@ const (
 	nixPrefix     = `./`
 )
 
+// trimCurrentPrefix removes a leading prefix of "./" or ".\" (referring to the
+// current directory in a platform independent manner).
+//
+// It is useful for callers such as "git lfs track" and "git lfs untrack", that
+// wish to compare filepaths and/or attributes patterns without cleaning across
+// multiple platforms.
 func trimCurrentPrefix(p string) string {
 	if strings.HasPrefix(p, windowsPrefix) {
 		return strings.TrimPrefix(p, windowsPrefix)
