c41afd5b29107f506986e136dbd8a352fcf7ee05
Taylor Blau <me@ttaylorr.com>
2018-07-13 13:05:19 -0500

    t: import t/cmd/util in t/git-lfs-test-server-api/main.go

--- t/git-lfs-test-server-api/main.go
+++ t/git-lfs-test-server-api/main.go
@@ -13,6 +13,7 @@ import (
 	"github.com/git-lfs/git-lfs/errors"
 	"github.com/git-lfs/git-lfs/fs"
 	"github.com/git-lfs/git-lfs/lfsapi"
+	t "github.com/git-lfs/git-lfs/t/cmd/util"
 	"github.com/git-lfs/git-lfs/tasklog"
 	"github.com/git-lfs/git-lfs/tq"
 	"github.com/spf13/cobra"
