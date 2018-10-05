4cf30300ee5ab5dca49d8626a21a0a7a19265954
brian m. carlson <bk2204@github.com>
2018-10-05 15:34:28 +0000

    config: add a umask lookup function
    On Windows, syscall.Umask doesn't exist, and trying to compile with it
    doesn't work.  Add two files, controlled by build tags, that return the
    umask on Unix systems and return a no-op default on Windows, so that we
    can compile on all platforms.

---
+++ config/util_nix.go
@@ -0,0 +1,13 @@
+// +build !windows
+
+package config
+
+import "syscall"
+
+func umask() int {
+	// umask(2), which this function wraps, also sets the umask, so set it
+	// back.
+	umask := syscall.Umask(022)
+	syscall.Umask(umask)
+	return umask
+}
