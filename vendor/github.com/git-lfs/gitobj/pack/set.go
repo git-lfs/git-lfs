f0378de47950ae6b41353cd6cd17720cd5a5556c
Taylor Blau <me@ttaylorr.com>
2019-03-11 11:04:26 -0700

    go.mod: bump gitobj to v1.2.0

--- vendor/github.com/git-lfs/gitobj/pack/set.go
+++ vendor/github.com/git-lfs/gitobj/pack/set.go
@@ -55,12 +55,21 @@ func NewSet(db string) (*Set, error) {
 
 		name := submatch[1]
 
-		packf, err := os.Open(filepath.Join(pd, fmt.Sprintf("pack-%s.pack", name)))
+		idxf, err := os.Open(filepath.Join(pd, fmt.Sprintf("pack-%s.idx", name)))
 		if err != nil {
-			return nil, err
+			// We have a pack (since it matched the regex), but the
+			// index is missing or unusable.  Skip this pack and
+			// continue on with the next one, as Git does.
+			if idxf != nil {
+				// In the unlikely event that we did open a
+				// file, close it, but discard any error in
+				// doing so.
+				idxf.Close()
+			}
+			continue
 		}
 
-		idxf, err := os.Open(filepath.Join(pd, fmt.Sprintf("pack-%s.idx", name)))
+		packf, err := os.Open(filepath.Join(pd, fmt.Sprintf("pack-%s.pack", name)))
 		if err != nil {
 			return nil, err
 		}
