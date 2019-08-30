391689b02e80bef335112cfe1a5517a06787a684
brian m. carlson <bk2204@github.com>
2019-08-30 18:55:11 +0000

    t: add a test helper equivalent to realpath(1)
    On Actions, our Windows build root has a path with a short name.  This
    causes significant grief, since our tests expect one version of the path
    and we output another, causing our tests for equality to fail.  Since
    Windows lacks a realpath(1) utility, provide a small wrapper around the
    Go primitives to provide a limited version of this utility for use only
    in our tests.

---
+++ t/cmd/lfstest-realpath.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func canonicalize(path string) (string, error) {
+	left := path
+	right := ""
+
+	for {
+		canon, err := filepath.EvalSymlinks(left)
+		if err != nil && !os.IsNotExist(err) {
+			return "", err
+		}
+		if err == nil {
+			if right == "" {
+				return canon, nil
+			}
+			return filepath.Join(canon, right), nil
+		}
+		// One component of our path is missing.  Let's walk up a level
+		// and canonicalize that and then append the remaining piece.
+		full := filepath.Join(left, right)
+		if right == "" {
+			full = left
+		}
+		newleft := filepath.Clean(fmt.Sprintf("%s%c..", left, os.PathSeparator))
+		newright, err := filepath.Rel(newleft, full)
+		if err != nil {
+			return "", err
+		}
+		left = newleft
+		right = newright
+	}
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s PATH\n", os.Args[0])
+		os.Exit(2)
+	}
+
+	path, err := filepath.Abs(os.Args[1])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating absolute path: %v", err)
+		os.Exit(3)
+	}
+
+	fullpath, err := canonicalize(path)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error canonicalizing: %v", err)
+		os.Exit(4)
+	}
+
+	fmt.Println(fullpath)
+}
