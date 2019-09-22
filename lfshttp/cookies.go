bb0adf4f2b519fba779c91e165dce2959283252a
Rafal Kupka <324064+kupson@users.noreply.github.com>
2019-09-22 10:58:33 +0100

    Allow tilde expansion in http.cookieFile config option

--- lfshttp/cookies.go
+++ lfshttp/cookies.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/git-lfs/git-lfs/tools"
 	"github.com/google/slothfs/cookie"
 )
 
@@ -16,5 +17,10 @@ func isCookieJarEnabledForHost(c *Client, host string) bool {
 func getCookieJarForHost(c *Client, host string) (http.CookieJar, error) {
 	cookieFile, _ := c.uc.Get("http", fmt.Sprintf("https://%v", host), "cookieFile")
 
-	return cookie.NewJar(cookieFile)
+	cookieFilePath, err := tools.ExpandPath(cookieFile, false)
+	if err != nil {
+		return nil, err
+	}
+
+	return cookie.NewJar(cookieFilePath)
 }
