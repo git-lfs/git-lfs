git-lfs/git-lfs

commit 1412d6e47af351d678f50cf9a5d6e0bc9c92c919

Don't fail if we lack objects the server has
A Git LFS client may not have the entire history of the objects for the
repository. However, in some situations, we traverse the entire history
of a branch when pushing it, meaning that we need to process every
LFS object in the history of that branch. If the objects for the entire
history are not present, we currently fail to push.

Instead, let's mark objects we don't have on disk as missing and only
fail when we would need to upload those objects. We'll know the server
has the objects if the batch response provides no actions to take for
them when we request an upload. Pass the missing flag down through the
code, and always set it to false for non-uploads.

If for some reason we fail to properly flag a missing object, we will
still fail later on when we cannot open the file, just in a messier and
more poorly controlled way. The technique used here will attempt to
abort the batch as soon as we notice a problem, which means that in the
common case (less than 100 objects) we won't have transferred any
objects, so the user can notice the failure as soon as possible.

Update the tests to look for a string which will occur in the error
message, since we no longer produce the system error message for ENOENT.

tq/transfer_queue.go
@@ -114,7 +114,7 @@ func (b batch) Concat(other batch, size int) (left, right batch, minWait time.Du
 func (b batch) ToTransfers() []*Transfer {
 	transfers := make([]*Transfer, 0, len(b))
 	for _, t := range b {
-		transfers = append(transfers, &Transfer{Oid: t.Oid, Size: t.Size})
+		transfers = append(transfers, &Transfer{Oid: t.Oid, Size: t.Size, Missing: t.Missing})
 	}
 	return transfers
 }
@@ -191,15 +191,17 @@ func (s *objects) First() *objectTuple {
 type objectTuple struct {
 	Name, Path, Oid string
 	Size            int64
+	Missing         bool
 	ReadyTime       time.Time
 }
 
 func (o *objectTuple) ToTransfer() *Transfer {
 	return &Transfer{
-		Name: o.Name,
-		Path: o.Path,
-		Oid:  o.Oid,
-		Size: o.Size,
+		Name:    o.Name,
+		Path:    o.Path,
+		Oid:     o.Oid,
+		Size:    o.Size,
+		Missing: o.Missing,
 	}
 }
 
@@ -284,12 +286,13 @@ func NewTransferQueue(dir Direction, manifest *Manifest, remote string, options 
 //
 // Only one file will be transferred to/from the Path element of the first
 // transfer.
-func (q *TransferQueue) Add(name, path, oid string, size int64) {
+func (q *TransferQueue) Add(name, path, oid string, size int64, missing bool) {
 	t := &objectTuple{
-		Name: name,
-		Path: path,
-		Oid:  oid,
-		Size: size,
+		Name:    name,
+		Path:    path,
+		Oid:     oid,
+		Size:    size,
+		Missing: missing,
 	}
 
 	if objs := q.remember(t); len(objs.objects) > 1 {
@@ -379,10 +382,10 @@ func (q *TransferQueue) collectBatches() {
 		done := make(chan struct{})
 
 		var retries batch
+		var err error
 
 		go func() {
 			defer close(done)
-			var err error
 
 			if len(next) == 0 {
 				return
@@ -396,6 +399,12 @@ func (q *TransferQueue) collectBatches() {
 
 		var collected batch
 		collected, closing = q.collectPendingUntil(done)
+
+		// If we've encountered a serious error here, abort immediately;
+		// don't process further batches.
+		if err != nil {
+			break
+		}
 
 		// Ensure the next batch is filled with, in order:
 		//
@@ -457,7 +466,7 @@ func (q *TransferQueue) enqueueAndCollectRetriesFor(batch batch) (batch, error) 
 		// Trust the external transfer agent can do everything by itself.
 		objects := make([]*Transfer, 0, len(batch))
 		for _, t := range batch {
-			objects = append(objects, &Transfer{Oid: t.Oid, Size: t.Size, Path: t.Path})
+			objects = append(objects, &Transfer{Oid: t.Oid, Size: t.Size, Path: t.Path, Missing: t.Missing})
 		}
 		bRes = &BatchResponse{
 			Objects:             objects,
@@ -488,12 +497,38 @@ func (q *TransferQueue) enqueueAndCollectRetriesFor(batch batch) (batch, error) 
 				}
 			}
 
+			if err != nil && bRes != nil {
+				// Avoid a hang if we return early.
+				q.wait.Add(-len(bRes.Objects))
+			}
+
 			return next, err
 		}
 	}
 
 	if len(bRes.Objects) == 0 {
 		return next, nil
+	}
+
+	// We check first that all of the objects we want to upload are present,
+	// and abort if any are missing. We'll never have any objects marked as
+	// missing except possibly on upload, so just skip iterating over the
+	// objects in that case.
+	if q.direction == Upload {
+		for _, o := range bRes.Objects {
+			// If the server already has the object, the list of
+			// actions will be empty. It's fine if the file is
+			// missing in that case, since we don't need to upload
+			// it.
+			if o.Missing && len(o.Actions) != 0 {
+				// Indicate that we've handled these objects, in
+				// this case by ignoring them and aborting
+				// early. Failing to do this means we deadlock
+				// on this WaitGroup.
+				q.wait.Add(-len(bRes.Objects))
+				return nil, errors.Errorf("Unable to find source for object %v (try running git lfs fetch --all)", o.Oid)
+			}
+		}
 	}
 
 	q.useAdapter(bRes.TransferAdapterName)

commands/command_migrate_export.go
@@ -136,7 +136,7 @@ func migrateExportCommand(cmd *cobra.Command, args []string) {
 			}
 
 			if _, err := os.Stat(downloadPath); os.IsNotExist(err) {
-				q.Add(p.Name, downloadPath, p.Oid, p.Size)
+				q.Add(p.Name, downloadPath, p.Oid, p.Size, false)
 			}
 		})
 		gs.ScanRefs(opts.Include, opts.Exclude, nil)

commands/command_smudge.go
@@ -59,7 +59,7 @@ func delayedSmudge(gf *lfs.GitFilter, s *git.FilterProcessScanner, to io.Writer,
 
 	if !skip && filter.Allows(filename) {
 		if _, statErr := os.Stat(path); statErr != nil {
-			q.Add(filename, path, ptr.Oid, ptr.Size)
+			q.Add(filename, path, ptr.Oid, ptr.Size, false)
 			return 0, true, ptr, nil
 		}
 

commands/commands.go
@@ -131,9 +131,9 @@ func buildFilepathFilter(config *config.Configuration, includeArg, excludeArg *s
 	return filepathfilter.New(inc, exc)
 }
 
-func downloadTransfer(p *lfs.WrappedPointer) (name, path, oid string, size int64) {
+func downloadTransfer(p *lfs.WrappedPointer) (name, path, oid string, size int64, missing bool) {
 	path, _ = cfg.Filesystem().ObjectPath(p.Oid)
-	return p.Name, path, p.Oid, p.Size
+	return p.Name, path, p.Oid, p.Size, false
 }
 
 // Get user-readable manual install steps for hooks

commands/uploader.go
@@ -246,7 +246,7 @@ func (c *uploadContext) UploadPointers(q *tq.TransferQueue, unfiltered ...*lfs.W
 			ExitWithError(err)
 		}
 
-		q.Add(t.Name, t.Path, t.Oid, t.Size)
+		q.Add(t.Name, t.Path, t.Oid, t.Size, t.Missing)
 		c.SetUploaded(p.Oid)
 	}
 }
@@ -338,6 +338,8 @@ var (
 )
 
 func (c *uploadContext) uploadTransfer(p *lfs.WrappedPointer) (*tq.Transfer, error) {
+	var missing bool
+
 	filename := p.Name
 	oid := p.Oid
 
@@ -347,40 +349,38 @@ func (c *uploadContext) uploadTransfer(p *lfs.WrappedPointer) (*tq.Transfer, err
 	}
 
 	if len(filename) > 0 {
-		if err = c.ensureFile(filename, localMediaPath, oid); err != nil && !errors.IsCleanPointerError(err) {
+		if missing, err = c.ensureFile(filename, localMediaPath, oid); err != nil && !errors.IsCleanPointerError(err) {
 			return nil, err
 		}
 	}
 
 	return &tq.Transfer{
-		Name: filename,
-		Path: localMediaPath,
-		Oid:  oid,
-		Size: p.Size,
+		Name:    filename,
+		Path:    localMediaPath,
+		Oid:     oid,
+		Size:    p.Size,
+		Missing: missing,
 	}, nil
 }
 
 // ensureFile makes sure that the cleanPath exists before pushing it.  If it
 // does not exist, it attempts to clean it by reading the file at smudgePath.
-func (c *uploadContext) ensureFile(smudgePath, cleanPath, oid string) error {
+func (c *uploadContext) ensureFile(smudgePath, cleanPath, oid string) (bool, error) {
 	if _, err := os.Stat(cleanPath); err == nil {
-		return nil
+		return false, nil
 	}
 
 	localPath := filepath.Join(cfg.LocalWorkingDir(), smudgePath)
 	file, err := os.Open(localPath)
 	if err != nil {
-		if c.allowMissing {
-			return nil
-		}
-		return errors.Wrapf(err, "Unable to find source for object %v (try running git lfs fetch --all)", oid)
+		return !c.allowMissing, nil
 	}
 
 	defer file.Close()
 
 	stat, err := file.Stat()
 	if err != nil {
-		return err
+		return false, err
 	}
 
 	cleaned, err := c.gitfilter.Clean(file, file.Name(), stat.Size(), nil)
@@ -389,9 +389,9 @@ func (c *uploadContext) ensureFile(smudgePath, cleanPath, oid string) error {
 	}
 
 	if err != nil {
-		return err
+		return false, err
 	}
-	return nil
+	return false, nil
 }
 
 // supportsLockingAPI returns whether or not a given url is known to support

lfs/gitfilter_smudge.go
@@ -96,7 +96,7 @@ func (f *GitFilter) downloadFile(writer io.Writer, ptr *Pointer, workingfile, me
 		tq.WithProgressCallback(cb),
 		tq.RemoteRef(f.RemoteRef()),
 	)
-	q.Add(filepath.Base(workingfile), mediafile, ptr.Oid, ptr.Size)
+	q.Add(filepath.Base(workingfile), mediafile, ptr.Oid, ptr.Size, false)
 	q.Wait()
 
 	if errs := q.Errors(); len(errs) > 0 {

t/git-lfs-test-server-api/main.go
@@ -206,7 +206,7 @@ func buildTestData(repo *t.Repo, manifest *tq.Manifest) (oidsExist, oidsMissing 
 		if err != nil {
 			return nil, nil, err
 		}
-		uploadQueue.Add(t.Name, t.Path, t.Oid, t.Size)
+		uploadQueue.Add(t.Name, t.Path, t.Oid, t.Size, false)
 	}
 	uploadQueue.Wait()
 

t/t-pre-push.sh
@@ -412,8 +412,7 @@ begin_test "pre-push reject missing pointers (lfs.allowincompletepush default)"
     exit 1
   fi
 
-  grep "no such file or directory" push.log || # unix
-    grep "cannot find the file" push.log       # windows
+  grep 'Unable to find source' push.log
 
   refute_server_object "$reponame" "$present_oid"
   refute_server_object "$reponame" "$missing_oid"

commit 1157c0df538d1666ae5002f8a1915713943a3e65

tq: retry batch failures
If the batch operation fails due to an error instead of a bad HTTP
status code, we'll abort the batch operation and retry.  This appears to
be a regression from 1412d6e4 ("Don't fail if we lack objects the server
has", 2019-04-30), which caused us to handle errors differently.

Since there are two error returns from enqueueAndCollectRetriesFor,
let's wrap the batch error case as a retriable error and not abort if we
find a retriable error later on.  This lets us continue to abort if we
get a missing object, which should be fatal, but retry in the more
common network failure case.

tq/transfer_queue.go
@@ -442,7 +442,7 @@ func (q *TransferQueue) collectBatches() {
 		// don't process further batches.  Abort the wait queue so that
 		// we don't deadlock waiting for objects to complete when they
 		// never will.
-		if err != nil {
+		if err != nil && !errors.IsRetriableError(err) {
 			q.wait.Abort()
 			break
 		}
@@ -538,7 +538,7 @@ func (q *TransferQueue) enqueueAndCollectRetriesFor(batch batch) (batch, error) 
 				}
 			}
 
-			return next, err
+			return next, errors.NewRetriableError(err)
 		}
 	}
 