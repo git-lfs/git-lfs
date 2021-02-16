ab94bbfa24e13c97dfcc23514314ddf11d870112
brian m. carlson <bk2204@github.com>
2021-02-16 22:29:25 +0000

    ssh: create a traceable pkt-line interface
    One thing that Git provides is a traceable pkt-line interface when
    GIT_TRACE_PACKET=1 is provided.  Let's add a wrapper around the text
    parts of the pkt-line interface so we can trace what's happening and
    provide helpful debug output.

---
+++ ssh/pktline.go
@@ -0,0 +1,86 @@
+package ssh
+
+import (
+	"io"
+
+	"github.com/git-lfs/pktline"
+	"github.com/rubyist/tracerx"
+)
+
+func pktlineReader(p Pktline) io.Reader {
+	if pl, ok := p.(*pktline.Pktline); ok {
+		return pktline.NewPktlineReaderFromPktline(pl, 65536)
+	}
+	tp := p.(*TraceablePktline)
+	return pktline.NewPktlineReaderFromPktline(tp.pl, 65536)
+}
+
+type Pktline interface {
+	ReadPacketList() ([]string, error)
+	ReadPacketTextWithLength() (string, int, error)
+	WritePacket([]byte) error
+	WritePacketText(string) error
+	WriteDelim() error
+	WriteFlush() error
+}
+
+type TraceablePktline struct {
+	pl *pktline.Pktline
+}
+
+func (tp *TraceablePktline) ReadPacketList() ([]string, error) {
+	var list []string
+	for {
+		data, pktLen, err := tp.pl.ReadPacketTextWithLength()
+		if err != nil {
+			return nil, err
+		}
+		if pktLen <= 1 {
+			tracerx.Printf("packet < %04x", pktLen)
+		} else {
+			tracerx.Printf("packet < %s", data)
+		}
+
+		if pktLen == 0 {
+			break
+		}
+
+		list = append(list, data)
+	}
+
+	return list, nil
+}
+
+func (tp *TraceablePktline) ReadPacketTextWithLength() (string, int, error) {
+	s, pktLen, err := tp.pl.ReadPacketTextWithLength()
+	if err != nil {
+		return "", 0, err
+	}
+
+	if pktLen <= 1 {
+		tracerx.Printf("packet < %04x", pktLen)
+	} else {
+		tracerx.Printf("packet < %s", s)
+	}
+	return s, pktLen, nil
+}
+
+func (tp *TraceablePktline) WritePacket(b []byte) error {
+	// Don't trace because this is probably binary data.
+	return tp.pl.WritePacket(b)
+}
+
+func (tp *TraceablePktline) WritePacketText(s string) error {
+	tracerx.Printf("packet > %s", s)
+	return tp.pl.WritePacketText(s)
+}
+
+func (tp *TraceablePktline) WriteDelim() error {
+	tracerx.Printf("packet > 0001")
+	return tp.pl.WriteDelim()
+}
+
+func (tp *TraceablePktline) WriteFlush() error {
+	tracerx.Printf("packet > 0000")
+	return tp.pl.WriteFlush()
+}
