d452e3a83ab7aa209cbcb939e573941fb5938f21
brian m. carlson <bk2204@github.com>
2021-07-26 17:24:34 +0000

    lfs/util: remove obsolete comment
    This comment mentions using a channel here, but we haven't used one in
    this code path in a long time.  Let's remove the obsolete comment now
    that we're just using a plain slice.

--- lfs/util.go
+++ lfs/util.go
@@ -97,7 +97,6 @@ type PathConverter interface {
 // Convert filenames expressed relative to the root of the repo relative to the
 // current working dir. Useful when needing to calling git with results from a rooted command,
 // but the user is in a subdir of their repo
-// Pass in a channel which you will fill with relative files & receive a channel which will get results
 func NewRepoToCurrentPathConverter(cfg *config.Configuration) (PathConverter, error) {
 	r, c, p, err := pathConverterArgs(cfg)
 	if err != nil {
@@ -134,7 +133,6 @@ func (p *repoToCurrentPathConverter) Convert(filename string) string {
 // Convert filenames expressed relative to the current directory to be
 // relative to the repo root. Useful when calling git with arguments that requires them
 // to be rooted but the user is in a subdir of their repo & expects to use relative args
-// Pass in a channel which you will fill with relative files & receive a channel which will get results
 func NewCurrentToRepoPathConverter(cfg *config.Configuration) (PathConverter, error) {
 	r, c, p, err := pathConverterArgs(cfg)
 	if err != nil {
