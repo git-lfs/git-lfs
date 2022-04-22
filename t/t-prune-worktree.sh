e0f359ea29e181e5ca970f49a0b52a3ce787b5cb
Chris Darroch <chrisd8088@github.com>
2022-04-21 23:34:29 -0700

    t/t-prune-worktree.sh: check excluded files
    The "prune worktree" test ensures that Git LFS objects
    referenced by worktrees are always retained when the "git lfs
    prune" command is run.
    
    However, it does not at present check that objects belonging to
    files which match a "lfs.fetchexclude" pattern are pruned even
    when they are referenced by a commit at the HEAD of a worktree.
    Logic in the "git lfs prune" command to prune objects from files
    matching the "lfs.fetchexclude" filter was added in PR #2851,
    and it should demonstrate this behaviour, but no checks actually
    confirm it.
    
    We therefore expand the "prune worktree" test so that its
    simulated commit history includes objects in each worktree's
    branch whose file path matches a path we then exclude using the
    "lfs.fetchexclude" filter.  We then confirm that these objects
    are pruned by "git lfs prune", despite being referenced by the
    worktrees' HEAD refs.

--- t/t-prune-worktree.sh
+++ t/t-prune-worktree.sh
@@ -18,15 +18,19 @@ begin_test "prune worktree"
 
   content_head="First checkout HEAD"
   content_worktree1head="Worktree 1 head"
+  content_worktree1excluded="Worktree 1 excluded by filter"
   content_worktree2head="Worktree 2 head"
+  content_worktree2excluded="Worktree 2 excluded by filter"
   content_oldcommit1="Always pruned 1"
   content_oldcommit2="Always pruned 2"
   content_oldcommit3="Always pruned 3"
 
   oid_head=$(calc_oid "$content_head")
   oid_worktree1head=$(calc_oid "$content_worktree1head")
+  oid_worktree1excluded=$(calc_oid "$content_worktree1excluded")
   oid_worktree2head=$(calc_oid "$content_worktree2head")
-  oid_oldcommit1=$(calc_oid "$content_oldcommit1")
+  oid_worktree2excluded=$(calc_oid "$content_worktree2excluded")
+  oid_oldcommit1=$(calc_oid "$content_oldcommit1"])
   oid_oldcommit2=$(calc_oid "$content_oldcommit2")
   oid_oldcommit3=$(calc_oid "$content_oldcommit3")
 
@@ -45,7 +49,8 @@ begin_test "prune worktree"
   {
     \"CommitDate\":\"$(get_date -20d)\",
     \"Files\":[
-      {\"Filename\":\"file.dat\",\"Size\":${#content_worktree1head}, \"Data\":\"$content_worktree1head\"}]
+      {\"Filename\":\"file.dat\",\"Size\":${#content_worktree1head}, \"Data\":\"$content_worktree1head\"},
+      {\"Filename\":\"foo/file.dat\",\"Size\":${#content_worktree1excluded}, \"Data\":\"$content_worktree1excluded\"}]
   },
   {
     \"CommitDate\":\"$(get_date -30d)\",
@@ -57,7 +62,8 @@ begin_test "prune worktree"
   {
     \"CommitDate\":\"$(get_date -15d)\",
     \"Files\":[
-      {\"Filename\":\"file.dat\",\"Size\":${#content_worktree2head}, \"Data\":\"$content_worktree2head\"}]
+      {\"Filename\":\"file.dat\",\"Size\":${#content_worktree2head}, \"Data\":\"$content_worktree2head\"},
+      {\"Filename\":\"foo/file.dat\",\"Size\":${#content_worktree2excluded}, \"Data\":\"$content_worktree2excluded\"}]
   },
   {
     \"CommitDate\":\"$(get_date -30d)\",
@@ -75,31 +81,34 @@ begin_test "prune worktree"
   git config lfs.fetchrecentremoterefs true
   git config lfs.fetchrecentcommitsdays 0
 
+  # We need to prevent MSYS from rewriting /foo into a Windows path.
+  MSYS_NO_PATHCONV=1 git config "lfs.fetchexclude" "/foo/**"
+
   # before worktree, everything except current checkout would be pruned
   git lfs prune --dry-run 2>&1 | tee prune.log
-  grep "prune: 6 local objects, 1 retained, done." prune.log
-  grep "prune: 5 files would be pruned" prune.log
+  grep "prune: 8 local objects, 1 retained, done." prune.log
+  grep "prune: 7 files would be pruned" prune.log
 
   # now add worktrees on the other branches
   git worktree add "../w1_$reponame" "branch1"
   git worktree add "../w2_$reponame" "branch2"
-  # now should retain all 3 heads
+  # now should retain all 3 heads except for paths excluded by filter
   git lfs prune --dry-run 2>&1 | tee prune.log
-  grep "prune: 6 local objects, 3 retained, done." prune.log
-  grep "prune: 3 files would be pruned" prune.log
+  grep "prune: 8 local objects, 3 retained, done." prune.log
+  grep "prune: 5 files would be pruned" prune.log
 
   # also check that the same result is obtained when inside worktree rather than main
   cd "../w1_$reponame"
   git lfs prune --dry-run 2>&1 | tee prune.log
-  grep "prune: 6 local objects, 3 retained, done." prune.log
-  grep "prune: 3 files would be pruned" prune.log
+  grep "prune: 8 local objects, 3 retained, done." prune.log
+  grep "prune: 5 files would be pruned" prune.log
 
   # now remove a worktree & prove that frees up 1 head while keeping the other
   cd "../$reponame"
   rm -rf "../w1_$reponame"
   git worktree prune # required to get git to tidy worktree metadata
   git lfs prune --dry-run 2>&1 | tee prune.log
-  grep "prune: 6 local objects, 2 retained, done." prune.log
-  grep "prune: 4 files would be pruned" prune.log
+  grep "prune: 8 local objects, 2 retained, done." prune.log
+  grep "prune: 6 files would be pruned" prune.log
 )
 end_test
