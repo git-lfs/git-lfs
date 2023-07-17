ac05466bfb1bcaae04ff36c4628ae85f821a05e4
Chris Darroch <chrisd8088@github.com>
2023-07-17 12:25:50 -0700

    commands/run.go: remove completion help text
    In commit 15723008062b941b53743295784554b2f3ba875e of PR #5311 the
    "git lfs completion" command was introduced, utilizing the support
    provided by the spf13/cobra package to generate tab-completion scripts
    for a number of shells.  Short and long help text was provided at
    the same time, in the formats offered by the spf13/cobra package.
    
    However, the Git LFS client explicitly overrides the default help
    functions of the spf13/cobra package, and replacing them with functions
    which output versions of the Git LFS manual pages that are pre-rendered
    by our build process.  Thus the long-format help text included in
    PR #5311 can not be viewed by end users.
    
    As this text is also somewhat terse, having been copied from the
    example text provided by the spf13/cobra package, we remove it now
    with the expectation that in a subsequent commit in this PR we will
    provide a more comprehensive set of instructions in a new manual page
    for the "git lfs completion" command.
    
    We also remove the short help text for the "git lfs completion" command,
    as the majority of other Git LFS commands (with the exception of the
    "git lfs help" command) do not define these text strings, and in a
    subsequent commit in this PR we will suppress their inclusion in the
    command lists output by the completion scripts, so as not to make
    the formatting of those lists irregular and uneven.

--- commands/run.go
+++ commands/run.go
@@ -64,39 +64,7 @@ func Run() int {
 	root.PreRun = nil
 
 	completionCmd := &cobra.Command{
-		Use:   "completion [bash|zsh|fish]",
-		Short: "Generate completion script",
-		Long: tr.Tr.Get(`To load completions:
-
-Bash:
-
-  $ source <(%[1]s completion bash)
-
-  # To load completions for each session, execute once:
-  # Linux:
-  $ %[1]s completion bash > /etc/bash_completion.d/%[1]s
-  # macOS:
-  $ %[1]s completion bash > $(brew --prefix)/etc/bash_completion.d/%[1]s
-
-Zsh:
-
-  # If shell completion is not already enabled in your environment,
-  # you will need to enable it.  You can execute the following once:
-
-  $ echo "autoload -U compinit; compinit" >> ~/.zshrc
-
-  # To load completions for each session, execute once:
-  $ %[1]s completion zsh > "${fpath[1]}/_%[1]s"
-
-  # You will need to start a new shell for this setup to take effect.
-
-fish:
-
-  $ %[1]s completion fish | source
-
-  # To load completions for each session, execute once:
-  $ %[1]s completion fish > ~/.config/fish/completions/%[1]s.fish
-`, root.Name()),
+		Use:                   "completion [bash|zsh|fish]",
 		DisableFlagsInUseLine: true,
 		ValidArgs:             []string{"bash", "zsh", "fish"},
 		Args:                  cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
