01555b58c5f97d69a9978aefc5967cf1709f4a98
Siyuan Zhang <16775200+murez@users.noreply.github.com>
2024-01-17 16:45:49 +0800

    Update install.sh
    add option `--local` to install at `$HOME/.local` and check permission of the prefix before installing

--- script/install.sh
+++ script/install.sh
@@ -9,14 +9,37 @@ elif [ "${BOXEN_HOME:-}" != "" ] ; then
   prefix=${BOXEN_HOME:-}
 fi
 
-mkdir -p $prefix/bin
-rm -rf $prefix/bin/git-lfs*
+local_install=false
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --local)
+      local_install=true
+      prefix="$HOME/.local"
+      shift
+      ;;
+    *)
+      echo "Unknown option: $1"
+      exit 1
+      ;;
+  esac
+done
+
+# Check if the user has permission to install in the specified prefix
+if [ ! -w "$prefix" ]; then
+  echo "Error: Insufficient permissions to install in $prefix. Try running with sudo or choose a different prefix."
+  exit 1
+fi
+
+mkdir -p "$prefix/bin"
+rm -rf "$prefix/bin/git-lfs*"
 
 pushd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null
   for g in git*; do
-    install $g "$prefix/bin/$g"
+    install "$g" "$prefix/bin/$g"
   done
 popd > /dev/null
 
-PATH+=:$prefix/bin
+PATH+=:"$prefix/bin"
 git lfs install
+
