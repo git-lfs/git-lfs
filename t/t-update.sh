a18876b132b929580b4695e78f69fb8ed5b4876c
Pinghao Wu <xdavidwuph@gmail.com>
2024-11-01 11:02:40 +0800

    t-update: fix missing check for replace old hook 5
    Fixes: 86a8d7369c9820caa378f51468de919ef3d4e5e2

--- t/t-update.sh
+++ t/t-update.sh
@@ -80,13 +80,13 @@ git lfs pre-push \"\$@\"" > .git/hooks/pre-push
 command -v git-lfs >/dev/null 2>&1 || { echo >&2 \"\\nThis repository has been set up with Git LFS but Git LFS is not installed.\\n\"; exit 2; }
 git lfs pre-push \"\$@\"" > .git/hooks/pre-push
   [ "Updated Git hooks." = "$(git lfs update)" ]
+  [ "$pre_push_hook" = "$(cat .git/hooks/pre-push)" ]
 
   # replace old hook 6
   echo "#!/bin/sh
 command -v git-lfs >/dev/null 2>&1 || { echo >&2 \"\\nThis repository is configured for Git LFS but 'git-lfs' was not found on your path. If you no longer wish to use Git LFS, remove this hook by deleting '.git/hooks/pre-push'.\\n\"; exit 2; }
 git lfs pre-push \"\$@\"" > .git/hooks/pre-push
   [ "Updated Git hooks." = "$(git lfs update)" ]
-  [ "$pre_push_hook" = "$(cat .git/hooks/pre-push)" ]
   [ "$pre_push_hook" = "$(cat .git/hooks/pre-push)" ]
 
   # replace old hook 7
