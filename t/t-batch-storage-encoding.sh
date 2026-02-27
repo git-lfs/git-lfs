5afa7ee7dba4abff69869dc5c0425080cbe19144
Chris Darroch <chrisd8088@github.com>
2026-02-26 19:54:19 -0800

    t: refactor object transfer zstd decoding tests
    In prior commits in this PR we introduced a new configuration option
    named "lfs.httpDownloadEncoding", which may be set to either of the
    values "gzip" or "zstd", with a default value of "gzip".  We also
    added a set of tests to confirm that the Git LFS client sends an
    Accept-Encoding header with the selected encoding type when making
    object transfer download HTTP requests, and that the client is able
    to decode the response data if the server chooses to encode it.
    
    We now move those tests into the "t/t-batch-storage-encoding.sh"
    script we created recently in PR #6207, and rename and refactor them
    to follow the design of the existing test in that script.  In doing
    so, we combine the "lfs.httpDownloadEncoding zstd" test and the
    "lfs.<url>.httpDownloadEncoding zstd" test into a single test
    named "batch storage HTTP download with zstd encoding".
    
    We also elide the "lfs.httpDownloadEncoding gzip does not set header"
    test, as the existing "batch storage HTTP download with gzip encoding"
    test should be sufficient to demonstrate that when our new configuration
    option is explicitly set to "gzip", all the Accept-Encoding headers
    we expect the client to send have the value "gzip".
    
    In our "batch storage HTTP download with zstd encoding" test we check
    that the client sends an "Accept-Encoding: zstd" header when downloading
    individual objects, but still sends an "Accept-Encoding: gzip" header
    in its Batch API requests, since we only support zstd encoding for
    object transfer downloads.  We use the -c option to the grep(1) command
    to verify that we see the expected number of Accept-Encoding headers
    with each value.
    
    As well as checking the Accept-Encoding headers, we also update the
    test and our "lfstest-gitserver" utility program so that we can confirm
    that the Git LFS client properly decodes the object data it receives
    from a server, if a "Content-Encoding: zstd" response header indicates
    that the data is zstd-encoded.
    
    First, we enhance the "lfstest-gitserver" utility so that when handling
    a GET request for an object, if the object's content is the special key
    string "storage-download-encoding-zstd" and if an "Accept-Encoding: zstd"
    header was sent, then the program returns a response with a
    "Content-Encoding: zstd" header and a zstd-encoded body.
    
    Next, in our "batch storage HTTP download with zstd encoding" test we
    check that a single "Content-Encoding: zstd" header was received by each
    "git lfs pull" command, and we use our assert_local_object() assertion
    to verify that a local copy of the object has been created.
    
    In a previous commit in this PR we updated the "batch storage HTTP
    download with gzip encoding" test so that it repeats its checks
    of the client's default behaviour with the "lfs.httpDownloadEncoding"
    option explicitly set to the value "gzip", and then repeats the checks
    again with a URL-specific version of the option set to the value "gzip".
    
    We follow this pattern in our "batch storage HTTP download with zstd
    encoding" test, except that the test does not exercise the client's
    default behaviour, since the client sends an "Accept-Encoding: gzip"
    header when performing object downloads in that case.
    
    In both tests, when we set the URL-specific configuration option, we
    use the value of the GITSERVER shell variable for the URL, since it is
    conveniently available and generic across all our tests' repositories.
    
    We also ascertain that each "git lfs pull" command succeeds by checking
    the first element of the PIPESTATUS array variable.  This is necessary
    because the shell's "errexit" attribute ignores the exit codes of all
    commands in a pipeline other than the final one, and we pipe the output
    of our "git lfs pull" commands into the tee(1) command to capture a
    log file.  As in many of the other tests in our suite, we use the name
    "pull.log" for this file.

--- t/t-batch-storage-encoding.sh
+++ t/t-batch-storage-encoding.sh
@@ -81,3 +81,98 @@ begin_test "batch storage HTTP download with gzip encoding"
   assert_local_object "$contents_oid" "${#contents}"
 )
 end_test
+
+begin_test "batch storage HTTP download with zstd encoding"
+(
+  set -e
+
+  reponame="batch-storage-download-encoding-zstd"
+  setup_remote_repo "$reponame"
+  clone_repo "$reponame" "$reponame"
+
+  git lfs track "*.dat"
+
+  # This content announces to the server that it should expect an
+  # "Accept-Encoding: zstd" header and send a zstd-compressed response.
+  contents="storage-download-encoding-zstd"
+  contents_oid=$(calc_oid "$contents")
+  printf "%s" "$contents" >a.dat
+
+  git add .gitattributes a.dat
+  git commit -m "initial commit"
+
+  git push origin main
+
+  # Test object transfer download with an "Accept-Encoding: zstd" header.
+  rm -rf .git/lfs/objects
+
+  git config lfs.httpDownloadEncoding zstd
+
+  GIT_TRACE=1 GIT_CURL_VERBOSE=1 git lfs pull 2>&1 | tee pull.log
+  if [ "0" -ne "${PIPESTATUS[0]}" ]; then
+    echo >&2 "fatal: expected 'git lfs pull' to succeed ..."
+    exit 1
+  fi
+
+  # We expect one "Accept-Encoding: gzip" header from the Batch API request,
+  # prior to the object transfer download request.
+  [ 1 -eq "$(grep -c "Accept-Encoding: gzip" pull.log)" ]
+  [ 1 -eq "$(grep -c "Accept-Encoding: zstd" pull.log)" ]
+
+  [ 1 -eq "$(grep -c "Content-Encoding: zstd" pull.log)" ]
+
+  assert_local_object "$contents_oid" "${#contents}"
+
+  # Test again with a URL-specific configuration.
+  rm -rf .git/lfs/objects
+
+  git config --unset lfs.httpDownloadEncoding
+  git config "lfs.$GITSERVER.httpDownloadEncoding" zstd
+
+  GIT_TRACE=1 GIT_CURL_VERBOSE=1 git lfs pull 2>&1 | tee pull.log
+  if [ "0" -ne "${PIPESTATUS[0]}" ]; then
+    echo >&2 "fatal: expected 'git lfs pull' to succeed ..."
+    exit 1
+  fi
+
+  # We expect one "Accept-Encoding: gzip" header from the Batch API request,
+  # prior to the object transfer download request.
+  [ 1 -eq "$(grep -c "Accept-Encoding: gzip" pull.log)" ]
+  [ 1 -eq "$(grep -c "Accept-Encoding: zstd" pull.log)" ]
+
+  [ 1 -eq "$(grep -c "Content-Encoding: zstd" pull.log)" ]
+
+  assert_local_object "$contents_oid" "${#contents}"
+)
+end_test
+
+begin_test "batch storage HTTP download with invalid encoding"
+(
+  set -e
+
+  reponame="batch-storage-download-encoding-invalid"
+  setup_remote_repo "$reponame"
+  clone_repo "$reponame" "$reponame"
+
+  git lfs track "*.dat"
+  printf "contents" > a.dat
+  git add .gitattributes a.dat
+  git commit -m "initial commit"
+
+  git push origin main
+
+  # Now test download with invalid Accept-Encoding value
+  rm -rf .git/lfs/objects
+
+  git config lfs.httpDownloadEncoding "br"
+
+  # Should fail with an error
+  git lfs pull 2>&1 | tee pull.log
+  if [ "0" -eq "${PIPESTATUS[0]}" ]; then
+    echo >&2 "fatal: expected 'git lfs pull' to fail ..."
+    exit 1
+  fi
+
+  grep "unsupported.*httpDownloadEncoding" pull.log
+)
+end_test
