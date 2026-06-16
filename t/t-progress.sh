4a4e524249cad77ce86cce3369172f9cfc80839d
Chris Darroch <chrisd8088@github.com>
2026-06-16 16:52:46 -0700

    t/t-progress.sh: update existing progress log test
    In commit e8325a90983a83a348907c9d160f0bc3a1ce0187 of PR #2608 we added
    the original version of our "t/t-progress.sh" shell test script with
    a single test named "GIT_LFS_PROGRESS".  This test remains the only
    one in the script, and is intended to verify that the Git LFS client
    writes progress log messages into the file specified with the
    GIT_LFS_PROGRESS environment variable.
    
    At present, the test creates a repository whose name is derived
    solely from the name of the script.  In a subsequent commit in this PR
    we expect to add additional tests to the "t/t-progress.sh" script,
    and so we now revise the initial test to name its repository from
    a local variable with a fixed value.  We also change the name of
    the test to more clearly identify its purpose and to align its
    name with that of its test repository.
    
    The Git LFS client requires that the GIT_LFS_PROGRESS environment
    variable contain an absolute file path before the client will
    write any progress log messages to the file.  The test therefore
    sets the environment variable with an absolute path to a file named
    "progress.log" in the directory specified by the TRASHDIR shell
    variable.  This directory is created during the initialization of
    each shell test script and is unique to the script.
    
    However, since we intend to add new tests to the "t/t-progress.sh"
    script, if we use the same file path for the GIT_LFS_PROGRESS
    environment variable in each test, we will need all the tests
    which follow the initial one to begin by removing the file, or
    else the tests may pass because they unintentionally match lines
    in the log created by previous tests.
    
    We therefore rewrite the initial test to create a series of
    unique progress log files in a directory unique to the test,
    whose path we construct using the TRASHDIR variable plus the
    name of the test's repository.
    
    As well, we expand the test so that it now verifies all of the
    types of progress log messages output by the Git LFS client,
    including for both "clean" filter operations and "upload" object
    transfers.  Note that "clean" filter operations are logged separately
    for each object, so we always find "1/1" fields in these messages
    (i.e., "one file of one total file"), whereas our other messages
    report "1/n" fields where "n" is the total number of objects being
    downloaded or uploaded.
    
    We also take the opportunity to insert clarifying whitespace to the
    test, add a check that the "git push" command returned an exit code
    of zero to indicate success, and change the name of the directory
    into which the test clones its primary repository to follow our
    current idiom, in which we append the suffix "-assert" to the name
    of the source repository.

--- t/t-progress.sh
+++ t/t-progress.sh
@@ -2,53 +2,82 @@
 
 . "$(dirname "$0")/testlib.sh"
 
-reponame="$(basename "$0" ".sh")"
-
-begin_test "GIT_LFS_PROGRESS"
+begin_test "progress log: file counts"
 (
   set -e
+
+  reponame="progress-log-file-counts"
   setup_remote_repo "$reponame"
-  clone_repo "$reponame" repo
+  clone_repo "$reponame" "$reponame"
 
   git lfs track "*.dat"
+
   echo "a" > a.dat
   echo "b" > b.dat
   echo "c" > c.dat
   echo "d" > d.dat
   echo "e" > e.dat
-  git add .gitattributes *.dat
+
+  progress_log_dir="$TRASHDIR/${reponame}-logs"
+
+  progress_log="$progress_log_dir/progress-clean.log"
+  GIT_LFS_PROGRESS="$progress_log" git add .gitattributes *.dat
+  cat "$progress_log"
+
+  [ 5 -eq "$(grep -c "clean 1/1" "$progress_log")" ]
+
   git commit -m "add files"
-  git push origin main 2>&1 | tee push.log
+
+  progress_log="$progress_log_dir/progress-push.log"
+  GIT_LFS_PROGRESS="$progress_log" git push origin main 2>&1 | tee push.log
+  [ 0 -eq "${PIPESTATUS[0]}" ]
+  cat "$progress_log"
+
   grep "Uploading LFS objects: 100% (5/5), 10 B" push.log
 
+  grep "upload 1/5" "$progress_log"
+  grep "upload 2/5" "$progress_log"
+  grep "upload 3/5" "$progress_log"
+  grep "upload 4/5" "$progress_log"
+  grep "upload 5/5" "$progress_log"
+
   cd ..
-  GIT_LFS_PROGRESS="$TRASHDIR/progress.log" git lfs clone "$GITSERVER/$reponame" clone
-  cat progress.log
-  grep "download 1/5" progress.log
-  grep "download 2/5" progress.log
-  grep "download 3/5" progress.log
-  grep "download 4/5" progress.log
-  grep "download 5/5" progress.log
 
-  GIT_LFS_SKIP_SMUDGE=1 git clone "$GITSERVER/$reponame" clone2
-  cd clone2
+  progress_log="$progress_log_dir/progress-clone.log"
+  GIT_LFS_PROGRESS="$progress_log" git lfs clone "$GITSERVER/$reponame" "${reponame}-assert"
+  cat "$progress_log"
 
-  rm -rf "$TRASHDIR/progress.log" .git/lfs/objects
-  GIT_LFS_PROGRESS="$TRASHDIR/progress.log" git lfs fetch --all
-  cat ../progress.log
-  grep "download 1/5" ../progress.log
-  grep "download 2/5" ../progress.log
-  grep "download 3/5" ../progress.log
-  grep "download 4/5" ../progress.log
-  grep "download 5/5" ../progress.log
+  grep "download 1/5" "$progress_log"
+  grep "download 2/5" "$progress_log"
+  grep "download 3/5" "$progress_log"
+  grep "download 4/5" "$progress_log"
+  grep "download 5/5" "$progress_log"
 
-  rm -rf "$TRASHDIR/progress.log"
-  GIT_LFS_PROGRESS="$TRASHDIR/progress.log" git lfs checkout
-  cat ../progress.log
-  grep "checkout 1/5" ../progress.log
-  grep "checkout 2/5" ../progress.log
-  grep "checkout 3/5" ../progress.log
-  grep "checkout 4/5" ../progress.log
-  grep "checkout 5/5" ../progress.log
+  rm -rf "${reponame}-assert"
+
+  GIT_LFS_SKIP_SMUDGE=1 git clone "$GITSERVER/$reponame" "${reponame}-assert"
+  cd "${reponame}-assert"
+
+  rm -rf .git/lfs/objects
+
+  progress_log="$progress_log_dir/progress-fetch.log"
+  GIT_LFS_PROGRESS="$progress_log" git lfs fetch --all
+  cat "$progress_log"
+
+  grep "download 1/5" "$progress_log"
+  grep "download 2/5" "$progress_log"
+  grep "download 3/5" "$progress_log"
+  grep "download 4/5" "$progress_log"
+  grep "download 5/5" "$progress_log"
+
+  progress_log="$progress_log_dir/progress-checkout.log"
+  GIT_LFS_PROGRESS="$progress_log" git lfs checkout
+  cat "$progress_log"
+
+  grep "checkout 1/5" "$progress_log"
+  grep "checkout 2/5" "$progress_log"
+  grep "checkout 3/5" "$progress_log"
+  grep "checkout 4/5" "$progress_log"
+  grep "checkout 5/5" "$progress_log"
 )
 end_test
