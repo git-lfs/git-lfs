9050527e631945fb4935651392904a05103dfff7
Chris Darroch <chrisd8088@github.com>
2026-06-26 15:26:27 -0700

    t/t-filter-process.sh: update and rename new test
    In a prior commit in this PR we revised our "clean" Git filter
    commands to address the secondary issue described in #4974, and
    then in a subsequent commit we introduced a new test which
    validates the changes we made to our filter commands.
    
    Specifically, the "filter process: don't trust file size on disk
    when cleaning" test in our "t/t-filter-process.sh" shell script
    creates the conditions described in the following comment, and then
    verifies that they no longer cause the "git lfs filter-process"
    command to report an error:
    
      https://github.com/git-lfs/git-lfs/issues/4974#issuecomment-2717907543
    
    To reproduce the conditions described in the comment, the test
    starts by creating a file larger than the size of the buffer we use
    to detect and decode Git LFS pointers.  The test then adds the file
    as a Git LFS object, causing its contents to be streamed to our
    "clean" filter process.  However, the test also ensures that a file
    smaller than the size of our pointer detection buffer is written to
    the same location as the original file before our filter process
    actually executes.
    
    Since we first developed this test for this PR, in commit
    6cc587c2a5eca3a6562763268533a6d5b9c65f59 of PR #6285 we revised a
    number of the pre-existing tests in our "t/t-filter-process.sh" shell
    script to better document their intent and to bring them into closer
    alignment with each other.
    
    We therefore now make several adjustments to our new "filter process:
    don't trust file size on disk when cleaning" test so that it conforms
    with the current implementation of the other tests in the same script.
    
    We also expand the script to perform one additional check, and take
    the opportunity to slightly rename the test to help clarify that it
    expects the Git LFS client to entirely ignore any file in the working
    tree which might (or might not) correspond to the source of the data
    Git streams to the "git lfs filter-process" command.
    
    First, we rename the test's "oid" shell variable to "contents_oid",
    to match the naming scheme found in our other tests.
    
    Next, we add a comment to explain how the "lfstest-badlocalfile"
    helper utility that we introduced in a prior commit in this PR is
    used by the test to replicate the original issue (i.e., the
    secondary issue reported in #4974).
    
    As well, we capture the output of the "git add" command into a log
    file and check that this file does not contain the error message
    "unknown command", which was a symptom of the original problem.
    Because our use of the tee(1) command to create the log file obscures
    the exit code from the "git add" command, we also explicitly check
    that command's exit code, following the idiom established by many
    of our other shell tests.
    
    Finally, we rename the test to "filter process: ignore file size on
    disk when cleaning", and change the name of the repository the test
    creates to match the new test name.

--- t/t-filter-process.sh
+++ t/t-filter-process.sh
@@ -334,12 +334,11 @@ begin_test "filter process: git archive does not invoke SSH"
 )
 end_test
 
-# https://github.com/git-lfs/git-lfs/issues/4974
-begin_test "filter process: don't trust file size on disk when cleaning"
+begin_test "filter process: ignore file size on disk when cleaning"
 (
   set -e
 
-  reponame="repo-issue-4974"
+  reponame="filter-process-ignore-file-size"
   setup_remote_repo "$reponame"
   clone_repo "$reponame" "$reponame"
 
@@ -354,16 +353,24 @@ begin_test "filter process: don't trust file size on disk when cleaning"
   max_pointer_size="$(lfstest-getlimit --max-pointer-size)"
   contents_size=$((max_pointer_size * 2))
   head -c "$contents_size" /dev/zero >large.dat
-  oid=$(calc_oid_file large.dat)
+  contents_oid="$(calc_oid_file "large.dat")"
 
+  # To replicate the reported issue, the "git-lfs filter-process" command's
+  # input from Git should be larger than the size of the pointer read buffer,
+  # but the file on disk should be smaller, so the lfstest-badlocalfile helper
+  # renames the initial file after Git has started piping its data, and then
+  # replaces the file with an empty file.
   GIT_TRACE_PACKET=1 git \
     -c "filter.lfs.process=lfstest-badlocalfile | git-lfs filter-process" \
     -c "filter.lfs.clean=false"\
     -c "filter.lfs.smudge=false" \
     -c "filter.lfs.required=true" \
-    add large.dat
+    add large.dat 2>&1 | tee filter.log
+  [ 0 -eq "${PIPESTATUS[0]}" ]
 
-  expected="$(pointer "$oid" "$contents_size")"
+  [ 0 -eq "$(grep -c "unknown command" filter.log)" ]
+
+  expected="$(pointer "$contents_oid" "$contents_size")"
   got="$(git cat-file -p :large.dat)"
 
   diff -u <(echo "$expected") <(echo "$got")
